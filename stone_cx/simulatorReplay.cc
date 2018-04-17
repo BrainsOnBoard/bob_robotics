e35a89c0dacc20ef9d4417e9cc6bfa72e61aa241
jamie <neworderofjamie@gmail.com>
2018-04-17 11:27:39 +0100

    Stone CX work * New simulator to 'replay' captured Vicon data * Moved activity visualizer into seperate module * Hopefully fixed simulator vicon

--- stone_cx/simulatorReplay.cc
+++ stone_cx/simulatorReplay.cc
@@ -1,7 +1,9 @@
 // Standard C++ includes
+#include <fstream>
 #include <iostream>
 #include <numeric>
 #include <random>
+#include <sstream>
 #include <vector>
 
 // Standard C includes
@@ -13,7 +15,6 @@
 
 // Common includes
 #include "../common/analogue_csv_recorder.h"
-#include "../common/von_mises_distribution.h"
 
 // GeNN generated code includes
 #include "stone_cx_CODE/definitions.h"
@@ -21,8 +22,17 @@
 // Model includes
 #include "parameters.h"
 #include "simulatorCommon.h"
-#include "spline.h"
 #include "visualizationCommon.h"
+
+namespace
+{
+double readDoubleField(std::istringstream &lineStream)
+{
+    std::string field;
+    std::getline(lineStream, field, ',');
+    return std::stod(field);
+}
+}
 
 int main()
 {
@@ -33,17 +43,8 @@ int main()
     
     const double preferredAngleTN2[] = { Parameters::pi / 4.0, -Parameters::pi / 4.0 };
     
-    // Outbound path generation parameters
-    const unsigned int numOutwardTimesteps = 1500;
-
-    // Agent dynamics parameters
-    const double pathLambda = 0.4;
-    const double pathKappa = 100.0;
-
     const double agentDrag = 0.15;
 
-    const double agentMinAcceleration = 0.0;
-    const double agentMaxAcceleration = 0.15;
     const double agentM = 0.5;
     
     allocateMem();
@@ -73,39 +74,6 @@ int main()
     cv::moveWindow("Activity", pathImageSize, 0);
     cv::Mat activityImage(activityImageHeight, activityImageWidth, CV_8UC3, cv::Scalar::all(0));
 
-    // Create Von Mises distribution to sample angular acceleration from
-    std::array<uint32_t, std::mt19937::state_size> seedData;
-    std::random_device seedSource;
-    std::generate(seedData.begin(), seedData.end(),
-                  [&seedSource](){ return seedSource(); });
-    std::seed_seq seeds(std::begin(seedData), std::end(seedData));
-    std::mt19937 gen(seeds);
-
-    VonMisesDistribution<double> pathVonMises(0.0, pathKappa);
-
-    // Create acceleration spline
-    tk::spline accelerationSpline;
-    {
-        // Create vectors to hold the times at which linear acceleration
-        // should change and it's values at those time
-        const unsigned int numAccelerationChanges = numOutwardTimesteps / 50;
-        std::vector<double> accelerationTime(numAccelerationChanges);
-        std::vector<double> accelerationMagnitude(numAccelerationChanges);
-
-        // Draw accelerations from real distribution
-        std::uniform_real_distribution<double> acceleration(agentMinAcceleration,
-                                                            agentMaxAcceleration);
-        std::generate(accelerationMagnitude.begin(), accelerationMagnitude.end(),
-                      [&gen, &acceleration](){ return acceleration(gen); });
-
-        for(unsigned int i = 0; i < numAccelerationChanges; i++) {
-            accelerationTime[i] = i * 50;
-        }
-
-        // Build spline from these
-        accelerationSpline.set_points(accelerationTime, accelerationMagnitude);
-    }
-
 #ifdef RECORD_ELECTROPHYS
     AnalogueCSVRecorder<scalar> tn2Recorder("tn2.csv", rTN2, Parameters::numTN2, "TN2");
     AnalogueCSVRecorder<scalar> cl1Recorder("cl1.csv", rCL1, Parameters::numCL1, "CL1");
@@ -114,17 +82,21 @@ int main()
     AnalogueCSVRecorder<scalar> cpu1Recorder("cpu1.csv", rCPU1, Parameters::numCPU1, "CPU1");
 #endif  // RECORD_ELECTROPHYS
 
+    std::ifstream replayData("data.csv");
+
     // Simulate
-    double omega = 0.0;
     double theta = 0.0;
     double xVelocity = 0.0;
     double yVelocity = 0.0;
     double xPosition = 0.0;
     double yPosition = 0.0;
+    double xStartPosition = 0.0;
+    double yStartPosition = 0.0;
+    bool first = true;
     for(unsigned int i = 0;; i++) {
         // Project velocity onto each TN2 cell's preferred angle and use as speed input
         for(unsigned int j = 0; j < Parameters::numTN2; j++) {
-            speedTN2[j] = (sin(theta + preferredAngleTN2[j]) * xVelocity) + 
+            speedTN2[j] = (sin(theta + preferredAngleTN2[j]) * xVelocity) +
                 (cos(theta + preferredAngleTN2[j]) * yVelocity);
         }
 
@@ -145,17 +117,40 @@ int main()
         // Visualize network activity
         visualize(activityImage);
 
-        // If we are on outbound segment of route
-        const bool outbound = (i < numOutwardTimesteps);
-        double a = 0.0;
+        // If we there is more data to replay
+        std::string line;
+        const bool outbound = !std::getline(replayData, line).fail();
         if(outbound) {
-            // Update angular velocity
-            omega = (pathLambda * omega) + pathVonMises(gen);
+            // Read position from file
+            std::istringstream lineStream(line);
+            xPosition = readDoubleField(lineStream);
+            yPosition = readDoubleField(lineStream);
+            readDoubleField(lineStream);
 
-            // Read linear acceleration off spline
-            a = accelerationSpline((double)i);
+            // If this is the first position, update start position so drawing can be centred
+            if(first) {
+                xStartPosition = xPosition;
+                yStartPosition = yPosition;
+                first = false;
+            }
 
-            if(i == (numOutwardTimesteps - 1)) {
+            // Read velocity
+            xVelocity = 6.0f * readDoubleField(lineStream);
+            yVelocity = 6.0f * readDoubleField(lineStream);
+            readDoubleField(lineStream);
+
+            // Read orientation, flipping and converting from (-pi,pi) to (0, 2pi)
+            readDoubleField(lineStream);
+            readDoubleField(lineStream);
+            theta = -readDoubleField(lineStream);
+            if(theta < 0.0) {
+                theta = (2.0 * Parameters::pi) + theta;
+            }
+
+            // If this is the end of outbound data, zero velocity (as simulated dynamics don't match well) and display CPU4 high-water mark
+            if(replayData.eof()) {
+                xVelocity = 0.0;
+                yVelocity = 0.0;
                 std::cout << "Max CPU4 level r=" << *std::max_element(&rCPU4[0], &rCPU4[Parameters::numCPU4]) << ", i=" << *std::max_element(&iCPU4[0], &iCPU4[Parameters::numCPU4]) << std::endl;
             }
         }
@@ -166,28 +161,28 @@ int main()
             const scalar rightMotor = std::accumulate(&rCPU1[8], &rCPU1[16], 0.0f);
 
             // Use difference between left and right to calculate angular velocity
-            omega = -agentM * (rightMotor - leftMotor);
+            const double omega = -agentM * (rightMotor - leftMotor);
 
             // Use fixed acceleration
-            a = 0.1;
+            const double a = 0.1;
+
+            // Update heading
+            theta += omega;
+
+            // Update linear velocity
+            // **NOTE** this comes from https://github.com/InsectRobotics/path-integration/blob/master/bee_simulator.py#L77-L83 rather than the methods section
+            xVelocity += sin(theta) * a;
+            yVelocity += cos(theta) * a;
+            xVelocity -= agentDrag * xVelocity;
+            yVelocity -= agentDrag * yVelocity;
+
+            // Update position
+            xPosition += xVelocity;
+            yPosition += yVelocity;
         }
 
-        // Update heading
-        theta += omega;
-
-        // Update linear velocity
-        // **NOTE** this comes from https://github.com/InsectRobotics/path-integration/blob/master/bee_simulator.py#L77-L83 rather than the methods section
-        xVelocity += sin(theta) * a;
-        yVelocity += cos(theta) * a;
-        xVelocity -= agentDrag * xVelocity;
-        yVelocity -= agentDrag * yVelocity;
-
-        // Update position
-        xPosition += xVelocity;
-        yPosition += yVelocity;
-
         // Draw agent position (centring so origin is in centre of path image)
-        const cv::Point p((pathImageSize / 2) + (int)xPosition, (pathImageSize / 2) + (int)yPosition);
+        const cv::Point p((pathImageSize / 2) + (int)(0.25 * (xPosition - xStartPosition)), (pathImageSize / 2) + (int)(0.25 * (yPosition - yStartPosition)));
         cv::line(pathImage, p, p,
                  outbound ? CV_RGB(0xFF, 0, 0) : CV_RGB(0, 0xFF, 0));
 
--- stone_cx/simulatorVicon.cc
+++ stone_cx/simulatorVicon.cc
@@ -13,21 +13,15 @@
 
 // Model includes
 #include "parameters.h"
+#include "robotCommon.h"
 #include "robotParameters.h"
 #include "simulatorCommon.h"
 
-
-enum class ViconEvent : unsigned int
-{
-    TrialStart,
-    HomeStart,
-    TrialEnd,
-};
-
 int main(int argc, char *argv[])
 {
-    float speedScale = 1.0f / 400.0f;
-  
+    const float speedScale = 6.0f;
+    const double preferredAngleTN2[] = { Parameters::pi / 4.0, -Parameters::pi / 4.0 };
+
     // Create joystick interface
     Joystick joystick;
 
@@ -35,7 +29,7 @@ int main(int argc, char *argv[])
     MotorI2C motor;
 
     // Create VICON UDP interface
-    Vicon::UDPClient<Vicon::ObjectDataVelocity> vicon(51001, 100);
+    Vicon::UDPClient<Vicon::ObjectDataVelocity> vicon(51001);
 
     // Initialise GeNN
     allocateMem();
@@ -70,10 +64,6 @@ int main(int argc, char *argv[])
         std::cout << "Waiting for object..." << std::endl;
     }
 
-    std::ofstream eventStream("events.csv", std::ios_base::app);
-    eventStream << "Frame number, event id" << std::endl;    
-    eventStream << vicon.getFrameNumber() << "," << static_cast<unsigned int>(ViconEvent::TrialStart) << std::endl;
-
     // Loop until second joystick button is pressed
     bool outbound = true;
     unsigned int numTicks = 0;
@@ -96,21 +86,20 @@ int main(int argc, char *argv[])
         const auto &velocity = objectData.getVelocity();
         const auto &rotation = objectData.getRotation();
         
-        // Calculate scalar speed and apply to both TN2 hemisphere
-        // **NOTE** robot is incapable of holonomic motion!
-        float speed = sqrt(std::accumulate(std::begin(velocity), std::end(velocity), 0.0f,
-                                           [](float acc, double v){ return acc + (float)(v * v); }));
+        // Update TL input
+        headingAngleTL = -rotation[2];
+        if(headingAngleTL < 0.0) {
+            headingAngleTL = (2.0 * Parameters::pi) + headingAngleTL;
+        }
 
-        speed *= speedScale;
-        speedTN2[Parameters::HemisphereLeft] = speed;
-        speedTN2[Parameters::HemisphereRight] = speed;
+        // Project velocity onto each TN2 cell's preferred angle and use as speed input
+        for(unsigned int j = 0; j < Parameters::numTN2; j++) {
+            speedTN2[j] = (sin(headingAngleTL + preferredAngleTN2[j]) * velocity[0]) +
+                (cos(headingAngleTL + preferredAngleTN2[j]) * velocity[1]);
+        }
 
-
-        // Get yaw from VICON and pass to TL neurons
-        // **TODO** check axes and add enum
-        headingAngleTL = rotation[2] + Parameters::pi;
         if(numTicks % 100 == 0) {
-            std::cout <<  "Ticks:" << numTicks << ", Heading: " << headingAngleTL << ", Speed:" << speed << std::endl;
+            std::cout <<  "Ticks:" << numTicks << ", Heading: " << headingAngleTL << ", Speed: (" << speedTN2[0] << ", " << speedTN2 << ")" << std::endl;
         }
         //std::cout << "Heading:" << headingAngleTL << std::endl;
 
@@ -132,9 +121,9 @@ int main(int argc, char *argv[])
             
             // If first button is pressed switch to returning home
             if(joystick.isButtonDown(0)) {
+                std::cout << "Max CPU4 level r=" << *std::max_element(&rCPU4[0], &rCPU4[Parameters::numCPU4]) << ", i=" << *std::max_element(&iCPU4[0], &iCPU4[Parameters::numCPU4]) << std::endl;
                 std::cout << "Returning home!" << std::endl;
                 outbound = false;
-                eventStream << vicon.getFrameNumber() << "," << static_cast<unsigned int>(ViconEvent::HomeStart) << std::endl;
             }
         }
         // Otherwise we're returning home so use CPU1 neurons to drive motor
@@ -163,8 +152,7 @@ int main(int argc, char *argv[])
     
     // Show overflow stats
     std::cout << numOverflowTicks << "/" << numTicks << " ticks overflowed, mean tick time: " << (double)totalMicroseconds / (double)numTicks << "uS" << std::endl;
-    eventStream << vicon.getFrameNumber() << "," << static_cast<unsigned int>(ViconEvent::TrialEnd) << std::endl;
-    
+
     // Stop motor
     motor.tank(0.0f, 0.0f);
     
