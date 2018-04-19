533a9578592f176af5e791d5952e90bc373cd99b
jamie <neworderofjamie@gmail.com>
2018-04-19 09:41:46 +0000

    dead-reckoning version of robot simulator - works better

--- common/motor_i2c.h
+++ common/motor_i2c.h
@@ -17,14 +17,20 @@
 class MotorI2C : public Motor
 {
 public:
-    MotorI2C(const char *path = "/dev/i2c-1", int slaveAddress = 0x29) : m_I2C(path, slaveAddress) {
+    MotorI2C(const char *path = "/dev/i2c-1", int slaveAddress = 0x29) 
+    : m_I2C(path, slaveAddress), m_Left(0.0f), m_Right(0.0f) 
+    {
     }
     
     //----------------------------------------------------------------------------
     // Motor virtuals
     //----------------------------------------------------------------------------
     virtual void tank(float left, float right) override
-    {  
+    {
+        // Cache left and right
+        m_Left = left;
+        m_Right = right;
+  
         // Convert standard (-1,1) values to bytes in order to send to I2C slave
         uint8_t buffer[2] = { floatToI2C(left), floatToI2C(right) };
         
@@ -47,7 +53,16 @@ public:
         m_I2C.write(data);
     }
     
-    
+    float getLeft() const
+    {
+        return m_Left;
+    }
+
+    float getRight() const
+    {
+        return m_Right;
+    }
+
 private:
     //----------------------------------------------------------------------------
     // Private methods
@@ -56,8 +71,11 @@ private:
     {
         return (uint8_t)std::min(255.0f, std::max(0.0f, std::round(((speed + 1.0f) / 2.0f) * 255.0f)));
     }
+
     //----------------------------------------------------------------------------
     // Private members
     //----------------------------------------------------------------------------
     I2CInterface m_I2C;
+    float m_Left;
+    float m_Right;
 };
