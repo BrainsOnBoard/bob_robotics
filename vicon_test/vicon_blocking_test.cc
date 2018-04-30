ddd780ef751bbdff3ac1d941f25274855774325c
Unknown <a.dewar@sussex.ac.uk>
2018-04-30 14:01:46 +0100

    add a blocking version of Vicon test

---
+++ vicon_test/vicon_blocking_test.cc
@@ -0,0 +1,37 @@
+#include <chrono>
+#include <cstdlib>
+#include <iostream>
+#include <thread>
+
+#include "../common/vicon_udp.h"
+
+Vicon::UDPClient<Vicon::ObjectData> *vicon;
+
+void
+readCallback(uint id, Vicon::ObjectData data)
+{
+    const auto &translation = data.getTranslation();
+    const auto &rotation = data.getRotation();
+
+    std::cout << translation[0] << ", " << translation[1] << ", "
+              << translation[2] << ", " << rotation[0] << ", " << rotation[1]
+              << ", " << rotation[2] << std::endl;
+}
+
+int
+main()
+{
+    // connect to Vicon system
+    vicon = new Vicon::UDPClient<Vicon::ObjectData>(51001);
+    std::cout << "Connected to Vicon system" << std::endl;
+
+    // set function to call on new position
+    vicon->setReadCallback(readCallback);
+
+    // wait for keypress + return
+    char c;
+    std::cin >> c;
+
+    delete vicon;
+    return EXIT_SUCCESS;
+}
