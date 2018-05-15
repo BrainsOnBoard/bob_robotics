a6ac3af23cfe6b14e883b9b3ab7faafe3ac7b44b
Alex Dewar <a.dewar@sussex.ac.uk>
2018-05-15 17:00:20 +0100

    add function for listing the names of video devices I've made the os/ folder for OS-speciifc functionality. I have a few other bits and pieces I'll stick in there soon.

---
+++ os/video.h
@@ -0,0 +1,58 @@
+#include <fcntl.h>
+#include <linux/videodev2.h>
+#include <stdio.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+namespace OS::Video {
+struct CameraDevice
+{
+    int number;
+    std::string name;
+};
+
+const std::string
+getCameraName(int deviceNumber)
+{
+    int fd;
+    if ((fd = open(("/dev/video" + std::to_string(deviceNumber)).c_str(),
+                   O_RDONLY)) == -1) {
+        return "";
+    }
+
+    struct v4l2_capability video_cap;
+    if (ioctl(fd, VIDIOC_QUERYCAP, &video_cap) == -1) {
+        std::cerr << "Warning: Could not get video device capabilities"
+                  << std::endl;
+        close(fd);
+        return "";
+    }
+
+    return std::string((char *) video_cap.card);
+}
+
+std::vector<CameraDevice>
+getCameras()
+{
+    std::vector<CameraDevice> cameras;
+    int fd;
+    struct v4l2_capability video_cap;
+    for (int i = 0; i < 64; i++) {
+        const std::string name = getCameraName(i);
+        if (name == "") {
+            continue;
+        }
+
+        CameraDevice dev;
+        dev.number = i;
+        dev.name = name;
+        cameras.push_back(dev);
+        close(fd);
+    }
+    return cameras;
+}
+}
