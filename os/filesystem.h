80728e63a68d3d8cfa7141db1679027d4987d880
Alex Dewar <a.dewar@sussex.ac.uk>
2018-05-16 19:06:32 +0100

    add fileExists() function for Windows

--- os/filesystem.h
+++ os/filesystem.h
@@ -1,14 +1,23 @@
 #pragma once
-
 #include <string>
+
+#ifdef _WIN32
+#include <shlwapi.h>
+#pragma comment(lib, "shlwapi.lib")
+#else
 #include <sys/stat.h>
 #include <unistd.h>
+#endif
 
 namespace OS::FileSystem {
-inline __attribute__((always_inline)) bool
+inline bool
 fileExists(const std::string &filePath)
 {
+#ifdef _WIN32
+    return PathFileExists((LPCWSTR) filePath.c_str());
+#else
     struct stat buffer;
     return (stat(filePath.c_str(), &buffer) == 0);
+#endif
 }
 }
