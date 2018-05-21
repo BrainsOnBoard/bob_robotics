db8b90a415328c8b44da7dad7d6957aa9201cb8b
Alex Dewar <a.dewar@sussex.ac.uk>
2018-05-21 11:02:37 +0100

    start tidying unwrapfile

--- unwrap/unwrapfile_src/processfiles.h
+++ unwrap/unwrapfile_src/processfiles.h
@@ -1,5 +1,14 @@
 #pragma once
 
+// C++ includes
+#include <iostream>
+#include <string>
+#include <vector>
+
+// GeNN robotics includes
+#include "os/filesystem.h"
+
+// local includes
 #include "unwrap.h"
 
 /*
@@ -10,38 +19,22 @@
 #define FFMPEG_PATH "/usr/bin/ffmpeg"
 #endif
 
-using namespace std;
-using namespace cv;
-
-/* breaks a filepath into path, name (with extension) and extension */
-void getfileparts(string filepath, string &path, string &name)
-{
-    // find last slash
-    size_t slash = filepath.rfind('/');
-    if (slash == string::npos)
-        slash = 0;
-    else
-        slash++;
-
-    path = filepath.substr(0, slash);
-    name = filepath.substr(slash);
-}
-
 /* unwrap a JPEG file */
-void processjpeg(const char* filepath)
+void
+processjpeg(const char *filepath)
 {
-    string path, name;
-    getfileparts(filepath, path, name);
+    std::string path, name;
+    OS::FileSystem::getFileParts(filepath, path, name);
 
     // read image into memory
-    Mat im = imread(filepath, CV_LOAD_IMAGE_COLOR);
+    cv::Mat im = cv::imread(filepath, CV_LOAD_IMAGE_COLOR);
 
     // matrix to store unwrapped image
-    Mat imunwrap(unwrap_height, unwrap_width, im.type());
+    cv::Mat imunwrap(unwrap_height, unwrap_width, im.type());
 
     // matrices to store maps for unwrapping
-    Mat map_x(unwrap_height, unwrap_width, CV_32FC1);
-    Mat map_y(unwrap_height, unwrap_width, CV_32FC1);
+    cv::Mat map_x(unwrap_height, unwrap_width, CV_32FC1);
+    cv::Mat map_y(unwrap_height, unwrap_width, CV_32FC1);
 
     // perform unwrapping
     if (!processframe(filepath, imunwrap, im, map_x, map_y)) {
@@ -49,35 +42,36 @@ void processjpeg(const char* filepath)
     }
 
     // save file
-    string outfilename = path + "unwrapped_" + name;
-    cout << "Saving image to " << outfilename << "..." << endl;
-    vector<int> params;
+    std::string outfilename = path + "unwrapped_" + name;
+    std::cout << "Saving image to " << outfilename << "..." << std::endl;
+    std::vector<int> params;
     params.push_back(CV_IMWRITE_JPEG_QUALITY);
     params.push_back(100);
-    imwrite(outfilename, imunwrap, params);
+    cv::imwrite(outfilename, imunwrap, params);
 }
 
 bool copysound = true;
 
 /* unwrap an MP4 video */
-void processmp4(const char* filepath)
+void
+processmp4(const char *filepath)
 {
-    string path, name;
-    getfileparts(filepath, path, name);
+    std::string path, name;
+    OS::FileSystem::getFileParts(filepath, path, name);
 
     // open video file
-    VideoCapture cap(filepath);
+    cv::VideoCapture cap(filepath);
 
     // read in first frame
-    Mat fr;
+    cv::Mat fr;
     cap >> fr;
 
     // matrices to store maps for unwrapping
-    Mat map_x(unwrap_height, unwrap_width, CV_32FC1);
-    Mat map_y(unwrap_height, unwrap_width, CV_32FC1);
+    cv::Mat map_x(unwrap_height, unwrap_width, CV_32FC1);
+    cv::Mat map_y(unwrap_height, unwrap_width, CV_32FC1);
 
     // matrix to store unwrapped image
-    Mat imunwrap(unwrap_height, unwrap_width, fr.type());
+    cv::Mat imunwrap(unwrap_height, unwrap_width, fr.type());
 
     // try unwrapping first frame and return if fail
     if (!processframe(filepath, imunwrap, fr, map_x, map_y)) {
@@ -86,16 +80,17 @@ void processmp4(const char* filepath)
     }
 
     // final filename for unwrapped video
-    string outfilename = path + "unwrapped_" + name;
+    std::string outfilename = path + "unwrapped_" + name;
 
     // temporary file name to which we write initially
-    string tempfilename = copysound ? path + ".TEMP.MP4" : outfilename;
+    std::string tempfilename = copysound ? path + ".TEMP.MP4" : outfilename;
 
     // start writing to file
-    cout << "Saving video to " << outfilename << "..." << endl;
-    VideoWriter writer(tempfilename, 0x21, cap.get(CV_CAP_PROP_FPS), imunwrap.size());
+    std::cout << "Saving video to " << outfilename << "..." << std::endl;
+    cv::VideoWriter writer(
+            tempfilename, 0x21, cap.get(CV_CAP_PROP_FPS), imunwrap.size());
     if (!writer.isOpened()) {
-        cerr << "Error: Could not open file for writing" << endl;
+        std::cerr << "Error: Could not open file for writing" << std::endl;
         return;
     }
 
@@ -120,14 +115,17 @@ void processmp4(const char* filepath)
     if (copysound) {
         // attempt to copy audio stream from original to new file with ffmpeg
         int stat = system((FFMPEG_PATH " -y -i \"" + tempfilename + "\" -i \"" +
-                string(filepath) + "\" -map 0:v -map 1:a -c copy -shortest \"" +
-                outfilename + "\" >/dev/null 2>&1").c_str());
+                           std::string(filepath) +
+                           "\" -map 0:v -map 1:a -c copy -shortest \"" +
+                           outfilename + "\" >/dev/null 2>&1")
+                                  .c_str());
         if (stat != 0) {
-            cerr << "Error (" << stat << ") occurred while copying audio with ffmpeg" << endl;
+            std::cerr << "Error (" << stat
+                 << ") occurred while copying audio with ffmpeg" << std::endl;
 
             /*
-             * just rename the temporary file to the output file, so there won't be
-             * audio but at least there'll be video
+             * just rename the temporary file to the output file, so there won't
+             * be audio but at least there'll be video
              */
             rename(tempfilename.c_str(), outfilename.c_str());
         } else {
@@ -136,4 +134,3 @@ void processmp4(const char* filepath)
         }
     }
 }
-
--- unwrap/unwrapfile_src/unwrapfile.cc
+++ unwrap/unwrapfile_src/unwrapfile.cc
@@ -1,32 +1,18 @@
+// C++ includes
 #include <cstdlib>
-#include <stdio.h>
 #include <iostream>
 
-#include "processfiles.h"
-#include <sys/stat.h>
+// GeNN robotics includes
+#include "os/filesystem.h"
 
-using namespace std;
+// local includes
+#include "processfiles.h"
 
 enum FileType {
     skip,
     image,
     video
 };
-
-inline string getextension(string filepath)
-{
-    // find last dot
-    size_t dot = filepath.rfind('.');
-    if (dot == string::npos)// no extension
-        return "";
-
-    string ext = filepath.substr(dot);
-
-    // make extension uppercase
-    transform(ext.begin(), ext.end(), ext.begin(), ::toupper);
-
-    return ext;
-}
 
 int main(int argc, char** argv)
 {
@@ -38,7 +24,6 @@ int main(int argc, char** argv)
     FileType ftype[argc - 1];
     bool anyvideo;
     string ext;
-    struct stat sbuff;
     for (int i = 1; i < argc; i++) {
         if (strcmp(argv[i], "--no-sound") == 0) {
             copysound = false;
@@ -46,12 +31,12 @@ int main(int argc, char** argv)
             continue;
         }
 
-        if (stat(argv[i], &sbuff) != 0) {
+        if (!OS::FileSystem::fileExists(argv[i])) {
             cerr << "Error: File " << argv[i] << " does not exist" << endl;
             return 1;
         }
 
-        ext = getextension(argv[i]);
+        ext = OS::FileSystem::getExtension(argv[i]);
         if (ext == ".MP4") {
             anyvideo = true;
             ftype[i - 1] = video;
@@ -63,7 +48,7 @@ int main(int argc, char** argv)
         }
     }
 
-    if (copysound && anyvideo && stat(FFMPEG_PATH, &sbuff) != 0) {
+    if (copysound && anyvideo && !OS::FileSystem::fileExists(FFMPEG_PATH)) {
         cerr << "Warning: ffmpeg not found, sound will not be copied for videos" << endl;
         copysound = false;
     }
