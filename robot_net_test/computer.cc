add645eb0eb83215a8cc10771b67a9073a138108
Alex Dewar <a.dewar@sussex.ac.uk>
2018-05-26 09:25:46 +0100

    example programs for new robot API

---
+++ robot_net_test/computer.cc
@@ -0,0 +1,53 @@
+/*
+ * Example program for controlling a robot from a computer. The robot can be
+ * controlled with a joystick and the robot's camera stream will be displayed on
+ * screen. Press escape to exit.
+ */
+
+// C++ includes
+#include <iostream>
+
+// GeNN robotics includes
+#include "net/client.h"
+#include "robots/motor_joystick.h"
+#include "robots/motor_netsink.h"
+#include "video/display.h"
+#include "video/netsource.h"
+
+using namespace GeNNRobotics;
+
+int
+main(int argc, char **argv)
+{
+    std::string robotIP;
+    if (argc == 2) {
+        // get robot IP from commandline argument
+        robotIP = argv[1];
+    } else {
+        // get robot IP from terminal
+        std::cout << "Robot IP [127.0.0.1]: ";
+        std::getline(std::cin, robotIP);
+        if (robotIP.empty()) {
+            robotIP = "127.0.0.1";
+        }
+    }
+
+    // make connection to robot on default port
+    Net::Client client(robotIP);
+    client.runInBackground();
+
+    // read video stream from network
+    Video::NetSource video;
+    client.addHandler(video);
+
+    // transmit motor commands over network
+    Robots::MotorNetSink motor(client);
+
+    // add joystick for controlling Motor
+    Robots::MotorJoystick joystick(motor);
+    joystick.runInBackground();
+
+    // display video stream
+    Video::Display display(video);
+    display.run(); // run until terminated
+}
---
+++ robot_net_test/robot.cc
@@ -0,0 +1,50 @@
+/*
+ * Example program to be run on robot, or locally on a desktop as a server.
+ * 
+ * If you don't want to build in I2C support (e.g. if running on desktop) then
+ * make the program with:
+ *     NO_I2C_ROBOT=1 make
+ * 
+ * Use the corresponding "computer" program to connect to the server.
+ */
+
+// If we're compiling on Windows, we know we don't need I2C
+#ifdef _WIN32
+#define NO_I2C_ROBOT
+#endif
+
+// GeNN robotics includes
+#include "net/server.h"
+#ifdef NO_I2C_ROBOT
+#include "robots/motor_dummy.h"
+#else
+#include "robots/motor_i2c.h"
+#endif
+#include "video/panoramic.h"
+
+using namespace GeNNRobotics;
+
+int
+main()
+{
+    // listen for incoming connection on default port
+    Net::Server server;
+
+    // get default camera
+    auto cam = Video::getPanoramicCamera();
+    server.addHandler(*cam.get()); // stream camera over network
+
+#ifdef NO_I2C_ROBOT
+    // output motor commands to terminal
+    Robots::MotorDummy motor;
+#else
+    // use Arduino robot
+    Robots::MotorI2C motor;
+#endif
+
+    // let the motor object handle received motor commands
+    server.addHandler(motor);
+
+    // run server on main thread
+    server.run();
+}
