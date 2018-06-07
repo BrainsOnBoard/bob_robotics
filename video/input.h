0dbcd882f1c75800c5bb472fe940ddb1cdb79d3b
jamie <neworderofjamie@gmail.com>
2018-06-07 11:56:27 +0100

    start splitting network functionality from ``Input`` into ``NetSink``

--- video/input.h
+++ video/input.h
@@ -6,7 +6,6 @@
 #include <memory>
 #include <stdexcept>
 #include <string>
-#include <thread>
 #include <vector>
 
 // OpenCV
@@ -14,8 +13,6 @@
 
 // GeNNRobotics includes
 #include "../imgproc/opencv_unwrap_360.h"
-#include "../net/node.h"
-#include "../net/socket.h"
 #include "../third_party/path.h"
 
 namespace GeNNRobotics {
@@ -25,17 +22,6 @@ namespace Video {
 class Input
 {
 public:
-    virtual ~Input()
-    {
-        if (m_ThreadRunning) {
-            if (m_Thread.joinable()) {
-                m_Thread.join();
-            } else {
-                m_Thread.detach();
-            }
-        }
-    }
-
     template<typename... Ts>
     ImgProc::OpenCVUnwrap360 createDefaultUnwrapper(Ts &&... args)
     {
@@ -120,55 +106,8 @@ public:
         throw std::runtime_error("This camera's resolution cannot be changed at runtime");
     }
 
-    void streamToNetwork(Net::Node &node)
-    {
-        // handle incoming IMG commands
-        node.addCommandHandler("IMG", [this] (Net::Node &node, const Net::Command& command) {
-            onCommandReceived(node, command);
-        });
-    }
-
-protected:
-    /*
-     * Default constructor is protected to stop users directly creating objects
-     * of this class.
-     */
-    Input()
-    {}
-
 private:
-    std::thread m_Thread;
-    bool m_ThreadRunning = false;
-    cv::Mat m_IntermediateFrame;    
-
-    void onCommandReceived(Net::Node &node, const Net::Command &command)
-    {
-        if (command[1] != "START") {
-            throw Net::bad_command_error();
-        }
-
-        // ACK the command and tell client the camera resolution
-        cv::Size res = getOutputSize();
-        node.getSocket()->send("IMG PARAMS " + std::to_string(res.width) + " " +
-                               std::to_string(res.height) + " " +
-                               getCameraName() + "\n");
-
-        // start thread to transmit images in background
-        m_Thread = std::thread([this](Net::Node *n) { runImageSink(n); }, &node);
-        m_ThreadRunning = true;
-    }
-
-    void runImageSink(Net::Node *node)
-    {
-        cv::Mat frame;
-        std::vector<uchar> buffer;
-        Net::Socket *sock = node->getSocket();
-        while (m_ThreadRunning && readFrame(frame)) {
-            cv::imencode(".jpg", frame, buffer);
-            sock->send("IMG FRAME " + std::to_string(buffer.size()) + "\n");
-            sock->send(buffer.data(), buffer.size());
-        }
-    }
+    cv::Mat m_IntermediateFrame;
 }; // Input
 } // Video
 } // GeNNRobotics
---
+++ video/netsink.h
@@ -0,0 +1,99 @@
+#pragma once
+
+// OpenCV
+#include <opencv2/opencv.hpp>
+
+// GeNN robotics includes
+#include "../net/node.h"
+
+//----------------------------------------------------------------------------
+// GeNNRobotics::Video::NetSink
+//----------------------------------------------------------------------------
+// Object for sending video frames synchronously or asynchronously over network
+namespace GeNNRobotics {
+namespace Video {
+class NetSink
+{
+public:
+    // Create a NetSink for asynchronous operation
+    NetSink(Net::Node &node, Input &input)
+    :   m_Node(node), m_Input(&input), m_FrameSize(input.getOutputSize()), m_Name(input.getCameraName())
+    {
+        // handle incoming IMG commands
+        m_Node.addCommandHandler("IMG",
+                                 [this](Net::Node &node, const Net::Command& command)
+                                 {
+                                     onCommandReceivedAsync(command);
+                                 });
+    }
+
+    NetSink(Net::Node &node, const cv::Size &frameSize, const std::string &name)
+    :   m_Node(node), m_Input(nullptr), m_FrameSize(frameSize), m_Name(nam)
+    {
+    }
+
+    virtual ~NetSink()
+    {
+        if (m_ThreadRunning) {
+            if (m_Thread.joinable()) {
+                m_Thread.join();
+            }
+            else {
+                m_Thread.detach();
+            }
+        }
+    }
+    //----------------------------------------------------------------------------
+    // Public API
+    //----------------------------------------------------------------------------
+    void streamToNetwork()
+    {
+
+    }
+
+    void sendFrame(const cv::Mat &frame)
+    {
+        cv::imencode(".jpg", frame, m_Buffer);
+        m_Node->getSocket()->send("IMG FRAME " + std::to_string(m_Buffer.size()) + "\n");
+        m_Node->getSocket()->send(m_Buffer.data(), m_Buffer.size());
+    }
+
+private:
+    //----------------------------------------------------------------------------
+    // Private methods
+    //----------------------------------------------------------------------------
+    void onCommandReceivedAsync(const Net::Command &command)
+    {
+        if (command[1] != "START") {
+            throw Net::bad_command_error();
+        }
+
+        // ACK the command and tell client the camera resolution
+        m_Node.getSocket()->send("IMG PARAMS " + std::to_string(m_FrameSize.width) + " " +
+                                 std::to_string(m_FrameSize.height) + " " +
+                                 getCameraName() + "\n");
+
+        // start thread to transmit images in background
+        m_ThreadRunning = true;
+        m_Thread = std::thread(&NetSink::runAsync, this);
+    }
+
+    void runAsync()
+    {
+        cv::Mat frame;
+        while (m_ThreadRunning && m_Input->readFrame(frame)) {
+            sendFrame(frame);
+        }
+    }
+
+    //----------------------------------------------------------------------------
+    // Members
+    //----------------------------------------------------------------------------
+    Net::Node &m_Node;
+    Input *m_Input;
+    const cv::Size m_FrameSize;
+    const std::string m_Name;
+    std::thread m_Thread;
+    std::vector<uchar> m_Buffer;
+    bool m_ThreadRunning = false;
+};
