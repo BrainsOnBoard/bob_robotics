37e52ab36e2100688ae4e36ee9e438d84fd3adcf
Alex Dewar <a.dewar@sussex.ac.uk>
2018-06-07 17:52:56 +0100

    add basic Bebop test program to control drone with joystick

--- bebop_test/bebop_test.cc
+++ bebop_test/bebop_test.cc
@@ -1,6 +1,20 @@
 #include "robots/bebop.h"
 
+using namespace GeNNRobotics;
+
 int main()
 {
-    
+    const float deadZone = 0.25f;
+    HID::Joystick joystick(deadZone);
+
+    Robots::Bebop drone;
+    drone.connect();
+    drone.setFlightEventHandler([&joystick](bool takeoff) {
+        if (!takeoff) {
+            joystick.stop();
+        }
+    });
+    drone.addJoystick(joystick);
+
+    joystick.run();
 }
