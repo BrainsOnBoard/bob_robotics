589a721cc952b1802dff8c2fd155461b04efec40
Alex Dewar <a.dewar@sussex.ac.uk>
2018-06-11 16:41:54 +0100

    bebop_test: catch all exceptions so that we can make sure the drone lands

--- bebop_test/bebop_test.cc
+++ bebop_test/bebop_test.cc
@@ -1,5 +1,7 @@
 // C++ includes
 #include <chrono>
+#include <iostream>
+#include <stdexcept>
 #include <thread>
 
 // GeNN robotics includes
@@ -16,11 +18,20 @@ int main()
     Robots::Bebop drone;
     drone.connect();
 
-    drone.addJoystick(joystick, /*maxSpeed=*/0.25);
-    Video::Display display(drone.getVideoStream());
-    do {
-        if (!joystick.update() && !display.update()) {
-            std::this_thread::sleep_for(std::chrono::milliseconds(25));
-        }
-    } while (display.isOpen());
+    try {
+        drone.addJoystick(joystick, /*maxSpeed=*/0.25);
+        Video::Display display(drone.getVideoStream());
+        do {
+            if (!joystick.update() && !display.update()) {
+                std::this_thread::sleep_for(std::chrono::milliseconds(25));
+            }
+        } while (display.isOpen());
+    } catch (std::exception &e) {
+        /*
+         * We catch all exceptions, because otherwise drone's destructor won't
+         * be called and the drone might be left hovering (or flying towards a
+         * wall).
+         */
+        std::cerr << "ERROR: Uncaught exception: " << e.what() << std::endl;
+    }
 }
