5415a26855c36903f00828a9c56476f0b7ca5437
Alex Dewar <a.dewar@sussex.ac.uk>
2018-07-12 14:48:31 +0100

    Build ant_world_db_creator with debug symbols

--- ant_world_db_creator/build.sh
+++ ant_world_db_creator/build.sh
@@ -1,5 +1,5 @@
 pushd ../libantworld
 make
 popd
-g++ ant_world_db_creator.cc -std=c++14 -Wall -Wpedantic -L../libantworld -lantworld -lglfw -lGL -lGLU -lGLEW -pthread `pkg-config --cflags --libs opencv` -o ant_world_db_creator
-g++ ant_world_map_creator.cc -std=c++14 -Wall -Wpedantic -L../libantworld -lantworld -lglfw -lGL -lGLU -lGLEW -pthread `pkg-config --cflags --libs opencv` -o ant_world_map_creator
+g++ ant_world_db_creator.cc -std=c++14 -Wall -Wpedantic -g -L../libantworld -lantworld -lglfw -lGL -lGLU -lGLEW -pthread `pkg-config --cflags --libs opencv` -o ant_world_db_creator
+g++ ant_world_map_creator.cc -std=c++14 -Wall -Wpedantic -g -L../libantworld -lantworld -lglfw -lGL -lGLU -lGLEW -pthread `pkg-config --cflags --libs opencv` -o ant_world_map_creator
