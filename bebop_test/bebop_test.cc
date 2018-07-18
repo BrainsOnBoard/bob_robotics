46d73db2481be20f2c663995f60fbf58968090f2
Alex Dewar <a.dewar@sussex.ac.uk>
2018-07-18 11:22:50 +0100

    Print speed limits

--- bebop_test/bebop_test.cc
+++ bebop_test/bebop_test.cc
@@ -12,6 +12,34 @@
 using namespace BoBRobotics;
 using namespace std::literals;
 
+template <class T>
+void print(T limits)
+{
+    std::cout << " (limits: " << std::get<0>(limits) << ", "
+              << std::get<1>(limits) << ")" << std::endl;
+}
+
+void printSpeedLimits(Robots::Bebop &drone)
+{
+    // max tilt
+    auto maxTilt = drone.getMaximumTilt();
+    auto tiltLimits = drone.getTiltLimits();
+    std::cout << "Max tilt: " << maxTilt;
+    print(tiltLimits);
+    
+    // max yaw speed
+    auto maxYawSpeed = drone.getMaximumYawSpeed();
+    auto yawLimits = drone.getYawSpeedLimits();
+    std::cout << "Max yaw speed: " << maxYawSpeed;
+    print(yawLimits);
+
+    // max vertical speed
+    auto maxVertSpeed = drone.getMaximumVerticalSpeed();
+    auto vertSpeedLimits = drone.getVerticalSpeedLimits();
+    std::cout << "Max vertical speed: " << maxVertSpeed;
+    print(vertSpeedLimits);
+}
+
 int main()
 {
     HID::Joystick joystick(/*deadZone=*/0.25);
@@ -20,7 +48,9 @@ int main()
     drone.connect();
 
     try {
+        printSpeedLimits(drone);
         drone.addJoystick(joystick, /*maxSpeed=*/1.0);
+
         Video::Display display(drone.getVideoStream());
         do {
             if (!joystick.update() && !display.update()) {
