45d387bc693a5298b0727693ba7bd598e9eece43
Alex Dewar <a.dewar@sussex.ac.uk>
2018-07-23 23:26:33 +0100

    build_all_examples.sh: Clean projects (and libs) properly

--- Examples/build_all_examples.sh
+++ Examples/build_all_examples.sh
@@ -1,17 +1,25 @@
 #!/bin/sh
 
 basename=$(dirname "$0")
+
+echo Cleaning libs...
+for lib in "$basename"/../lib*; do
+    make -C "$lib" clean
+done
+
 goodcount=0
 projectcount=0
 for project in "$basename"/*_test; do
     echo -e "\e[34m========== BUILDING $project ==========\e[39m"
-    if make -C $project clean all -j `nproc`; then
+    cd "$project"
+    if make clean all -j `nproc`; then
         echo -e "\e[32m========== $project built successfully ==========\e[39m"
         (( goodcount++ ))
     else
         echo -e "\e[31m========== Building $project failed! ==========\e[39m"
     fi
     (( projectcount++ ))
+    cd ..
 done
 
 echo -e "\e[34m
