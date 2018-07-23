14da3a991c6dd83fd9a2f3b6ccfa44d61ed844d0
Alex Dewar <a.dewar@sussex.ac.uk>
2018-07-23 18:14:11 +0100

    Tweaks to script and makefiles

--- Examples/build_all_examples.sh
+++ Examples/build_all_examples.sh
@@ -5,7 +5,7 @@ goodcount=0
 projectcount=0
 for project in "$basename"/*_test; do
     echo -e "\e[34m========== BUILDING $project ==========\e[39m"
-    if make -C $project; then
+    if make -C $project clean all -j `nproc`; then
         echo -e "\e[32m========== $project built successfully ==========\e[39m"
         (( goodcount++ ))
     else
