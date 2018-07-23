cca08b10eda30413bf04549345232782669df034
Alex Dewar <a.dewar@sussex.ac.uk>
2018-07-23 17:59:38 +0100

    Add script to build all the example code

---
+++ Examples/build_all_examples.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+basename=$(dirname "$0")
+goodcount=0
+projectcount=0
+for project in "$basename"/*_test; do
+    echo -e "\e[34m========== BUILDING $project ==========\e[39m"
+    if make -C $project; then
+        echo -e "\e[32m========== $project built successfully ==========\e[39m"
+        (( goodcount++ ))
+    else
+        echo -e "\e[31m========== Building $project failed! ==========\e[39m"
+    fi
+    (( projectcount++ ))
+done
+
+echo -e "\e[34m
+BUILD COMPLETED
+[$goodcount/$projectcount of the projects built successfully]\e[39m"
