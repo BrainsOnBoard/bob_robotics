cd96d5741e7fefeaeed716ac8e9588bf55aad6be
Alex Dewar <a.dewar@sussex.ac.uk>
2018-07-24 12:45:48 +0100

    Only set CPP_STANDARD if not already set + add comments

--- common/flags.mk
+++ common/flags.mk
@@ -1,16 +1,21 @@
 # This makefile is just an easy way to standardise the compiler flags we're using
 # between different samples/mini-libraries.
 #
-# Just put "-include ../common/flags.mk" in your makefile.
+# Just put "include ../common/flags.mk" in your makefile.
 
-CPP_STANDARD = c++14
+# This variable is for GeNN (GeNN currently defaults to c++11, which won't work with some of our code)
+CPP_STANDARD ?= c++14
+
+# Build flags
 CXXFLAGS += -std=$(CPP_STANDARD) -Wall -Wpedantic -Wextra -MMD -MP
 ifdef DEBUG
 	CXXFLAGS += -g -O0 -DDEBUG
 endif
 
+# Linking flags (-lm and -lstdc++ needed for clang)
 LINK_FLAGS += -lm -lstdc++ -pthread
 
+# Build with OpenCV
 ifndef NO_OPENCV
 	CXXFLAGS += `pkg-config --cflags opencv`
 	LINK_FLAGS += `pkg-config --libs opencv`
