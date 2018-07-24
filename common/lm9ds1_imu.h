0fc5f6791bcf9fbf777fd52f97da118d7dd1cfb0
Alex Dewar <a.dewar@sussex.ac.uk>
2018-07-24 08:16:05 +0100

    Revert "Handle default cases in switch statements to appease gcc warning"
    This reverts commit b07cdfbcb4981ea68a2f68dcb328e72616c69d39.

--- common/lm9ds1_imu.h
+++ common/lm9ds1_imu.h
@@ -3,7 +3,6 @@
 // Standard C++ includes
 #include <algorithm>
 #include <iostream>
-#include <limits>
 
 // Standard C includes
 #include <cstdint>
@@ -767,8 +766,6 @@ private:
                 return 0.0175f;
             case GyroScale::DPS2000:
                 return 0.07f;
-            default:
-                return std::numeric_limits<float>::quiet_NaN();
         }
     }
     
@@ -783,8 +780,6 @@ private:
                 return 0.000244f;
             case AccelScale::G16:
                 return 0.000732f;
-            default:
-                return std::numeric_limits<float>::quiet_NaN();
         }
     }
     
@@ -799,8 +794,6 @@ private:
                 return 0.00043f;
             case MagnetoScale::GS16:
                 return 0.00058f;
-            default:
-                return std::numeric_limits<float>::quiet_NaN();
         }
     }
     
