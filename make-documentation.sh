3e550a542d68bd171a80bc3529c7413bb1beb9dc
Alex Dewar <a.dewar@sussex.ac.uk>
2018-07-26 09:09:28 +0100

    Put Doxyfile in root; output to documentation/ This way you can just run ``doxygen`` from the root folder to get documentation.

--- make-documentation.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# This is a simple script to build the source code documentation with doxygen.
-
-BRDIR="$(dirname $0)"
-if doxygen "$BRDIR/doxygen/Doxyfile"; then
-	echo -e "
-\e[32mDocumentation successfully built. You can now open $BRDIR/doxygen/html/index.html in your browser."
-fi
