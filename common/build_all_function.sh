1c888022a3571ecf140765e5c0802181b4302444
Alex Dewar <a.dewar@sussex.ac.uk>
2018-08-08 16:56:07 +0100

    Add build_all.sh for projects

--- common/build_all_function.sh
+++ common/build_all_function.sh
@@ -6,16 +6,26 @@ build_all() {
     projectcount=0
     skipcount=0
     for project in $@; do
+        # Skip if this isn't a directory
         if [ ! -d $project ]; then continue; fi
+
+        # Skip if the file .test_skip exists in folder
         if [ -f $project/.test_skip ]; then
             (( skipcount++ ))
             continue
         fi
+
+        # If MakefileTest exists, use that instead of Makefile
+        if [ -f $project/MakefileTest ]; then
+            makefile="-f MakefileTest"
+        else
+            makefile=
+        fi
         
         echo -e "\e[34m========== BUILDING $project ==========\e[39m"
         cd "$project"
-        make clean
-        if make all -j `nproc`; then
+        make clean $makefile
+        if make all $makefile -j `nproc`; then
             echo -e "\e[32m========== $project built successfully ==========\e[39m"
             (( goodcount++ ))
         else
