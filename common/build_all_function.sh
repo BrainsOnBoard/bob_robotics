16f8c5a51654451acb298437c7919617c7748c20
Alex Dewar <a.dewar@sussex.ac.uk>
2018-08-08 16:35:42 +0100

    Option to skip certain projects when testing

--- common/build_all_function.sh
+++ common/build_all_function.sh
@@ -4,8 +4,13 @@ build_all() {
     types=$1; shift
     goodcount=0
     projectcount=0
+    skipcount=0
     for project in $@; do
         if [ ! -d $project ]; then continue; fi
+        if [ -f $project/.test_skip ]; then
+            (( skipcount++ ))
+            continue
+        fi
         
         echo -e "\e[34m========== BUILDING $project ==========\e[39m"
         cd "$project"
@@ -23,6 +28,10 @@ build_all() {
     echo -e "\e[34m
     BUILD COMPLETED
     [$goodcount/$projectcount of the $types built successfully]\e[39m"
+
+    if [ $skipcount -gt 0 ]; then
+        echo $skipcount projects skipped.
+    fi
 
     if [ $goodcount -lt $projectcount ]; then
         exit 1
