682fa672978d4efdd35ae1af022d3363b4c2b2fe
Alex Dewar <a.dewar@sussex.ac.uk>
2018-08-14 14:59:52 +0100

    Try dropping PYTHON_BIN var from makefile to see if that helps Ok I'm really just clutching at straws here :-/

--- common/flags.mk
+++ common/flags.mk
@@ -28,11 +28,9 @@ ifndef NO_OPENCV
 endif
 
 ifdef WITH_MATPLOTLIBCPP
-	PYTHON_BIN 			 ?= python
-	PYTHON_CONFIG        := $(PYTHON_BIN)-config
-	PYTHON_VERSION       := $(shell $(PYTHON_BIN) --version 2>&1 | awk '{ print $$2 }' | sed -r 's/\.[0-9]+$$//g')
-	PYTHON_NUMPY_INCLUDE ?= $(shell find $$($(PYTHON_CONFIG) --prefix)/lib -type d -path "*/site-packages/numpy/core/include" | grep -m 1 $(PYTHON_VERSION))
+	PYTHON_VERSION       := $(shell python --version 2>&1 | awk '{ print $$2 }' | sed -r 's/\.[0-9]+$$//g')
+	PYTHON_NUMPY_INCLUDE ?= $(shell find $$(python-config --prefix)/lib -type d -path "*/site-packages/numpy/core/include" | grep -m 1 $(PYTHON_VERSION))
 
-	CXXFLAGS += $(shell $(PYTHON_CONFIG) --includes) -I$(PYTHON_NUMPY_INCLUDE)
-	LINK_FLAGS += $(shell $(PYTHON_CONFIG) --libs)
+	CXXFLAGS += $(shell python-config --includes) -I$(PYTHON_NUMPY_INCLUDE)
+	LINK_FLAGS += $(shell python-config --libs)
 endif
