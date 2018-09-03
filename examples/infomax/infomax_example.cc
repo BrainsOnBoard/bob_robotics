7808194b3120e8dc4edb4740f5686e548a61e57b
Alex Dewar <a.dewar@sussex.ac.uk>
2018-09-03 15:19:25 +0100

    Add infomax_example program for testing with "real" data

---
+++ examples/infomax/infomax_example.cc
@@ -0,0 +1,19 @@
+// Standard C++ includes
+#include <iostream>
+
+// OpenCV
+#include "opencv2/opencv.hpp"
+
+// BoB robotics includes
+#include "navigation/infomax.h"
+
+using namespace BoBRobotics::Navigation;
+
+int main()
+{
+    const cv::Size imSize { 25, 90 };
+    InfoMax<> infomax(imSize);
+
+    std::cout << "Training InfoMax network..." << std::endl;
+    infomax.loadSnapshots("../../tools/ant_world_db_creator/ant1_route1", true);
+}
