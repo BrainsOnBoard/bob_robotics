96b2c7e2d15a05889cc05879da58d9be3ba5d8cf
Alex Dewar <a.dewar@sussex.ac.uk>
2018-09-28 15:06:10 +0100

    Various tidy-ups

--- robots/gantry.h
+++ robots/gantry.h
@@ -4,6 +4,7 @@
 
 // Standard C++ includes
 #include <chrono>
+#include <sstream>
 #include <stdexcept>
 #include <string>
 #include <thread>
@@ -25,33 +26,17 @@ using namespace std::literals;
 using namespace units::math;
 using namespace units::length;
 
-class GantryError : public std::runtime_error
-{
-public:
-    GantryError(const std::string &msg, LRESULT err)
-      : std::runtime_error(msg + " (error no: "s + std::to_string(err) + ")"s)
-    {}
-};
-
 class Gantry
 {
 public:
-    enum Axis : BYTE
-    {
-        XAxis = 1,
-        YAxis = 2,
-        ZAxis = 4,
-        AllAxes = XAxis | YAxis | ZAxis
-    };
-
-    Gantry()
+    Gantry(BYTE boardId = 0)
+      : m_BoardId(0)
     {
         // Try to open PCI device
-        CheckError(P1240MotDevOpen(BoardId), "Could not open PCI card");
+        checkError(P1240MotDevOpen(m_BoardId), "Could not open PCI card");
 
-        if (isEmergencyButtonPressed()) {
-            throw std::runtime_error("Emergency button is pressed");
-        }
+        // Give error if emergency button pressed
+        checkEmergencyButton();
     }
 
     ~Gantry()
@@ -59,29 +44,32 @@ public:
         stopMoving();
 
         // Close PCI device
-        P1240MotDevClose(BoardId);
+        P1240MotDevClose(m_BoardId);
     }
 
     void home()
     {
         // Raise z-axis so we don't bump objects in arena
-        CheckError(P1240MotChgDV(BoardId, ZAxis, 1000), "Could not set z-axis motor speed");
-        CheckError(P1240MotCmove(BoardId, ZAxis, 0), "Could not move z-axis to limit");
-        waitToStopMoving(ZAxis);
+        checkError(P1240MotChgDV(m_BoardId, Z_Axis, 1000), "Could not set z-axis speed");
+        checkError(P1240MotCmove(m_BoardId, Z_Axis, 0 /*up*/), "Could not move z-axis to limit");
+        waitToStopMoving(Z_Axis);
 
         // Home along x- and y-axes
-        CheckError(P1240MotHome(BoardId, XAxis | YAxis), "Could not home x- and y- axes");
-        waitToStopMoving(XAxis | YAxis);
+        checkError(P1240MotHome(m_BoardId, XY_Axis), "Could not home x- and y- axes");
+        waitToStopMoving(XY_Axis);
 
         // Home z-axis
-        CheckError(P1240MotHome(BoardId, ZAxis), "Could not home z-axis");
-        waitToStopMoving(ZAxis);
+        checkError(P1240MotHome(m_BoardId, Z_Axis), "Could not home z-axis");
+        waitToStopMoving(Z_Axis);
+
+        // Give error if emergency button pressed
+        checkEmergencyButton();
     }
 
     bool isEmergencyButtonPressed()
     {
         DWORD ret;
-        CheckError(P1240MotRdReg(BoardId, 1, RR2, &ret), "Could not read emergency button flag");
+        checkError(P1240MotRdReg(m_BoardId, 1, RR2, &ret), "Could not read emergency button flag");
         return ret & 32;
     }
 
@@ -92,11 +80,11 @@ public:
 
         // Request position from card
         LONG val;
-        CheckError(P1240GetTheorecticalRegister(BoardId, XAxis, &val), "Could not get x position");
+        checkError(P1240GetTheorecticalRegister(m_BoardId, X_Axis, &val), "Could not get x position");
         pos[0] = (double) val / PulsesPerMillimetreX;
-        CheckError(P1240GetTheorecticalRegister(BoardId, YAxis, &val), "Could not get y position");
+        checkError(P1240GetTheorecticalRegister(m_BoardId, Y_Axis, &val), "Could not get y position");
         pos[1] = (double) val / PulsesPerMillimetreY;
-        CheckError(P1240GetTheorecticalRegister(BoardId, ZAxis, &val), "Could not get z position");
+        checkError(P1240GetTheorecticalRegister(m_BoardId, Z_Axis, &val), "Could not get z position");
         pos[2] = (double) val / PulsesPerMillimetreZ;
 
         return pos;
@@ -104,50 +92,74 @@ public:
 
     void setPosition(millimeter_t x, millimeter_t y, millimeter_t z)
     {
-        if (isEmergencyButtonPressed()) {
-            throw std::runtime_error("Emergency button is pressed");
-        }
-
-        P1240SetDrivingSpeed(BoardId, 0, 1000);
-        P1240SetStartSpeed(BoardId, 0, 125);
+        // Set params for line interpolation
+        checkError(P1240SetDrivingSpeed(m_BoardId, 0, 1000), "Could not set driving speed");
+        checkError(P1240SetStartSpeed(m_BoardId, 0, 125), "Could not set start speed");
 
         const LONG posX = (LONG) round(x * PulsesPerMillimetreX);
         const LONG posY = (LONG) round(y * PulsesPerMillimetreY);
         const LONG posZ = (LONG) round(z * PulsesPerMillimetreZ);
-        CheckError(P1240MotLine(BoardId, AllAxes, AllAxes, posX, posY, posZ, 0), "Could not move gantry");
+        checkError(P1240MotLine(m_BoardId, XYZ_Axis, XYZ_Axis, posX, posY, posZ, 0), "Could not move gantry");
     }
 
-    void stopMoving(BYTE axis = AllAxes) noexcept
+    void stopMoving(BYTE axis = XYZ_Axis) noexcept
     {
-		// To stop moving immediately, cf. gradually, set the 3rd arg to zero
-        P1240MotStop(BoardId, axis, axis);
+        // To stop moving immediately, cf. gradually, set the 3rd arg to zero
+        P1240MotStop(m_BoardId, axis, axis);
     }
 
-    bool isMoving(BYTE axis = AllAxes) noexcept
+    bool isMoving(BYTE axis = XYZ_Axis)
     {
         // Indicate whether specified axis/axes busy
-        return P1240MotAxisBusy(BoardId, axis);
-    }
-
-    void waitToStopMoving(BYTE axis = AllAxes) noexcept
-    {
-        // Repeatedly poll card to check whether gantry is moving
-        while (isMoving(axis)) {
-            std::this_thread::sleep_for(25ms);
+        LRESULT res = P1240MotAxisBusy(m_BoardId, axis);
+        switch (res) {
+        case AxisDrvBusy:
+        case AxisHomeBusy:
+        case AxisExtBusy:
+            return true;
+        case ERROR_SUCCESS:
+            return false;
+        default:
+            throwError(res, "Error while reading axis busy state");
         }
     }
 
+    void waitToStopMoving(BYTE axis = XYZ_Axis)
+    {
+        // Repeatedly poll card to check whether gantry is moving
+        while (isMoving(axis)) {
+            std::this_thread::sleep_for(10ms);
+        }
+
+        checkEmergencyButton();
+    }
+
 private:
-    static constexpr BYTE BoardId = 0;
+    BYTE m_BoardId;
     static constexpr auto PulsesPerMillimetreX = 7.49625 / 1_mm;
     static constexpr auto PulsesPerMillimetreY = 8.19672 / 1_mm;
     static constexpr auto PulsesPerMillimetreZ = 13.15789 / 1_mm;
 
-    static void CheckError(LRESULT err, const std::string &msg)
+    inline void checkEmergencyButton()
     {
-        if (err) {
-            throw GantryError(msg, err);
+        if (isEmergencyButtonPressed()) {
+            throw std::runtime_error("Gantry error: Emergency button is pressed");
         }
+    }
+
+    inline static void checkError(LRESULT res, const std::string &msg)
+    {
+        if (res) {
+            throwError(res, msg);
+        }
+    }
+
+    template<class T>
+    inline static void throwError(T res, const std::string &msg)
+    {
+        std::stringstream sstream;
+        sstream << "Gantry error (0x" << std::hex << res << "): " << msg;
+        throw std::runtime_error(sstream.str());
     }
 }; // Gantry
 } // Robots
