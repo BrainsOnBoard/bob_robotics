1a6a4003cc7146cc73c4a2638d5a2c9556191c4a
Alex Dewar <a.dewar@sussex.ac.uk>
2018-10-01 12:53:48 +0100

    Stream gantry camera in example

--- examples/gantry/gantry.cc
+++ examples/gantry/gantry.cc
@@ -1,29 +1,41 @@
+#include "os/windows_include.h"
+
 // Standard C++ includes
 #include <iostream>
 
 // BoB robotics includes
 #include "robots/gantry.h"
+#include "video/display.h"
+#include "video/opencvinput.h"
+
+using namespace BoBRobotics;
 
 int
 main()
 {
     try {
-        BoBRobotics::Robots::Gantry gantry;
+		// Object to interface with gantry robot
+        Robots::Gantry gantry;
 
+		// Return gantry to its home position
         std::cout << "Homing gantry...\n";
         gantry.home();
         std::cout << "Gantry homed.\n";
 
+		// Show gantry camera stream
+		Video::OpenCVInput cam(0, "gantry");
+        cam.setOutputSize({720, 576});
+        Video::Display display(cam, {576, 720});
+
+		// Move the gantry to the specified coordinates
         std::cout << "Moving gantry...\n";
         gantry.setPosition(500_mm, 500_mm, 0_mm);
-        gantry.waitToStopMoving();
+        do {
+            display.update();
+		} while (gantry.isMoving());
         std::cout << "Gantry moved.\n";
 
-		std::cout << "Moving gantry...\n";
-        gantry.setPosition(500_mm, 500_mm, 500_mm);
-        gantry.waitToStopMoving();
-        std::cout << "Gantry moved.\n";
-
+		// Print the gantry's current position
         const auto pos = gantry.getPosition<>();
         std::cout << "Gantry is at: " << pos[0] << ", " << pos[1] << ", "
                   << pos[2] << "\n";
