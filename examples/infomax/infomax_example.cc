44f13dbce02730e3e451196e575834546efbd2eb
Alex Dewar <a.dewar@sussex.ac.uk>
2018-10-02 15:27:26 +0100

    Update infomax_example for image database code changes

--- examples/infomax/infomax_example.cc
+++ examples/infomax/infomax_example.cc
@@ -9,8 +9,8 @@
 #include "opencv2/opencv.hpp"
 
 // BoB robotics includes
-#include "common/image_database.h"
 #include "common/timer.h"
+#include "navigation/image_database.h"
 #include "navigation/infomax.h"
 
 // Third-party includes
@@ -24,7 +24,7 @@ int
 main()
 {
     const cv::Size imSize{ 90, 25 };
-    const filesystem::path routePath = "../../tools/ant_world_db_creator/ant1_route1";
+    const ImageDatabase routeImages("../../tools/ant_world_db_creator/ant1_route1");
 
     std::cout << "Eigen is using " << Eigen::nbThreads() << " threads." << std::endl
               << std::endl;
@@ -36,15 +36,13 @@ main()
     {
         std::cout << "Training InfoMax network..." << std::endl;
         Timer<> trainingTimer{ "Network trained in: " };
-        for (unsigned i = 0; i < 810; i += 10) {
-            const filesystem::path filepath = routePath / getRouteDatabaseFilename(i);
-            infomax.VisualNavigationBase::train(filepath, true);
+        for (unsigned i = 0; i < routeImages.size(); i += 50) {
+            infomax.VisualNavigationBase::train(routeImages[i].loadGreyscale(), true);
         }
     }
 
     // Get output
-    const filesystem::path testPath = routePath / getRouteDatabaseFilename(10);
-    cv::Mat testImage = cv::imread(testPath.str(), CV_LOAD_IMAGE_GRAYSCALE);
+    cv::Mat testImage = routeImages[10].loadGreyscale();
     cv::resize(testImage, testImage, imSize);
     const auto result = infomax.getHeading(testImage);
     std::cout << std::endl << "Best heading: "
