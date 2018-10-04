BrainsOnBoard/bob_robotics

commit 92c1c091416eb3c7ac8183e1914d9e4ca0d5579c

Detemplatise ImageDatabase's constructor

navigation/image_database.h
@@ -44,9 +44,16 @@ public:
         }
     };
 
-    template<class T>
-    ImageDatabase(T &&databaseName)
-      : m_Path(std::forward<T>(databaseName))
+    ImageDatabase(const char *databasePath)
+      : m_Path(databasePath)
+    {}
+
+    ImageDatabase(const std::string &databasePath)
+      : m_Path(databasePath)
+    {}
+
+    ImageDatabase(const filesystem::path &databasePath)
+      : m_Path(databasePath)
     {
         const auto metadataPath = m_Path / "metadata.csv";
 

commit ffd7b6adf7b7ac3996c66fe7498fa98aa14721f3

ImageDatabase: fix constructors This fixes a bug introduced with 92c1c091416eb3c7ac8183e1914d9e4ca0d5579c

navigation/image_database.h
@@ -48,11 +48,11 @@ public:
     };
 
     ImageDatabase(const char *databasePath)
-      : m_Path(databasePath)
+      : ImageDatabase(filesystem::path(databasePath))
     {}
 
     ImageDatabase(const std::string &databasePath)
-      : m_Path(databasePath)
+      : ImageDatabase(filesystem::path(databasePath))
     {}
 
     ImageDatabase(const filesystem::path &databasePath)