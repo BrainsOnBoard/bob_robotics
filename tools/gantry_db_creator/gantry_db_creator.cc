35ba94f084ffac6a6fe60d1c28d84a0f30dba3e3
Alex Dewar <a.dewar@sussex.ac.uk>
2018-10-09 13:26:43 +0100

    Fix whitespace in gantry_db_creator.cc What is VS playing at?!

--- tools/gantry_db_creator/gantry_db_creator.cc
+++ tools/gantry_db_creator/gantry_db_creator.cc
@@ -25,18 +25,18 @@ main()
     const auto z = 200_mm;
 
     try {
-		// Open gantry and home it
+        // Open gantry and home it
         Robots::Gantry gantry;
         std::cout << "Homing gantry." << std::endl;
         gantry.raiseAndHome();
         std::cout << "Gantry homed." << std::endl;
 
-		// Get gantry camera
+        // Get gantry camera
         const cv::Size imSize(720, 576);
         Video::OpenCVInput cam(0, "gantry");
         cam.setOutputSize(imSize);
 
-		// Save images into a folder called gantry
+        // Save images into a folder called gantry
         Navigation::ImageDatabase database("gantry");
 
         auto x = xstart;
@@ -44,7 +44,7 @@ main()
         cv::Mat frame(imSize, CV_8UC3);
         bool increaseY = true;
         while (true) {
-			// While gantry is moving, poll for user keypress
+            // While gantry is moving, poll for user keypress
             gantry.setPosition(x, y, z);
             while (gantry.isMoving()) {
                 if (cv::waitKeyEx(1) == OS::KeyCodes::Escape) {
@@ -52,31 +52,31 @@ main()
                 }
             }
 
-			// Read frame (blocking) and display
-			while (!cam.readFrame(frame))
+            // Read frame (blocking) and display
+            while (!cam.readFrame(frame))
                 ;
             cv::imshow("Gantry camera", frame);
 
-			// Write to file with metadata
+            // Write to file with metadata
             database.addImage(frame, x, y, z, 0_deg, false);
 
-			// Get next position
+            // Get next position
             if (increaseY) {
                 if (y < yend) {
                     y += step;
                     continue;
-				}
+                }
             } else {
                 if (y > ystart) {
                     y -= step;
                     continue;
-				}
-			}
-			if (x < xend) {
+                }
+            }
+            if (x < xend) {
                 x += step;
-				increaseY = !increaseY;
+                increaseY = !increaseY;
             } else {
-				// ... otherwise we've finished
+                // ... otherwise we've finished
                 break;
             }
         }
