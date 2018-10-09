8c69333463f9303f63698fb6727c9a08f4957aa6
Alex Dewar <a.dewar@sussex.ac.uk>
2018-10-09 17:12:26 +0100

    Make PID class templatable so you can choose the data type

--- common/pid.h
+++ common/pid.h
@@ -7,44 +7,44 @@ namespace BoBRobotics {
 //------------------------------------------------------------------------
 // BoBRobotics::PID
 //------------------------------------------------------------------------
+template<typename T = float>
 class PID
 {
 public:
-    PID(float kp, float ki, float kd, float outMin, float outMax) 
+    PID(T kp, T ki, T kd, T outMin, T outMax)
     :   m_Intergral(0.0f), m_KP(kp), m_KI(ki), m_KD(kd), m_OutMin(outMin), m_OutMax(outMax)
-    {
-    }
+    {}
 
     //------------------------------------------------------------------------
     // Public API
     //------------------------------------------------------------------------
     // (Re-)initialise PID - use before output of PID is connected to plant
-    void initialise(float input, float output)
+    void initialise(T input, T output)
     {
         m_LastInput = input;
         m_Intergral = output;
         m_Intergral = std::min(m_OutMax, std::max(m_OutMin, m_Intergral));
     }
-    
+
     // Get output based on setpoint
-    float update(float setpoint, float input)
+    T update(T setpoint, T input)
     {
-        const float error = setpoint - input;
-        
+        const T error = setpoint - input;
+
         // Update integral term and clamp
         m_Intergral += (m_KI * error);
         m_Intergral = std::min(m_OutMax, std::max(m_OutMin, m_Intergral));
-        
+
         // Calculate derivative term
-        const float derivative = input - m_LastInput;
-        
+        const T derivative = input - m_LastInput;
+
         // Calculate output and clamp
-        float output = (m_KP * error) + m_Intergral - (m_KD * derivative);
+        T output = (m_KP * error) + m_Intergral - (m_KD * derivative);
         output = std::min(m_OutMax, std::max(m_OutMin, output));
-        
+
         // Update last input
         m_LastInput = input;
-        
+
         return output;
     }
 
@@ -53,18 +53,18 @@ private:
     // Members
     //------------------------------------------------------------------------
     // Last input (used for calculating derivative)
-    float m_LastInput;
-    
+    T m_LastInput;
+
     // Integral
-    float m_Intergral;
-    
+    T m_Intergral;
+
     // PID constants
-    float m_KP;
-    float m_KI;
-    float m_KD;
-    
+    T m_KP;
+    T m_KI;
+    T m_KD;
+
     // Output range
-    const float m_OutMin;
-    const float m_OutMax;
+    const T m_OutMin;
+    const T m_OutMax;
 };
 } // BoBRobotics
