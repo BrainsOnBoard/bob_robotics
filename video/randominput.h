453e139fd209b1db40a9a37551ac2b04f3e9252f
Alex Dewar <a.dewar@sussex.ac.uk>
2018-10-13 16:08:45 +0100

    Add a Video::Input which generates white noise

---
+++ video/randominput.h
@@ -0,0 +1,65 @@
+#pragma once
+
+// BoB robotics includes
+#include "input.h"
+
+// OpenCV
+#include <opencv2/opencv.hpp>
+
+// Standard C++ includes
+#include <random>
+
+namespace BoBRobotics {
+namespace Video {
+//! A Video::Input which generates white noise, for testing purposes
+class RandomInput : public Input {
+public:
+    RandomInput(const cv::Size &size)
+      : m_Size(size)
+      , m_Generator(m_RandomDevice())
+    {}
+
+    virtual bool readFrame(cv::Mat &outFrame) override
+    {
+        outFrame.create(m_Size, CV_8UC3);
+        fillRandom(outFrame.data, outFrame.data + m_Size.area() * 3);
+        return true;
+    }
+
+    virtual bool readGreyscaleFrame(cv::Mat &outFrame) override
+    {
+        outFrame.create(m_Size, CV_8UC1);
+        fillRandom(outFrame.data, outFrame.data + m_Size.area());
+        return true;
+    }
+
+    virtual const std::string getCameraName() const
+    {
+        return "random";
+    }
+
+    virtual cv::Size getOutputSize() const
+    {
+        return m_Size;
+    }
+
+    virtual void setOutputSize(const cv::Size &size)
+    {
+        m_Size = size;
+    }
+
+private:
+    cv::Size m_Size;
+    std::random_device m_RandomDevice;
+    std::mt19937 m_Generator;
+    std::uniform_int_distribution<uchar> m_Distribution;
+
+    void fillRandom(uchar *start, uchar *end)
+    {
+        for (uchar *p = start; p < end; p++) {
+            *p = m_Distribution(m_Generator);
+        }
+    }
+};
+} // Video
+} // BoBRobotics
