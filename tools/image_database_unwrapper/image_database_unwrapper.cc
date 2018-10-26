8e2ca5996a8147b616a605f50570c85055766d96
Alex Dewar <a.dewar@sussex.ac.uk>
2018-10-26 17:54:27 +0100

    Allow users to pick unwrapped size of images with command-line arguments

--- tools/image_database_unwrapper/image_database_unwrapper.cc
+++ tools/image_database_unwrapper/image_database_unwrapper.cc
@@ -1,12 +1,22 @@
 // BoB robotics includes
 #include "navigation/image_database.h"
 
+// Standard C++ includes
+#include <string>
+
 using namespace BoBRobotics;
 
 int
 main(int argc, char **argv)
 {
-    BOB_ASSERT(argc == 2);
+    // We must have a path + an optional image size
+    BOB_ASSERT(argc == 2 || argc == 4);
+
+    // Either parse image size from command line or use default
+    const cv::Size unwrapRes = argc == 4 ? cv::Size(std::stoi(argv[2]), std::stoi(argv[3]))
+                                         : cv::Size(720, 58);
+
+    // Unwrap image database
     Navigation::ImageDatabase database(argv[1]);
-    database.unwrap("unwrapped_" + database.getName(), { 720, 58 });
+    database.unwrap("unwrapped_" + database.getName(), unwrapRes);
 }
