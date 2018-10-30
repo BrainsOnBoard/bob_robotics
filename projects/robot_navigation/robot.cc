20165d79b0d7d128348c61dd64d42ac71d13fada
Alex Dewar <a.dewar@sussex.ac.uk>
2018-10-30 12:11:32 +0000

    We don't need net API here

--- projects/robot_navigation/robot.cc
+++ projects/robot_navigation/robot.cc
@@ -7,10 +7,8 @@
 #endif
 
 // BoB robotics includes
-#include "net/server.h"
-#include "os/net.h"
+#include "hid/joystick.h"
 #include "robots/tank.h"
-#include "video/netsink.h"
 #include "video/panoramic.h"
 
 #ifndef NO_I2C_ROBOT
@@ -30,15 +28,6 @@ main()
     try {
         auto cam = Video::getPanoramicCamera();
 
-        // Enable networking on Windows
-        OS::Net::WindowsNetworking net;
-
-        // Listen for incoming connection on default port
-        Net::Server server;
-
-        // Stream camera synchronously over network
-        Video::NetSink netSink(server, cam->getOutputSize(), cam->getCameraName());
-
 #ifdef NO_I2C_ROBOT
         // Output motor commands to terminal
         Robots::Tank tank;
@@ -47,29 +36,16 @@ main()
         Robots::Norbot tank;
 #endif
 
-        // Read motor commands from network
-        tank.readFromNetwork(server);
+        // Control robot with joystick
+        HID::Joystick joystick;
+        tank.addJoystick(joystick);
 
-        // Run server in background,, catching any exceptions for rethrowing
-        BackgroundException::enableCatching();
-        server.runInBackground();
-
-        // Send frames over network
-        cv::Mat frame;
-        while (true) {
-            // Rethrow any exceptions caught on background thread
-            BackgroundException::check();
-
-            // If there's a new frame, send it, else sleep
-            if (cam->readFrame(frame)) {
-                netSink.sendFrame(frame);
-            } else {
-                std::this_thread::sleep_for(25ms);
+        // Poll joystick
+        while (!joystick.isPressed(HID::JButton::B)) {
+            if (!joystick.update()) {
+                std::this_thread::sleep_for(10ms);
             }
         }
-    } catch (Net::SocketClosingError &) {
-        // The connection was closed on purpose: do nothing
-        std::cout << "Connection closed" << std::endl;
     } catch (std::exception &e) {
         std::cerr << "Uncaught exception: " << e.what() << std::endl;
         return 1;
