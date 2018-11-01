d153f266fc8e24c47c2a0fd7883651e8a936e63e
Alex Dewar <a.dewar@sussex.ac.uk>
2018-11-01 11:02:27 +0000

    Plot position of robot and objects

--- projects/robot_navigation/training.cc
+++ projects/robot_navigation/training.cc
@@ -7,6 +7,8 @@
 #endif
 
 // BoB robotics includes
+#include "common/plot_agent.h"
+#include "common/pose.h"
 #include "hid/joystick.h"
 #include "navigation/image_database.h"
 #include "robots/tank.h"
@@ -18,16 +20,27 @@
 #endif
 
 // Third-party includes
+#include "third_party/matplotlibcpp.h"
 #include "third_party/path.h"
+#include "third_party/units.h"
+
+// OpenCV
+#include <opencv2/opencv.hpp>
 
 // Standard C++ includes
+#include <algorithm>
 #include <chrono>
 #include <iostream>
 #include <string>
 #include <thread>
+#include <utility>
+#include <vector>
 
 using namespace BoBRobotics;
+using namespace units::length;
 using namespace std::literals;
+
+namespace plt = matplotlibcpp;
 
 Navigation::ImageDatabase
 createTrainingDatabase()
@@ -46,11 +59,28 @@ createTrainingDatabase()
 }
 
 int
-main()
+main(int argc, char **argv)
 {
     const cv::Size unwrapResolution{ 360, 75 };
+    const std::string objectsPath = argc < 2 ? "../../tools/vicon_arena_constructor/objects.yaml"
+                                             : argv[1];
 
     try {
+        std::vector<std::pair<std::vector<millimeter_t>, std::vector<millimeter_t>>> objects;
+        {
+            std::cout << "Loading object positions from " << objectsPath << "..." << std::endl;
+            cv::FileStorage fs(objectsPath, cv::FileStorage::READ);
+            std::vector<double> vertex(2);
+            for (auto objectNode : fs["objects"]) {
+                objects.emplace_back();
+                for (auto vertexNode : objectNode) {
+                    vertexNode >> vertex;
+                    objects.back().first.emplace_back(vertex[0]);
+                    objects.back().second.emplace_back(vertex[1]);
+                }
+            }
+        }
+
         Vicon::UDPClient<> vicon(51001);
 
         auto cam = Video::getPanoramicCamera();
@@ -87,14 +117,36 @@ main()
         bool trainingMode = false;
         cv::Mat frameRaw, frameUnwrapped;
         while (!joystick.isPressed(HID::JButton::B)) {
+            const auto pose = vicon.getObjectData(0);
+
+            plt::figure(1);
+            plt::clf();
+
+            // Plot objects
+            for (auto object : objects) {
+                std::vector<double> x, y;
+
+                const auto mm2double = [](millimeter_t mm) { return mm.value(); };
+                std::transform(object.first.cbegin(), object.first.cend(), std::back_inserter(x), mm2double);
+                std::transform(object.second.cbegin(), object.second.cend(), std::back_inserter(y), mm2double);
+                x.push_back(x[0]);
+                y.push_back(y[0]);
+
+                plt::plot(x, y);
+                plt::xlabel("x (mm)");
+                plt::ylabel("y (mm)");
+            }
+
+            // Plot position of robot
+            plotAgent(pose, { -1500, 1500 }, { -1500, 1500 });
+
             bool joystickUpdate = joystick.update();
             bool cameraUpdate = trainingMode && cam->readGreyscaleFrame(frameRaw);
             if (cameraUpdate) {
-                auto pose = vicon.getObjectData(0);
                 unwrapper.unwrap(frameRaw, frameUnwrapped);
                 route.record(pose.getPosition<>(), pose.getAttitude()[0], frameUnwrapped);
             } else if (!joystickUpdate) {
-                std::this_thread::sleep_for(10ms);
+                plt::pause(0.1);
             }
 
             if (joystick.isPressed(HID::JButton::Y)) {
