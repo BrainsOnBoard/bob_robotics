c598ee22d98ed1b922801a51af8416b9152e61c0
Alex Dewar <a.dewar@sussex.ac.uk>
2018-11-08 20:32:07 +0000

    Don't set robot size every loop

--- examples/robot_simulator/robot_simulator.cc
+++ examples/robot_simulator/robot_simulator.cc
@@ -15,9 +15,9 @@ using namespace units::literals;
 int main()
 {
     Robots::Simulator simulator;
+    simulator.setRobotSize(16.4_cm, 35_cm);
     while (!simulator.didQuit()) {
         simulator.simulationStep(1_mps, 100_deg_per_s, 10_ms);
-        simulator.setRobotSize(16.4_cm, 35_cm);
         std::this_thread::sleep_for(10ms);
     }
 }
