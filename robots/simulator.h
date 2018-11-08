0c450810d2e4161864cf5584b6479ae571be174d
Unknown <a.dewar@sussex.ac.uk>
2018-11-08 19:54:56 +0000

    Add car.bmp

--- robots/simulator.h
+++ robots/simulator.h
@@ -1,9 +1,18 @@
-// Simulator
-#pragma once;
-#include <SDL2/SDL.h>
+#pragma once
+
+// Third-party includes
 #include "../third_party/units.h"
+
+// SDL
+#include <SDL2/SDL.h>
+
+// Standard C includes
+#include <cstdlib>
+
+// Standard C++ includes
 #include <vector>
 #include <iostream>
+#include <string>
 
 #define PI 3.14159
 #define WINDOW_WIDTH 800.0
@@ -19,12 +28,12 @@ private:
 
     units::velocity::meters_per_second_t m_v;               // velocity v (translational velocity)
     units::angular_velocity::degrees_per_second_t m_w;      // velocity w (rotational velocity)
-    units::length::millimeter_t m_height;                   // height of the robot 
-    units::length::millimeter_t m_width;                    // width of the robot 
+    units::length::millimeter_t m_height;                   // height of the robot
+    units::length::millimeter_t m_width;                    // width of the robot
     units::angle::degree_t m_angle;                         // angle of the robot
     units::length::millimeter_t m_x;                        // position 'x' of the robot on the screen
     units::length::millimeter_t m_y;                        // position 'y' of the robot on the screen
-    units::time::millisecond_t m_dt;                        // delta time 
+    units::time::millisecond_t m_dt;                        // delta time
 
     float mouseClickX;
     float mouseClickY;
@@ -38,11 +47,11 @@ private:
     SDL_Rect dstrect;
     SDL_Rect rect_goal;
 
-    void updatePose(const units::velocity::meters_per_second_t v, 
-                    const units::angular_velocity::degrees_per_second_t w, 
-                    const units::time::millisecond_t dt) 
+    void updatePose(const units::velocity::meters_per_second_t v,
+                    const units::angular_velocity::degrees_per_second_t w,
+                    const units::time::millisecond_t dt)
     {
-        // set current velocities 
+        // set current velocities
         m_v = v;
         m_w = w;
 
@@ -56,7 +65,7 @@ private:
 
     //! draws the rectangle at the desired coordinates
     void drawRectangleAtCoordinates(SDL_Rect &rectangle, const float x, const float y) {
-        
+
         rectangle.x = x;
         rectangle.y = y;
         rectangle.w = 5;
@@ -67,31 +76,32 @@ private:
     }
 
 public:
-    Simulator() 
+    Simulator()
     {
         SDL_Init(SDL_INIT_VIDEO);
- 
+
         window = SDL_CreateWindow("Wheeled-robot simulator",
             SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WINDOW_WIDTH, WINDOW_HEIGHT, 0);
-    
+
         renderer = SDL_CreateRenderer(window, -1, 0);
-        image = SDL_LoadBMP("car.bmp");
+        const std::string imagePath = std::string(std::getenv("BOB_ROBOTICS_PATH")) + "/robots/car.bmp";
+        image = SDL_LoadBMP(imagePath.c_str());
         texture = SDL_CreateTextureFromSurface(renderer, image);
 
-        // Select the color for drawing. 
+        // Select the color for drawing.
         SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
 
         // Clear the entire screen to our selected color.
         SDL_RenderClear(renderer);
 
         // initial position and size of the robot car
-        dstrect = { 5, 5, 10, 13 };	  
+        dstrect = { 5, 5, 10, 13 };
 
         m_x = units::length::millimeter_t((float)dstrect.x *SCALE_FACTOR);
         m_y = units::length::millimeter_t((float)dstrect.y *SCALE_FACTOR);
         m_angle = 10_deg;
-        
-        // first goal is set to the middle of the window 
+
+        // first goal is set to the middle of the window
         mouseClickX = WINDOW_WIDTH/2;
         mouseClickY = WINDOW_HEIGHT/2;
     }
@@ -99,9 +109,9 @@ public:
     ~Simulator() {}
 
     //! sets the robot's size in millimeter
-    void setRobotSize(const units::length::millimeter_t height, 
+    void setRobotSize(const units::length::millimeter_t height,
                       const units::length::millimeter_t width
-                      ) 
+                      )
     {
         m_height = height;
         m_width = width;
@@ -115,20 +125,20 @@ public:
     }
 
     //! suimulates a step of the simulation with the provided velocities
-    void simulationStep(units::velocity::meters_per_second_t v, 
+    void simulationStep(units::velocity::meters_per_second_t v,
                         units::angular_velocity::degrees_per_second_t w,
                         units::time::millisecond_t delta_time
-                        ) 
+                        )
     {
          // Clear the entire screen to our selected color.
         SDL_RenderClear(renderer);
         SDL_PollEvent(&event);
 
-        // getting events 
+        // getting events
         switch (event.type) {
             case SDL_QUIT:
                 quit = true;
-                // freeing up resources 
+                // freeing up resources
                 SDL_DestroyTexture(texture);
                 SDL_FreeSurface(image);
                 SDL_DestroyRenderer(renderer);
@@ -138,32 +148,32 @@ public:
 
             case SDL_KEYDOWN:
                 switch (event.key.keysym.sym) {
-                    case SDLK_LEFT:   
+                    case SDLK_LEFT:
                         w = units::angular_velocity::degrees_per_second_t(-35.0);
                         break;
-                    case SDLK_RIGHT:  
+                    case SDLK_RIGHT:
                         w = units::angular_velocity::degrees_per_second_t( 35.0);
                         break;
-                    case SDLK_UP:     
+                    case SDLK_UP:
                         v = units::velocity::meters_per_second_t(5.0);
                         w = units::angular_velocity::degrees_per_second_t(1e-10);
                         break;
-                    case SDLK_DOWN:  
+                    case SDLK_DOWN:
                         v = units::velocity::meters_per_second_t(5.0);
                         w = units::angular_velocity::degrees_per_second_t(1e-10);
                         break;
                 }
-            
+
             case SDL_MOUSEBUTTONDOWN:
-                // If the left button was pressed. 
+                // If the left button was pressed.
                 if (event.button.button == SDL_BUTTON_LEFT) {
                     int x, y;
                     SDL_GetMouseState(&x, &y);
                     mouseClickX = x;
-                    mouseClickY = y;			
+                    mouseClickY = y;
                 }
-                       
-            default: 
+
+            default:
                 // adding a small value to avoid dividing by 0
                 if (v.value() == 0 && w.value() == 0) {
                     v = units::velocity::meters_per_second_t(1e-10);
@@ -175,33 +185,33 @@ public:
         updatePose(v, w, delta_time);
 
         // moving the robot
-        dstrect.x = m_x.value() /SCALE_FACTOR; 
+        dstrect.x = m_x.value() /SCALE_FACTOR;
         dstrect.y = m_y.value() /SCALE_FACTOR;
 
         // draw a rectangle at the goal position
         drawRectangleAtCoordinates(rect_goal , mouseClickX, mouseClickY);
 
-        // Select the color for drawing. 
+        // Select the color for drawing.
         SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-        
+
         // render texture with rotation
         SDL_RenderCopyEx( renderer, texture, NULL, &dstrect, m_angle.value(), NULL, SDL_FLIP_NONE );
-        
+
         SDL_RenderPresent(renderer);
     }
 
     //! returns the current position of the robot
-    std::vector<float> getCurrentPosition() 
-    { 
+    std::vector<float> getCurrentPosition()
+    {
         std::vector<float> position;
         position.push_back(dstrect.x);
         position.push_back(dstrect.y);
         position.push_back(m_angle.value());
         return position;
-    } 
+    }
 
     //! gets the position of the latest mouse click relative to the window
-    std::vector<float> getMouseClickLocation() 
+    std::vector<float> getMouseClickLocation()
     {
         std::vector<float> mouseClickCoordinates;
         mouseClickCoordinates.push_back(mouseClickX);
@@ -213,7 +223,7 @@ public:
     static void changePixelToMM(const float x_pos,
                          const float y_pos,
                          units::length::millimeter_t &x_mm,
-                         units::length::millimeter_t &y_mm) 
+                         units::length::millimeter_t &y_mm)
     {
         x_mm = units::length::millimeter_t(x_pos / SCALE_FACTOR);
         y_mm = units::length::millimeter_t(y_pos / SCALE_FACTOR);
