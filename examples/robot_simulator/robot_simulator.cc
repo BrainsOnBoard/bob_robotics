d4e2dcbd02a3ed502bb4f582e3ba774820b115b4
Unknown <a.dewar@sussex.ac.uk>
2018-11-08 19:55:15 +0000

    Add example for robot simulator

---
+++ examples/robot_simulator/robot_simulator.cc
@@ -0,0 +1,21 @@
+// BoB robotics includes
+#include "robots/simulator.h"
+
+// Third-party includes
+#include "third_party/units.h"
+
+// Standard C++ includes
+#include <chrono>
+#include <thread>
+
+using namespace std::literals;
+using namespace units::literals;
+
+int main()
+{
+    Simulator simulator;
+    while (true) {
+        simulator.simulationStep(1_mps, 100_deg_per_s, 10_ms);
+        std::this_thread::sleep_for(10ms);
+    }
+}
