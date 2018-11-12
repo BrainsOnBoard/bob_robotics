9b62cdbf3fa2373ece4b0d2e96b362a712db77d7
Unknown <a.dewar@sussex.ac.uk>
2018-11-12 13:46:25 +0000

    Do plotting on another thread

--- projects/agent_navigation/navigation_common.h
+++ projects/agent_navigation/navigation_common.h
@@ -22,8 +22,10 @@
 
 // Standard C++ includes
 #include <algorithm>
+#include <atomic>
 #include <chrono>
 #include <iostream>
+#include <mutex>
 #include <string>
 #include <thread>
 #include <utility>
@@ -38,7 +40,88 @@ using namespace std::literals;
 
 namespace plt = matplotlibcpp;
 
-class EggTimer {
+using ObjectType = std::pair<std::vector<millimeter_t>, std::vector<millimeter_t>>;
+
+template<typename PoseGetterType, typename LengthUnit>
+class Plotter
+{
+public:
+    Plotter(const std::vector<ObjectType> &objects, PoseGetterType &poseGetter,
+            const Vector2<LengthUnit> xlim, const Vector2<LengthUnit> ylim)
+      : m_Objects(objects)
+      , m_PoseGetter(poseGetter)
+      , m_Xlim(xlim)
+      , m_Ylim(ylim)
+      , m_Thread(&Plotter::run, this)
+      , m_Running(true)
+      , m_Finished(false)
+    {}
+
+    ~Plotter()
+    {
+        m_Running = false;
+        if (m_Thread.joinable()) {
+            m_Thread.join();
+        }
+    }
+
+    void setTitle(const std::string& title)
+    {
+        std::lock_guard<std::mutex> guard(m_TitleMutex);
+        m_Title = title;
+    }
+
+    bool isOpen() const { return m_Running; }
+
+private:
+    const std::vector<ObjectType> &m_Objects;
+    PoseGetterType &m_PoseGetter;
+    std::string m_Title;
+    const Vector2<LengthUnit> m_Xlim, m_Ylim;
+    std::thread m_Thread;
+    std::mutex m_TitleMutex;
+    std::atomic_bool m_Running;
+    std::atomic_bool m_Finished;
+
+    void run()
+    {
+        do {
+            plt::figure(1);
+            plt::clf();
+
+            // Plot objects
+            // plt::subplot(2, 1, 1);
+            for (auto object : m_Objects) {
+                std::vector<double> x, y;
+
+                const auto mm2double = [](millimeter_t mm) { return mm.value(); };
+                std::transform(object.first.cbegin(), object.first.cend(), std::back_inserter(x), mm2double);
+                std::transform(object.second.cbegin(), object.second.cend(), std::back_inserter(y), mm2double);
+                x.push_back(x[0]);
+                y.push_back(y[0]);
+
+                plt::plot(x, y);
+            }
+
+            // Plot position of robot
+            plotAgent(m_PoseGetter, m_Xlim[0], m_Xlim[1], m_Ylim[0], m_Ylim[1]);
+
+            // Set title
+            {
+                std::lock_guard<std::mutex> guard(m_TitleMutex);
+                plt::title(m_Title);
+            }
+
+            plt::pause(0.1);
+        } while (m_Running && plt::fignum_exists(1));
+        plt::close();
+        m_Running = false;
+        m_Finished = true;
+    }
+}; // Plotter
+
+class EggTimer
+{
 private:
     using TimeType = std::chrono::time_point<std::chrono::high_resolution_clock, std::chrono::nanoseconds>;
     TimeType m_StartTime;
@@ -109,24 +192,26 @@ public:
         std::cout << "Stopping training (" << size() << " stored)" << std::endl;
     }
 
-    Navigation::ImageDatabase::RouteRecorder &getRouteRecorder() { return m_Recorder; }
+    Navigation::ImageDatabase::RouteRecorder &getRouteRecorder()
+    {
+        return m_Recorder;
+    }
 
 private:
     Navigation::ImageDatabase::RouteRecorder m_Recorder;
 };
 
-using ObjectType = std::pair<std::vector<millimeter_t>, std::vector<millimeter_t>>;
-
 template<typename PoseGetterType, typename LengthUnit>
-void runNavigation(Robots::Robot &robot,
-                   PoseGetterType &poseGetter,
-                   const float forwardSpeed,
-                   Video::Input &videoInput,
-                   const LengthUnit xLower,
-                   const LengthUnit xUpper,
-                   const LengthUnit yLower,
-                   const LengthUnit yUpper,
-                   const std::vector<ObjectType> &objects = {})
+void
+runNavigation(Robots::Robot &robot,
+              PoseGetterType &poseGetter,
+              const float forwardSpeed,
+              Video::Input &videoInput,
+              const LengthUnit xLower,
+              const LengthUnit xUpper,
+              const LengthUnit yLower,
+              const LengthUnit yUpper,
+              const std::vector<ObjectType> &objects = {})
 {
     const auto robotTurnSpeed = robot.getMaximumTurnSpeed();
 
@@ -135,7 +220,7 @@ void runNavigation(Robots::Robot &robot,
 
     // Count number of routes
     int numRoutes = 0;
-    while(getRoutePath(++numRoutes).exists())
+    while (getRoutePath(++numRoutes).exists())
         ;
     std::unique_ptr<TrainingDatabase> trainingDatabase;
 
@@ -180,10 +265,9 @@ void runNavigation(Robots::Robot &robot,
 
                 std::cout << "Starting testing" << std::endl;
                 if (pm.getNumSnapshots() == 0) {
-                    const int imageStep = 1;
                     const Navigation::ImageDatabase database(getRoutePath(numRoutes - 1));
-                    std::cout << "Loading " << database.size() / imageStep << " images" << std::endl;
-                    pm.trainRoute(database, imageStep);
+                    pm.trainRoute(database, /*imageStep=*/10);
+                    std::cout << pm.getNumSnapshots() << " snapshots loaded." << std::endl;
                 }
                 testing = true;
 
@@ -195,38 +279,21 @@ void runNavigation(Robots::Robot &robot,
         }
     });
 
+    Plotter<PoseGetterType, LengthUnit> plotter(objects, poseGetter,
+                                                { xLower, xUpper }, { yLower, yUpper });
+
     cv::Mat frame;
-    std::map<std::string, std::string> imshowKeywords;
-    imshowKeywords["cmap"] = "gray";
+    // std::map<std::string, std::string> imshowKeywords{ { "cmap", "gray" } };
     do {
         catcher.check();
 
-        plt::figure(1);
-        plt::clf();
-
-        // Plot objects
-        plt::subplot(2, 1, 1);
-        for (auto object : objects) {
-            std::vector<double> x, y;
-
-            const auto mm2double = [](millimeter_t mm) { return mm.value(); };
-            std::transform(object.first.cbegin(), object.first.cend(), std::back_inserter(x), mm2double);
-            std::transform(object.second.cbegin(), object.second.cend(), std::back_inserter(y), mm2double);
-            x.push_back(x[0]);
-            y.push_back(y[0]);
-
-            plt::plot(x, y);
-        }
-
-        // Plot position of robot
-        plotAgent(poseGetter, xLower, xUpper, yLower, yUpper);
         if (trainingDatabase) {
-            plt::title("Training");
+            plotter.setTitle("Training");
         } else if (testing) {
-            plt::title("Testing");
+            plotter.setTitle("Testing");
         }
 
-        bool joystickUpdate = joystick.update();
+        joystick.update();
         if (turnTimer.running()) {
             if (turnTimer.finished()) {
                 robot.moveForward(forwardSpeed);
@@ -237,9 +304,8 @@ void runNavigation(Robots::Robot &robot,
         }
 
         if (videoInput.readGreyscaleFrame(frame)) {
-            plt::subplot(2, 1, 2);
-            plt::imshow(frame, imshowKeywords);
-            plt::pause(0.1);
+            // plt::subplot(2, 1, 2);
+            // plt::imshow(frame, imshowKeywords);
 
             if (trainingDatabase) {
                 const auto pose = poseGetter.template getPose<millimeter_t, degree_t>();
@@ -252,10 +318,6 @@ void runNavigation(Robots::Robot &robot,
                 turnTimer.start(heading / robotTurnSpeed);
                 robot.turnOnTheSpot(heading < 0_deg ? 1.f : -1.f);
             }
-        } else if (!joystickUpdate) {
-            plt::pause(0.1);
         }
-    } while (!joystick.isPressed(HID::JButton::B) && plt::fignum_exists(1));
-
-    plt::close();
+    } while (!joystick.isPressed(HID::JButton::B) && plotter.isOpen());
 }
