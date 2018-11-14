c58ea48a352bc508b5c8777dc8d0948f0773bed4
Alex Dewar <a.dewar@sussex.ac.uk>
2018-11-14 14:06:56 +0000

    Tidy robot_positioner

--- examples/robot_positioner/robot_positioner.cc
+++ examples/robot_positioner/robot_positioner.cc
@@ -1,26 +1,37 @@
-// C++ includes
-#include <chrono>
+// BoB robotics includes
+#include "robots/norbot.h"
+#include "robots/robot_positioner.h"
+#include "vicon/udp.h"
+
+// Third-party includes
+#include "third_party/units.h"
+
+// Standard C includes
 #include <cstdlib>
+
+// Standard C++ includes
+#include <chrono>
 #include <iostream>
+#include <thread>
 
-#include "../../robots/norbot.h"
-#include "../../robots/robot_positioner.h"
-#include "../../third_party/units.h"
-#include "viconDataStreamer.h"
-
-// a small example program demonstratings the usage of 'robotPositioner.h'
-
+using namespace BoBRobotics;
 using namespace units::length;
+using namespace units::literals;
 using namespace units::angle;
 using namespace units::angular_velocity;
 using namespace units::velocity;
+using namespace std::literals;
 
 int
 main()
 {
+    // Connect to Vicon system
+    Vicon::UDPClient<> vicon(51001);
+    while (vicon.getNumObjects() == 0) {
+        std::this_thread::sleep_for(1s);
+        std::cout << "Waiting for object" << std::endl;
+    }
 
-    //setup the vicon streamer
-    ViconDataStreamer streamer(51001, "192.168.1.100", "c:\\users\\ad374\\Desktop", 3003);
     BoBRobotics::Robots::Norbot bot;
 
     // setup parameters
@@ -44,19 +55,16 @@ main()
             max_turning_velocity);
 
     // set goal pose
-    robp.setGoalPose(0_mm, 0_mm, 15_deg);
+    constexpr millimeter_t goalX = 0_mm, goalY = 0_mm;
+    constexpr degree_t goalHeading = 15_deg;
+    std::cout << "Goal: (" << goalX << ", " << goalY << ") at " << goalHeading << std::endl;
+    robp.setGoalPose(goalX, goalY, goalHeading);
 
     while (1) {
-        BoBRobotics::Vector3<millimeter_t> pos_robot = streamer.getTranslation();
-        BoBRobotics::Vector3<radian_t> rot_robot = streamer.getRotation();
-
-        millimeter_t posx = pos_robot[0];
-        millimeter_t posy = pos_robot[1];
-        degree_t heading = rot_robot[0];
-
-        meters_per_second_t v;
-        degrees_per_second_t w;
-        robp.updateMotors(bot, posx, posy, heading);
+        const auto objectData = vicon.getObjectData(0);
+        const Vector3<millimeter_t> position = objectData.getPosition();
+        const Vector3<radian_t> attitude = objectData.getAttitude();
+        robp.updateMotors(bot, position[0], position[1], attitude[0]);
     }
 
     return 0;
