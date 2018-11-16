77f946ee0d6ababf69817e8d78b8eebf5db04606
Alex Dewar <a.dewar@sussex.ac.uk>
2018-11-16 18:08:04 +0000

    SimulatedTank example: drive with two sticks

--- examples/simulated_tank/simulated_tank.cc
+++ examples/simulated_tank/simulated_tank.cc
@@ -10,21 +10,36 @@
 
 using namespace BoBRobotics;
 using namespace units::literals;
+namespace plt = matplotlibcpp;
 
 int
 bob_main(int, char **)
 {
     Robots::SimulatedTank<units::length::millimeter_t, units::angle::radian_t> robot(0.1_mps, 34_mm);
-    HID::Joystick joystick;
-    robot.addJoystick(joystick);
+    HID::Joystick joystick(0.25f);
 
+    joystick.addHandler([&robot](HID::JButton button, bool pressed) {
+        if (pressed && button == HID::JButton::Start) {
+            robot.setPose({ 0_m, 0_m, 0_rad });
+            return true;
+        } else {
+            return false;
+        }
+    });
+
+    // robot.drive(0.1_mps, 50_deg_per_s);
     do {
         joystick.update();
-        plotAgent(robot, { -2000, 2000 }, { -2000, 2000 });
-        matplotlibcpp::pause(0.1);
-    } while (!joystick.isPressed(HID::JButton::B));
 
-    matplotlibcpp::close();
+        robot.tank(-joystick.getState(HID::JAxis::LeftStickVertical),
+                   -joystick.getState(HID::JAxis::RightStickVertical));
+
+        constexpr int lim = 250;
+        plotAgent(robot, { -lim, lim }, { -lim, lim });
+        plt::pause(0.1);
+    } while (!joystick.isPressed(HID::JButton::B) && plt::fignum_exists(1));
+
+    plt::close();
 
     return EXIT_SUCCESS;
 }
