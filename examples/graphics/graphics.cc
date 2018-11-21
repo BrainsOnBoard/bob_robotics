54b8cb3b4256d149e481f388df14ca052ddd614d
Alex Dewar <a.dewar@sussex.ac.uk>
2018-11-21 16:47:00 +0000

    Load objects from yaml file and render in arena

--- examples/graphics/graphics.cc
+++ examples/graphics/graphics.cc
@@ -1,5 +1,14 @@
 // BoB robotics includes
+#include "common/pose.h"
+#include "robots/simulated_tank.h"
 #include "viz/agent_renderer.h"
+
+// Third-party includes
+#include "third_party/path.h"
+#include "third_party/units.h"
+
+// OpenCV
+#include <opencv2/opencv.hpp>
 
 // Standard C++ includes
 #include <chrono>
@@ -7,11 +16,36 @@
 
 using namespace BoBRobotics;
 using namespace std::literals;
+using namespace units::length;
+
+auto
+loadObjects(const filesystem::path &objectsPath)
+{
+    std::vector<std::vector<Vector2<millimeter_t>>> objects;
+
+    std::cout << "Loading object positions from " << objectsPath << "..." << std::endl;
+    cv::FileStorage fs(objectsPath.str(), cv::FileStorage::READ);
+    std::vector<double> vertex(2);
+    for (auto objectNode : fs["objects"]) {
+        objects.emplace_back();
+        for (auto vertexNode : objectNode) {
+            vertexNode >> vertex;
+            objects.back().emplace_back(Vector2<millimeter_t>{ millimeter_t{ vertex[0] },
+                                                               millimeter_t{ vertex[1] } });
+        }
+    }
+
+    return objects;
+}
 
 int
-main()
+main(int, char **argv)
 {
-    Viz::AgentRenderer renderer;
+    const auto objectsPath = filesystem::path(argv[0]).parent_path() / "objects.yaml";
+    const auto objects = loadObjects(objectsPath);
+
+    Viz::AgentRenderer<> renderer;
+    renderer.addObjects(objects);
 
     // run the program as long as the window is open
     while (renderer.isOpen()) {
--- viz/agent_renderer.h
+++ viz/agent_renderer.h
@@ -1,21 +1,32 @@
 #pragma once
 
+// BoB robotics includes
+#include "../common/assert.h"
+#include "../common/pose.h"
+
+// Third-party includes
+#include "../third_party/units.h"
+
 // SFML
 #include <SFML/Graphics.hpp>
 
 namespace BoBRobotics {
 namespace Viz {
+using namespace units::literals;
+
+template<typename LengthUnit = units::length::millimeter_t>
 class AgentRenderer
 {
 public:
     static constexpr int WindowWidth = 800, WindowHeight = 600;
 
-    AgentRenderer()
+    AgentRenderer(const LengthUnit arenaHeight = 3.2_m)
       : m_Window(sf::VideoMode(WindowWidth, WindowHeight),
                  "BoB robotics",
                  sf::Style::Titlebar | sf::Style::Close)
       , m_OriginLineHorizontal({ OriginLineLength, OriginLineThickness })
       , m_OriginLineVertical({ OriginLineThickness, OriginLineLength })
+      , m_UnitPerPixel(arenaHeight / WindowHeight)
     {
         m_Window.setVerticalSyncEnabled(true);
 
@@ -44,6 +55,11 @@ public:
         m_Window.draw(m_OriginLineHorizontal);
         m_Window.draw(m_OriginLineVertical);
 
+        // Draw objects
+        for (auto &object : m_Objects) {
+            m_Window.draw(object);
+        }
+
         // Swap buffers
         m_Window.display();
     }
@@ -53,16 +69,29 @@ public:
         return m_Window.isOpen();
     }
 
+    void addObjects(const std::vector<std::vector<Vector2<LengthUnit>>> &objects)
+    {
+        BOB_ASSERT(m_Objects.empty());
+        m_Objects.reserve(objects.size());
+
+        for (auto &object : objects) {
+            m_Objects.emplace_back();
+            m_Objects.back().setPointCount(object.size());
+            m_Objects.back().setFillColor(sf::Color::Black);
+            for (size_t i = 0; i < object.size(); i++) {
+                m_Objects.back().setPoint(i, { static_cast<float>((object[i][0] / m_UnitPerPixel).value()) + (WindowWidth / 2),
+                                               -static_cast<float>((object[i][1] / m_UnitPerPixel).value()) + (WindowHeight / 2)});
+            }
+        }
+    }
+
 private:
     sf::RenderWindow m_Window;
     sf::RectangleShape m_OriginLineHorizontal, m_OriginLineVertical;
+    std::vector<sf::ConvexShape> m_Objects;
+    const LengthUnit m_UnitPerPixel;
 
     static constexpr float OriginLineThickness = 3.f, OriginLineLength = 20.f;
 }; // AgentRenderer
-
-#ifndef NO_HEADER_DEFINITIONS
-constexpr int AgentRenderer::WindowWidth, AgentRenderer::WindowHeight;
-constexpr float AgentRenderer::OriginLineThickness, AgentRenderer::OriginLineLength;
-#endif
 } // Viz
 } // BobRobotics
