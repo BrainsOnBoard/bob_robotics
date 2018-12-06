29fdea3577d78737b8a09bcc95962d857d07ef80
Alex Cope <ac@ac>
2018-12-06 09:50:05 +0000

    Added delay to make the connection over serial to the betaflight controller work

--- examples/betaflight/betaflight_test.cc
+++ examples/betaflight/betaflight_test.cc
@@ -2,23 +2,34 @@
 
 #include "redirect_net.h"
 
+// C++ includes
+#include <chrono>
+#include <cstdlib>
+#include <iostream>
+#include <thread>
+
+//using namespace BoBRobotics::Vicon;
+using namespace std::literals;
+
 int main(int argc, char *argv[]){
     const std::string device = (argc>1) ? std::string(argv[1]) : "/dev/ttyS3";
     const size_t baudrate = (argc>2) ? std::stoul(argv[2]) : 115200;
 
 	BoBRobotics::Robots::betaflight_uav my_drone(device, baudrate);
 
+  std::this_thread::sleep_for(1s);
+
 	my_drone.subscribe();
-	
+
 	m_Port = 50091;
     m_Send_Port = 50101;
-	
+
 	// set up the networking code needed to receive images:
 	//if (!setupSockets()) {
 	//	// error
 	//	return -1;
 	//}
-	
+
 	int count = 0;
 	int rx_timeout = 200;
 
@@ -27,9 +38,9 @@ int main(int argc, char *argv[]){
             std::cout << my_drone.getArmStateAsString() << std::endl;
         }
         std::cout << "V = " << my_drone.getVoltage() << std::endl;
-        
+
         ++count;
-        if (count > 3000 && count < 3100) {
+      /*  if (count > 3000 && count < 3100) {
             my_drone.armDrone();
         }
         if (count > 20000) {
@@ -38,23 +49,13 @@ int main(int argc, char *argv[]){
         if (count > 6000) {
             my_drone.setVerticalSpeed(-1.0);
         }
-        std::cout << count << std::endl; 
-        
-        //readFrame();
-		cmds commands = getMotorCommands();
-		if( commands.have_new) {
-		    //std::cout << "here" << std::endl;
-		    my_drone.setRoll(commands.roll);
-		    my_drone.setPitch(commands.pitch);
-		    my_drone.setVerticalSpeed(commands.throttle);
-		    my_drone.setYawSpeed(commands.yaw);
-		 //robot.omni2D((float) commands.fb, (float) commands.lr, (float) commands.yaw);
-		} else {
-		    //--rx_timeout;
-		}
-		if (rx_timeout > 0) {
-		    my_drone.sendCommands();
-		}
+        std::cout << count << std::endl; */
+
+        std::this_thread::sleep_for(10ms);
+
+    		if (rx_timeout > 0) {
+    		    my_drone.sendCommands();
+    		}
     }
 
     // Ctrl+C to quit
--- robots/betaflight_uav.h
+++ robots/betaflight_uav.h
@@ -52,7 +52,7 @@ namespace BetaFlight {
 		new_flag.set_arm_flag(16, std::string("A Black Sheep Telemetry device (TBS Core Pro for example) disarmed and is preventing arming"), arm_flags);
 		new_flag.set_arm_flag(17, std::string("MSP connection is active, probably via Betaflight Configurator"), arm_flags);
 		new_flag.set_arm_flag(18, std::string("Arm switch is in an unsafe position"), arm_flags);
-	
+
 	}
 
 	struct MyIdent : public msp::Request {
@@ -91,13 +91,13 @@ namespace BetaFlight {
 				currentArmFlags.append(af.msg);
 				    currentArmFlags.append(std::string(" (Error number ") + std::to_string(af.err_num) + std::string("), "));
 					//std::cout <<  af.msg << " (Error number " << af.err_num << "), ";
-				} 
+				}
 			}
 			//std::cout << std::endl;
 
 		}
 		void onAnalog(const Analog &anog){
-			std::cout << "Battery voltage level: " << anog.vbat << " V." << std::endl;
+			//std::cout << "Battery voltage level: " << anog.vbat << " V." << std::endl;
 			currentVoltage = anog.vbat;
 			//std::cout << "Power Meter Summery: " << anog.powerMeterSum << std::endl;
 			//std::cout << "Received Signal Strength Indication: " << anog.rssi << std::endl;
@@ -112,20 +112,20 @@ class betaflight_uav : public UAV {
 	public:
 
 		betaflight_uav(std::string device, int baud) :
-			m_Device(device), 
+			m_Device(device),
 			m_Baud(baud)
 		{
-			
+
 			BetaFlight::setup_arm_flags();
-			
+
 			m_Fcu = new fcu::FlightController(m_Device, m_Baud);
 			m_Fcu->initialise();
 
-		}		
+		}
 
 		void subscribe() {
-			m_Fcu->subscribe(&BetaFlight::Callbacks::onIdent, &m_Cbs, 1);
-			m_Fcu->subscribe(&BetaFlight::Callbacks::onAnalog, &m_Cbs, 5);
+			m_Fcu->subscribe(&BetaFlight::Callbacks::onIdent, &m_Cbs, 0.1);
+			m_Fcu->subscribe(&BetaFlight::Callbacks::onAnalog, &m_Cbs, 0.5);
 		}
 
 		~betaflight_uav() {
@@ -133,62 +133,62 @@ class betaflight_uav : public UAV {
 				delete m_Fcu;
 			}
 		}
-		
+
 		virtual void takeOff() override {}
         virtual void land() override {}
-        virtual void setRoll(float right) override 
+        virtual void setRoll(float right) override
         {
             right = std::min(1.0f, std::max(-1.0f, right));
             M_RC_values[0] = 1500 + right*M_ControlScale;
         }
-        virtual void setPitch(float pitch) override 
+        virtual void setPitch(float pitch) override
         {
             pitch = std::min(1.0f, std::max(-1.0f, pitch));
             M_RC_values[1] = 1500 + pitch*M_ControlScale;
         }
-        virtual void setVerticalSpeed(float up) override 
+        virtual void setVerticalSpeed(float up) override
         {
             up = std::min(1.0f, std::max(-1.0f, up));
             M_RC_values[2] = 1500 + up*M_ControlScale;
         }
-        virtual void setYawSpeed(float right) override 
+        virtual void setYawSpeed(float right) override
         {
             right = std::min(1.0f, std::max(-1.0f, right));
             M_RC_values[4] = 1500 + right*M_ControlScale;
         }
-        
+
         std::string getArmStateAsString() {
             return BetaFlight::currentArmFlags.c_str();
         }
-        
+
         float getVoltage() {
             return BetaFlight::currentVoltage;
         }
-        
+
         float getAmpDraw() {
             return BetaFlight::currentAmpDraw;
         }
-        
+
         // accessors for M_ControlScale
         void setControlScale(float scale) {
             this->M_ControlScale = scale;
         }
-        
+
         float getControlScale() {
             return this->M_ControlScale;
         }
-        
+
         void sendCommands() {
             std::vector<uint16_t> auxs;
             m_Fcu->setRc(M_RC_values[0],M_RC_values[1],M_RC_values[3],M_RC_values[2],M_RC_values[4],M_RC_values[5],M_RC_values[6],M_RC_values[7], auxs);
         }
-        
+
         void armDrone() {
             // hold throttle low to arm
             //M_RC_values[2] = 1100;
             M_RC_values[5] = 2000;
         }
-        
+
         void disarmDrone() {
             M_RC_values[5] = 1000;
         }
@@ -200,7 +200,7 @@ class betaflight_uav : public UAV {
 		BetaFlight::Callbacks m_Cbs;
 		uint16_t M_RC_values[8] = {1500,1500,1040,1500,2000,1000,1500,1500};
 		float M_ControlScale = 200.0f;
-		
+
 
 
 };
