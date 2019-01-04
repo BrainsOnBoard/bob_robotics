afa01aefbdb946a52d3a9349737dc765a7eb52f8
jamie <neworderofjamie@gmail.com>
2019-01-04 12:33:52 +0000

    More or less usable PN spike plotting

--- projects/ardin_mb/visual_navigation_ui.cc
+++ projects/ardin_mb/visual_navigation_ui.cc
@@ -12,7 +12,7 @@
 //----------------------------------------------------------------------------
 namespace
 {
-void rasterPlot(unsigned int numNeurons, const MBMemoryHOG::Spikes &spikes, float pointSize = 1.0f)
+void rasterPlot(unsigned int numNeurons, const MBMemoryHOG::Spikes &spikes, float timeAxisStep = 20.0f, float pointSize = 1.0f)
 {
     if(spikes.empty()) {
         return;
@@ -20,13 +20,43 @@ void rasterPlot(unsigned int numNeurons, const MBMemoryHOG::Spikes &spikes, floa
 
     // Create dummy widget
     // **TODO** handle different dt
-    ImGui::Dummy(ImVec2(spikes.back().first * pointSize, (float)numNeurons * pointSize));
+    assert(MBParams::timestepMs == 1.0);
+    const float width = spikes.back().first * pointSize;
+    const float height = (float)numNeurons * pointSize;
+    const float leftBorder = 20.0f;
+    const float topBorder = 10.0f;
+    ImGui::Dummy(ImVec2(width + leftBorder, height + topBorder));
 
     const auto windowPos = ImGui::GetWindowPos();
-    for(const auto &s : spikes) {
-        for(unsigned int n : s.second) {
-            const float x = windowPos.x + (spikes.back().first * pointSize);
-            const float y = windowPos.y + ((float)n * pointSize);
+    const float rasterLeft = windowPos.x + leftBorder;
+    const float rasterTop = windowPos.y + topBorder;
+
+    // Y-axis
+    ImGui::GetWindowDrawList()->AddLine(ImVec2(rasterLeft, rasterTop),
+                                        ImVec2(rasterLeft, rasterTop + height),
+                                        IM_COL32(128, 128, 128, 255));
+
+    // X-axis
+    ImGui::GetWindowDrawList()->AddLine(ImVec2(rasterLeft, rasterTop + height),
+                                        ImVec2(rasterLeft + width, rasterTop + height),
+                                        IM_COL32(128, 128, 128, 255));
+
+    char tickText[32];
+    for(float t = 0.0f; t < spikes.back().first; t += timeAxisStep) {
+
+        snprintf(tickText, 32, "%0.1f", t);
+        const auto tickDims = ImGui::CalcTextSize(tickText);
+        ImGui::GetWindowDrawList()->AddText(ImVec2(rasterLeft + (t * pointSize) - (tickDims.x * 0.5f), rasterTop + height + 2),
+                                            IM_COL32(128, 128, 128, 255), tickText);
+    }
+
+    // Loop through timesteps during which spikes occured
+    for(const auto &timestepSpikes : spikes) {
+        // Loop through neuron ids which spiked this timestep
+        for(unsigned int n : timestepSpikes.second) {
+            // Calculate coordinate
+            const float x = rasterLeft + (timestepSpikes.first * pointSize);
+            const float y = rasterTop + ((float)n * pointSize);
             ImGui::GetWindowDrawList()->AddRectFilled(ImVec2(x, y), ImVec2(x + pointSize, y + pointSize),
                                                       IM_COL32(255, 255, 255, 255));
         }
@@ -56,14 +86,14 @@ void MBHogUI::handleUI()
     }
 
     if(ImGui::Begin("PN spikes")) {
-        rasterPlot(MBParams::numPN, m_Memory.getPNSpikes());
+        rasterPlot(MBParams::numPN, m_Memory.getPNSpikes(), 50.0f);
         ImGui::End();
     }
 
-    if(ImGui::Begin("KC spikes")) {
+    /*if(ImGui::Begin("KC spikes")) {
         rasterPlot(MBParams::numKC, m_Memory.getKCSpikes(), 0.1f);
         ImGui::End();
-    }
+    }*/
 
 }
 //----------------------------------------------------------------------------
