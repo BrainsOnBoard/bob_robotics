daf92f17464bb5f1925e5fb7cba9c77a6de3664b
Alex Dewar <a.dewar@sussex.ac.uk>
2019-01-15 17:39:28 +0000

    Tidy up use of sockets in RPiCamera
    This (might?) make it also work on Windows.

--- video/rpi_cam.h
+++ video/rpi_cam.h
@@ -30,33 +30,50 @@ namespace Video {
 class RPiCamera : public Input
 {
 public:
-    RPiCamera()
-      : RPiCamera(0)
-    {}
-
-    RPiCamera(int port)
-      : m_Socket(0)
+    RPiCamera(uint16_t port = 0)
+      : m_Socket(INVALID_SOCKET)
+      , m_Port(port)
     {
-        m_Port = port;
+        struct sockaddr_in addr;
 
-        // set up the networking code needed to receive images:
-        if (!setupSockets()) {
-            // error
-            return;
+        m_Socket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+        if (m_Socket == INVALID_SOCKET) {
+            throw OS::Net::NetworkError("Could not create socket");
         }
+
+        memset(&addr, 0, sizeof(addr));
+        addr.sin_family = AF_INET;
+        addr.sin_addr.s_addr = htonl(INADDR_ANY);
+        addr.sin_port = htons(m_Port);
+
+        if (bind(m_Socket, (const sockaddr *) &addr, (socklen_t) sizeof(addr))) {
+            throw OS::Net::NetworkError("Could not create socket");
+        }
+
+        // non-blocking socket
+#ifdef WIN32
+        ulong nonblocking_enabled = 1;
+        ioctlsocket(m_Socket, FIONBIO, &nonblocking_enabled);
+#else
+        fcntl(m_Socket, F_SETFL, O_NONBLOCK);
+#endif
     }
 
     virtual ~RPiCamera()
-    {}
+    {
+        if (m_Socket != INVALID_SOCKET) {
+            close(m_Socket);
+        }
+    }
 
     virtual std::string getCameraName() const override
     {
-        return "Raspberry Pi Camera";
+        return "rpi_camera";
     }
 
     virtual cv::Size getOutputSize() const override
     {
-        // this is fixed
+        // This is fixed
         return cv::Size(152, 72);
     }
 
@@ -66,19 +83,19 @@ public:
         return readGreyscaleFrame(outFrame);
     }
 
-    virtual bool readGreyscaleFrame(cv::Mat &outFrame)
+    virtual bool readGreyscaleFrame(cv::Mat &outFrame) override
     {
         unsigned char buffer[72 * 19];
 
-        if (!m_Socket) {
+        if (m_Socket == INVALID_SOCKET) {
             return false;
         }
 
         // Make sure frame is correct size and type
         outFrame.create(72, 152, CV_32FC1);
 
-        // get the most recent UDP frame (grayscale for now)
-        while (recv(m_Socket, buffer, 72 * 19, 0) > 0) {
+        // Get the most recent UDP frame (grayscale for now)
+        while (recv(m_Socket, reinterpret_cast<readbuff_t>(buffer), 72 * 19, 0) > 0) {
             // fill in the outFrame
             for (int i = 0; i < 72 * 19 - 1; ++i) {
                 outFrame.at<float>(i % 72, buffer[0] + floor(i / 72)) = float(buffer[i]) / 255.0f;
@@ -89,52 +106,15 @@ public:
         return true;
     }
 
-    virtual bool needsUnwrapping() const
+    virtual bool needsUnwrapping() const override
     {
-        // we do not need to unwrap this - it is done onboard the RPi
+        // We do not need to unwrap this - it is done onboard the RPi
         return false;
     }
 
-    virtual void setOutputSize(const cv::Size &)
-    {
-        throw std::runtime_error("This camera's resolution cannot be changed");
-    }
-
 private:
-    bool setupSockets()
-    {
-        struct sockaddr_in addr;
-
-        m_Socket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-        if (m_Socket == INVALID_SOCKET) {
-            goto error;
-        }
-
-        memset(&addr, 0, sizeof(addr));
-        addr.sin_family = AF_INET;
-        addr.sin_addr.s_addr = htonl(INADDR_ANY);
-        addr.sin_port = htons(m_Port);
-
-        if (bind(m_Socket, (const sockaddr *) &addr, (int) sizeof(addr))) {
-            goto error;
-        }
-
-        // non-blocking socket
-#ifdef WIN32
-        ulong nonblocking_enabled = 1;
-        ioctlsocket(m_Socket, FIONBIO, &nonblocking_enabled);
-#else
-        fcntl(m_Socket, F_SETFL, O_NONBLOCK);
-#endif
-        return true;
-
-    error:
-        std::cerr << "Error (" << errno << "): RPi Cam UDP " << m_Port << std::endl;
-        exit(1);
-    }
-
-    int m_Socket;
-    int m_Port;
+    socket_t m_Socket;
+    uint16_t m_Port;
 }; // RPiCamera
 } // Video
 } // BoBRobotics
