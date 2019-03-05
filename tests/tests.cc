9f5ce520b16100a9c51e116bbe1687e177c59481
Alex Dewar <a.dewar@sussex.ac.uk>
2019-03-05 19:27:17 +0000

    Fix: Shouldn't use ASSERT_EQ() with floating-point numbers

--- tests/tests.cc
+++ tests/tests.cc
@@ -1,9 +1,17 @@
+// Third-party includes
+#include "third_party/units.h"
+
 // Google Test
 #include "gtest/gtest.h"
 
-// Put this using declaration here so we don't have to bother putting it in the test headers
+// Put these using declarations here so we don't have to bother putting it in the test headers
 namespace BoBRobotics {}
 using namespace BoBRobotics;
+using namespace units::literals;
+
+// Little helper macro for floating-point comparisons with the units library
+#define ASSERT_UNIT_EQ(val1, val2) \
+    ASSERT_DOUBLE_EQ(val1.value(), static_cast<decltype(val1)>(val2).value())
 
 // Automatically generated includes for the files in test_headers
 #include ".include_test_headers.h"
