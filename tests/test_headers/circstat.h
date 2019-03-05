605b508c33bfaaa2e6f02c2cadff5cdd6766fe16
Alex Dewar <a.dewar@sussex.ac.uk>
2019-03-05 19:27:17 +0000

    Fix: Shouldn't use ASSERT_EQ() with floating-point numbers

--- tests/test_headers/circstat.h
+++ tests/test_headers/circstat.h
@@ -1,24 +1,22 @@
 // BoB robotics includes
 #include "common/circstat.h"
 
-using namespace units::literals;
-
 TEST(CircStatTest, NormaliseAngle180) {
-    ASSERT_EQ(normaliseAngle180(360_deg), 0_deg);
-    ASSERT_EQ(normaliseAngle180(-360_deg), 0_deg);
-    ASSERT_EQ(normaliseAngle180(540_deg), 180_deg);
-    ASSERT_EQ(normaliseAngle180(-540_deg), 180_deg);
-    ASSERT_EQ(normaliseAngle180(180_deg), 180_deg);
-    ASSERT_EQ(normaliseAngle180(-180_deg), 180_deg);
-    ASSERT_EQ(normaliseAngle180(720_deg), 0_deg);
-    ASSERT_EQ(normaliseAngle180(-720_deg), 0_deg);
+    ASSERT_UNIT_EQ(normaliseAngle180(360_deg), 0_deg);
+    ASSERT_UNIT_EQ(normaliseAngle180(-360_deg), 0_deg);
+    ASSERT_UNIT_EQ(normaliseAngle180(540_deg), 180_deg);
+    ASSERT_UNIT_EQ(normaliseAngle180(-540_deg), 180_deg);
+    ASSERT_UNIT_EQ(normaliseAngle180(180_deg), 180_deg);
+    ASSERT_UNIT_EQ(normaliseAngle180(-180_deg), 180_deg);
+    ASSERT_UNIT_EQ(normaliseAngle180(720_deg), 0_deg);
+    ASSERT_UNIT_EQ(normaliseAngle180(-720_deg), 0_deg);
 }
 
 TEST(CircStatTest, CircularDistance) {
-    ASSERT_EQ(circularDistance(360_deg, 0_deg), 0_deg);
-    ASSERT_EQ(circularDistance(0_deg, 360_deg), 0_deg);
-    ASSERT_EQ(circularDistance(180_deg, -180_deg), 0_deg);
-    ASSERT_EQ(circularDistance(-180_deg, 180_deg), 0_deg);
-    ASSERT_EQ(circularDistance(180_deg, -170_deg), -10_deg);
-    ASSERT_EQ(circularDistance(-180_deg, 170_deg), 10_deg);
+    ASSERT_UNIT_EQ(circularDistance(360_deg, 0_deg), 0_deg);
+    ASSERT_UNIT_EQ(circularDistance(0_deg, 360_deg), 0_deg);
+    ASSERT_UNIT_EQ(circularDistance(180_deg, -180_deg), 0_deg);
+    ASSERT_UNIT_EQ(circularDistance(-180_deg, 180_deg), 0_deg);
+    ASSERT_UNIT_EQ(circularDistance(180_deg, -170_deg), -10_deg);
+    ASSERT_UNIT_EQ(circularDistance(-180_deg, 170_deg), 10_deg);
 }
