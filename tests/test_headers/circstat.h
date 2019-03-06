a1d8bd10653deccf4f5ba71c2e1554b018c048cb
Alex Dewar <a.dewar@sussex.ac.uk>
2019-03-06 11:47:53 +0000

    Should be using EXPECT_DOUBLE_EQ() not ASSERT_DOUBLE_EQ()

--- tests/test_headers/circstat.h
+++ tests/test_headers/circstat.h
@@ -2,21 +2,21 @@
 #include "common/circstat.h"
 
 TEST(CircStatTest, NormaliseAngle180) {
-    ASSERT_UNIT_EQ(normaliseAngle180(360_deg), 0_deg);
-    ASSERT_UNIT_EQ(normaliseAngle180(-360_deg), 0_deg);
-    ASSERT_UNIT_EQ(normaliseAngle180(540_deg), 180_deg);
-    ASSERT_UNIT_EQ(normaliseAngle180(-540_deg), 180_deg);
-    ASSERT_UNIT_EQ(normaliseAngle180(180_deg), 180_deg);
-    ASSERT_UNIT_EQ(normaliseAngle180(-180_deg), 180_deg);
-    ASSERT_UNIT_EQ(normaliseAngle180(720_deg), 0_deg);
-    ASSERT_UNIT_EQ(normaliseAngle180(-720_deg), 0_deg);
+    BOB_EXPECT_UNIT_T_EQ(normaliseAngle180(360_deg), 0_deg);
+    BOB_EXPECT_UNIT_T_EQ(normaliseAngle180(-360_deg), 0_deg);
+    BOB_EXPECT_UNIT_T_EQ(normaliseAngle180(540_deg), 180_deg);
+    BOB_EXPECT_UNIT_T_EQ(normaliseAngle180(-540_deg), 180_deg);
+    BOB_EXPECT_UNIT_T_EQ(normaliseAngle180(180_deg), 180_deg);
+    BOB_EXPECT_UNIT_T_EQ(normaliseAngle180(-180_deg), 180_deg);
+    BOB_EXPECT_UNIT_T_EQ(normaliseAngle180(720_deg), 0_deg);
+    BOB_EXPECT_UNIT_T_EQ(normaliseAngle180(-720_deg), 0_deg);
 }
 
 TEST(CircStatTest, CircularDistance) {
-    ASSERT_UNIT_EQ(circularDistance(360_deg, 0_deg), 0_deg);
-    ASSERT_UNIT_EQ(circularDistance(0_deg, 360_deg), 0_deg);
-    ASSERT_UNIT_EQ(circularDistance(180_deg, -180_deg), 0_deg);
-    ASSERT_UNIT_EQ(circularDistance(-180_deg, 180_deg), 0_deg);
-    ASSERT_UNIT_EQ(circularDistance(180_deg, -170_deg), -10_deg);
-    ASSERT_UNIT_EQ(circularDistance(-180_deg, 170_deg), 10_deg);
+    BOB_EXPECT_UNIT_T_EQ(circularDistance(360_deg, 0_deg), 0_deg);
+    BOB_EXPECT_UNIT_T_EQ(circularDistance(0_deg, 360_deg), 0_deg);
+    BOB_EXPECT_UNIT_T_EQ(circularDistance(180_deg, -180_deg), 0_deg);
+    BOB_EXPECT_UNIT_T_EQ(circularDistance(-180_deg, 180_deg), 0_deg);
+    BOB_EXPECT_UNIT_T_EQ(circularDistance(180_deg, -170_deg), -10_deg);
+    BOB_EXPECT_UNIT_T_EQ(circularDistance(-180_deg, 170_deg), 10_deg);
 }
