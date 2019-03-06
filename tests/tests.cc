719e3216c20b8b6dd606313fd8cb5a344042e67a
Alex Dewar <a.dewar@sussex.ac.uk>
2019-03-06 11:47:53 +0000

    Should be using EXPECT_DOUBLE_EQ() not ASSERT_DOUBLE_EQ()

--- tests/tests.cc
+++ tests/tests.cc
@@ -10,8 +10,8 @@ using namespace BoBRobotics;
 using namespace units::literals;
 
 // Little helper macro for floating-point comparisons with the units library
-#define ASSERT_UNIT_EQ(val1, val2) \
-    ASSERT_DOUBLE_EQ(val1.value(), static_cast<decltype(val1)>(val2).value())
+#define BOB_EXPECT_UNIT_T_EQ(val1, val2) \
+    EXPECT_DOUBLE_EQ(val1.value(), static_cast<decltype(val1)>(val2).value())
 
 // Automatically generated includes for the files in test_headers
 #include ".include_test_headers.h"
