a87d1ca7974fdaa14326cdedd48b9e48b74c63fc
Alex Dewar <a.dewar@sussex.ac.uk>
2019-03-22 17:12:33 +0000

    norbot_control: Fix possible race condition

--- tools/norbot_control/norbot_control.cc
+++ tools/norbot_control/norbot_control.cc
@@ -27,72 +27,75 @@ using namespace BoBRobotics;
 int
 bob_main(int, char **)
 {
-    // Listen for incoming connection on default port
-    Net::Server server;
-    auto connection = server.waitForConnection();
-
-    std::unique_ptr<Video::Input> camera;
-    std::unique_ptr<Video::NetSink> netSink;
-    std::unique_ptr<Net::Client> client;
-    std::unique_ptr<Robots::Tank> tank;
-    std::unique_ptr<HID::Joystick> joystick;
-
-    // Get panoramic camera
     try {
-        camera = Video::getPanoramicCamera();
-    } catch (std::runtime_error &e) {
-        // Camera not found
-        std::cerr << e.what() << std::endl;
-    }
-    if (camera) {
-        // Stream camera synchronously over network
-        netSink = std::make_unique<Video::NetSink>(connection, camera->getOutputSize(), camera->getCameraName());
-    }
+        // Listen for incoming connection on default port
+        Net::Server server;
+        auto connection = server.waitForConnection();
 
-    // Try to connect to servos over I2C and if that fails, try to connect to EV3
-    try {
-        tank = std::make_unique<Robots::Norbot>();
-    } catch (std::runtime_error &) {
-        std::cout << "Trying to connect to EV3..." << std::endl;
-        client = std::make_unique<Net::Client>("10.42.0.130");
-        tank = std::make_unique<Robots::TankNetSink>(*client);
-    }
+        std::unique_ptr<Net::Client> client;
+        std::unique_ptr<Video::Input> camera;
+        std::unique_ptr<Video::NetSink> netSink;
+        std::unique_ptr<Robots::Tank> tank;
+        std::unique_ptr<HID::Joystick> joystick;
 
-    // Read motor commands from network
-    tank->readFromNetwork(connection);
-
-    // Try to get joystick
-    try {
-        joystick = std::make_unique<HID::Joystick>();
-        tank->addJoystick(*joystick);
-    } catch (std::runtime_error &e) {
-        // Joystick not found
-        std::cerr << e.what() << std::endl;
-    }
-
-    // Run server in background,, catching any exceptions for rethrowing
-    BackgroundExceptionCatcher catcher;
-    catcher.trapSignals(); // Catch Ctrl-C
-    connection.runInBackground();
-    if (client) {
-        client->runInBackground();
-    }
-
-    cv::Mat frame;
-    while (connection.isOpen()) {
-        // Rethrow any exceptions caught on background thread
-        catcher.check();
-
-        const bool joystickUpdate = joystick && joystick->update();
-        const bool cameraUpdate = camera && camera->readFrame(frame);
-
-        // If there's a new frame, send it, else sleep
-        if (cameraUpdate) {
-            netSink->sendFrame(frame);
-        } else if (!joystickUpdate) {
-            std::this_thread::sleep_for(25ms);
+        // Get panoramic camera
+        try {
+            camera = Video::getPanoramicCamera();
+        } catch (std::runtime_error &e) {
+            // Camera not found
+            std::cerr << e.what() << std::endl;
         }
-    }
+        if (camera) {
+            // Stream camera synchronously over network
+            netSink = std::make_unique<Video::NetSink>(connection, camera->getOutputSize(), camera->getCameraName());
+        }
 
+        // Try to connect to servos over I2C and if that fails, try to connect to EV3
+        try {
+            tank = std::make_unique<Robots::Norbot>();
+        } catch (std::runtime_error &) {
+            std::cout << "Trying to connect to EV3..." << std::endl;
+            client = std::make_unique<Net::Client>("10.42.0.130");
+            tank = std::make_unique<Robots::TankNetSink>(*client);
+        }
+
+        // Read motor commands from network
+        tank->readFromNetwork(connection);
+
+        // Try to get joystick
+        try {
+            joystick = std::make_unique<HID::Joystick>();
+            tank->addJoystick(*joystick);
+        } catch (std::runtime_error &e) {
+            // Joystick not found
+            std::cerr << e.what() << std::endl;
+        }
+
+        // Run server in background,, catching any exceptions for rethrowing
+        BackgroundExceptionCatcher catcher;
+        catcher.trapSignals(); // Catch Ctrl-C
+        connection.runInBackground();
+        if (client) {
+            client->runInBackground();
+        }
+
+        cv::Mat frame;
+        while (connection.isOpen()) {
+            // Rethrow any exceptions caught on background thread
+            catcher.check();
+
+            const bool joystickUpdate = joystick && joystick->update();
+            const bool cameraUpdate = camera && camera->readFrame(frame);
+
+            // If there's a new frame, send it, else sleep
+            if (cameraUpdate) {
+                netSink->sendFrame(frame);
+            } else if (!joystickUpdate) {
+                std::this_thread::sleep_for(25ms);
+            }
+        }
+    } catch (Net::SocketClosedError &) {
+        std::cout << "Connection closed" << std::endl;
+    }
     return EXIT_SUCCESS;
 }
