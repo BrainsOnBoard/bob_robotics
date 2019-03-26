46e1a17eac2536a465128517c7dc6faea94922df
Alex Dewar <a.dewar@sussex.ac.uk>
2019-03-26 20:15:27 +0000

    Fix parsing of LOG_LEVEL env var

--- common/logging.h
+++ common/logging.h
@@ -6,6 +6,10 @@
 
 // Standard C includes
 #include <cstdlib>
+#include <cstring>
+
+// Standard C++ includes
+#include <string>
 
 namespace BoBRobotics {
 #ifdef DEBUG
@@ -14,20 +18,58 @@ constexpr plog::Severity DefaultLogLevel = plog::Severity::debug;
 constexpr plog::Severity DefaultLogLevel = plog::Severity::info;
 #endif
 
+constexpr inline auto
+logLevelToString(plog::Severity severity)
+{
+    switch (severity) {
+    case plog::fatal:
+        return "FATAL";
+    case plog::error:
+        return "ERROR";
+    case plog::warning:
+        return "WARNING";
+    case plog::info:
+        return "INFO";
+    case plog::debug:
+        return "DEBUG";
+    case plog::verbose:
+        return "VERBOSE";
+    default:
+        return "NONE";
+    }
+}
+
+// Get log level from env var, or set default
+inline auto getLogLevel()
+{
+    // Try to get from environment variable
+    char *severityEnvVar = std::getenv("LOG_LEVEL");
+    if (!severityEnvVar) {
+        return DefaultLogLevel;
+    }
+
+    // Convert to uppsercase
+    for (char *c = severityEnvVar; *c; c++) {
+        *c = std::toupper(*c);
+    }
+
+    // Match to enum member
+    for (plog::Severity severity = plog::none; severity <= plog::verbose; severity = static_cast<plog::Severity>(severity + 1))
+    {
+        if (std::strcmp(logLevelToString(severity),
+                        severityEnvVar) == 0) {
+            return severity;
+        }
+    }
+
+    throw std::runtime_error(std::string(severityEnvVar) + " is not a valid log level");
+}
+
 //! Initialises our logging library with default options
 inline void initialiseLogging()
 {
-    // Get log level from env var, or set default
-    plog::Severity severity;
-    char *severityEnvVar = std::getenv("LOG_LEVEL");
-    if (severityEnvVar) {
-        for (char *c = severityEnvVar; c; c++) {
-            *c = std::toupper(*c);
-        }
-        severity = plog::severityFromString(severityEnvVar);
-    } else {
-        severity = DefaultLogLevel; // Default log level
-    }
+    // Get log level
+    const auto severity = getLogLevel();
 
     // Get filename to log to, if specified in env var
     char *filenameEnvVar = std::getenv("LOG_FILE");
