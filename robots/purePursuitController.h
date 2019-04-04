497ed7b5532a1745e96ad6e7f1b065e44e2ea1ef
Norbert Domcsek <liquidmind@me.com>
2019-04-04 12:29:24 +0100

    empty controller class added, need to write implementation

---
+++ robots/purePursuitController.h
@@ -0,0 +1,32 @@
+// purePursuitController.h
+
+// BoB robotics includes
+#include "common/main.h"
+#include "robots/car_display.h"
+
+// Third-party includes
+#include "third_party/units.h"
+
+
+// Standard C++ includes
+#include <chrono>
+
+#include <iostream>
+#include <thread>
+
+
+class PurePursuitController {
+public:
+    void getTurningAngle() {
+
+    }
+
+private:
+
+    void getLookaheadPoint() {
+
+    }
+
+    
+
+};
