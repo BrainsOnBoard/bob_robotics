a9bfb65902eeb70512801b2eb091f06ac42aa04b
Norbert <norbert@Norberts-MacBook-Pro.local>
2019-04-08 03:19:18 +0100

    refactored code, made comments

--- examples/PurePursuitController/purePursuitControllerExample.cc
+++ examples/PurePursuitController/purePursuitControllerExample.cc
@@ -1,77 +1,62 @@
+/*
+    Small example program to demonstrate the Pure Pursuit Controller. The controller
+    takes a list of coordinates as waypoint and calculates a desired turning angle
+    to steer the car for a smooth path tracking
+*/
+
 // BoB robotics includes
 #include "common/main.h"
 #include "robots/simulatedAckermanCar.h"
 #include "robots/car_display.h"
+#include "robots/purePursuitController.h"
 
 // Third-party includes
 #include "third_party/units.h"
 
-#include "robots/purePursuitController.h"
-
 // Standard C++ includes
 #include <chrono>
-
 #include <iostream>
 #include <thread>
 
 using namespace BoBRobotics;
 using namespace std::literals;
 using namespace units::literals;
+using namespace units::length;
+using namespace units::angle;
 
-void drawLinesBetweenRects(std::vector<SDL_Rect> listRects, SDL_Renderer *renderer) {
-    if (!listRects.empty()) {
-
-        for (unsigned int i = 0; i < listRects.size()-1; i++) {
-            SDL_Rect current_rectangle = listRects.at(i);
-            SDL_Rect next_rectangle = listRects.at(i+1);
-
-            float lineStartX = current_rectangle.x;
-            float lineStartY = current_rectangle.y;
-            float lineEndX = next_rectangle.x;
-            float lineEndY = next_rectangle.y;
-            
-            SDL_RenderDrawLine(renderer, lineStartX, lineStartY, lineEndX, lineEndY);
-            
-        }
-    }
-}
-
-
-
-int
-bob_main(int, char **)
+int bob_main(int, char **)
 {
 
-    Robots::SimulatedAckermanCar<> car(1.4_mps, 500_mm);
-    Robots::CarDisplay display(10.2_m,160_mm);                     // For displaying the agent
-    std::vector<SDL_Rect> rekt_list;
+    Robots::SimulatedAckermanCar<> car(1.4_mps, 500_mm);   // simulated car
+    Robots::CarDisplay display(10.2_m,160_mm);             // For displaying the agent
+    std::vector<SDL_Rect> rekt_list;                       // list of waypoints
 
-    float currentX = 0, currentY = 0;
+    float currentX = 0, currentY = 0;                      // current mouse coordinate click
 
-
+    // adding the first coordinate
     //-------------------------------------------------------------------
-    units::length::millimeter_t xMM, yMM;
+    millimeter_t xMM, yMM;
     display.pixelToMM(currentX, currentY,  xMM,  yMM);
-    std::vector<std::vector<units::length::millimeter_t>> wpCoordinates; 
-    std::vector<units::length::millimeter_t> firstCoord;
-    firstCoord.push_back(xMM);
-    firstCoord.push_back(yMM);
+    std::vector<Vector2<millimeter_t>> wpCoordinates; 
+    Vector2<millimeter_t> firstCoord(xMM, yMM);
     wpCoordinates.push_back(firstCoord);
     //-------------------------------------------------------------------
 
-    auto mmps = 0_mps;
-    units::angle::degree_t deg = units::angle::degree_t(0);
+    auto mmps = 0_mps;    // speed of robot car
+    degree_t deg = 0_deg; // angle of steering of robot car
+    millimeter_t lookaheadDistance = 1000_mm; // lookahead distance
 
-    auto wheelBase = car.getDistanceBetweenAxis();
-    BoBRobotics::Robots::PurePursuitController controller(units::length::millimeter_t(2000), wheelBase);
+    auto wheelBase = car.getDistanceBetweenAxis(); // distance between wheel bases
+    BoBRobotics::Robots::PurePursuitController controller(lookaheadDistance, wheelBase);
 
     while(display.isOpen()) {
         
-        // each click will spawn a way point which is connected together /////////
+        // each click will spawn a way point which is connected together 
         std::vector<float> mousePos = display.getMouseClickPixelPosition();
         float xp = mousePos[0];
         float yp = mousePos[1];
         
+        // click to the screen to store waypoint in list
         if (xp != currentX && yp != currentY) {
             // draw a rectangle at the goal position
             SDL_Rect rekt;
@@ -81,40 +66,31 @@ bob_main(int, char **)
             rekt.h = 5;
             currentX = xp;
             currentY = yp;
-
-            rekt_list.push_back(rekt);
+            rekt_list.push_back(rekt); // save rectangle
             
-            
+            // we store the physical unit coordinate in the waypoint list
             display.pixelToMM(xp, yp,  xMM,  yMM);
-            std::vector<units::length::millimeter_t> coords;
-            coords.push_back(xMM);
-            coords.push_back(yMM);
-            wpCoordinates.push_back(coords);
-
-            std::cout << " x " << xMM.value() << " y " << yMM.value() << std::endl;
+            Vector2<millimeter_t> coords(xMM,yMM);
+            wpCoordinates.push_back(coords);  
         }
-        //------------------------------------------------------------------------
-
-
-        controller.setWayPoints(wpCoordinates);
-        //controller.getTurningAngle(car.getPose().x(), car.getPose().y());
-
-
         
+        // set waypoints in controller
+        controller.setWayPoints(wpCoordinates);
+
+        // run a GUI step    
         auto key = display.runGUI(car.getPose());
         display.clearScreen();
 
+        // draw all the waypoints on the screen
         for (unsigned int i =0; i < rekt_list.size(); i++) {
             display.drawRectangleAtCoordinates(rekt_list.at(i));
         }
 
+        // draw lines between waypoints to form a path
+        display.drawLinesBetweenRects(rekt_list, display.getRenderer());
 
-        // draw lines between waypoints
-        drawLinesBetweenRects(rekt_list, display.getRenderer());
-
-
-        // draw lookahead point
-        std::vector<units::length::millimeter_t> lookPoint =  controller.getLookAheadPoint(car.getPose().x(), car.getPose().y(),units::length::millimeter_t(1000));
+        // draw lookahead point and a line to it from the robot
+        std::vector<millimeter_t> lookPoint =  controller.getLookAheadPoint(car.getPose().x(), car.getPose().y(),lookaheadDistance);
         int pxx, pxy;
         auto robx = car.getPose().x();
         auto roby = car.getPose().y();
@@ -134,39 +110,37 @@ bob_main(int, char **)
             display.drawRectangleAtCoordinates(rkt);
             rektVec.push_back(rkt);
             rektVec.push_back(rkt_rob);
-            drawLinesBetweenRects(rektVec, display.getRenderer());
+            // draw line between robot and lookahead point
+            display.drawLinesBetweenRects(rektVec, display.getRenderer());
         }
+
+        // calculate turning angle with controller
         auto turningAngle = controller.getTurningAngle(robx, roby, heading);
-        units::angle::degree_t turningAngleDeg = turningAngle;
-        
-
+        degree_t turningAngleDeg = turningAngle;
     
-
+        // if there is a key command, move car with keys, othwerwise listen to 
+        //the controller command to turn the car so it follows the path
         if (key.first == SDLK_UP) {
-            mmps = 1.4_mps;
+            mmps = 1.4_mps; // go max speed
         } 
-
-        if (key.first == SDLK_DOWN) {
-            mmps = 0_mps;
+        else if (key.first == SDLK_DOWN) {
+            mmps = 0_mps; // stop
         }
 
+        // if left or right button pressed ->steering 30 degrees left and right, 
+        // if no turn command pressed, turning is performed by the controller
         if (key.first == SDLK_LEFT) {
-            deg = units::angle::degree_t(30);
+            deg = 30_deg;
             car.move(mmps, deg);
         }
-
         else if (key.first == SDLK_RIGHT) {
-            deg = units::angle::degree_t(-30);
+            deg = -30_deg;
             car.move(mmps, deg);
         }
-
         else {
-            deg = units::angle::degree_t(0);
-            //auto heading = car.getPose().yaw();
             car.move(mmps, turningAngleDeg);
-            std::cout << turningAngleDeg.value() << std::endl;
         }
-        
+
     }
     return EXIT_SUCCESS;
 }
