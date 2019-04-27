db697209575e5e4a053ba7b40079c1b492ed81e4
Alex Dewar <a.dewar@sussex.ac.uk>
2019-04-27 21:09:55 +0100

    Make build_all.sh scripts into symlinks

--- projects/build_all.sh
+++ projects/build_all.sh
@@ -1,14 +1,1 @@
-#!/bin/bash
-
-builddir=$(dirname "$0")/build
-OLDPWD=$PWD
-if [ ! -d $builddir ]; then
-    mkdir $builddir
-fi
-cd $builddir
-
-cmake .. && make -k -j `nproc`
-ret=$?
-cd $OLDPWD
-
-exit $ret
+../cmake/build_all_function.sh
