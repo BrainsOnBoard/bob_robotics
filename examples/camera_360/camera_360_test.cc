5d3845a11960e7dde534db56b3c73a677d35b152
Tenxlenx <liquidmind@me.com>
2019-04-29 14:21:08 +0100

    data collection test, I accidentally overwritten the original file, but I will put it back

--- examples/camera_360/camera_360_test.cc
+++ examples/camera_360/camera_360_test.cc
@@ -1,8 +1,11 @@
 // BoB robotics includes
-#include "common/logging.h"
-#include "common/timer.h"
-#include "imgproc/opencv_unwrap_360.h"
-#include "video/panoramic.h"
+#include "../../common/logging.h"
+#include "../../common/timer.h"
+#include "../../imgproc/opencv_unwrap_360.h"
+#include "../../video/panoramic.h"
+#include "../../common/gps.h"
+#include "../../third_party/units.h"
+#include <time.h> 
 
 // Standard C++ includes
 #include <iostream>
@@ -13,10 +16,11 @@
 using namespace BoBRobotics;
 using namespace BoBRobotics::ImgProc;
 using namespace BoBRobotics::Video;
-
+using namespace units::angle;
+using namespace units::length;
 int main()
 {
-    const cv::Size unwrapRes(90, 25);
+    const cv::Size unwrapRes(720, 150);
     const unsigned int outputScale = 10;
 
     // Create panoramic camera and suitable unwrapper
@@ -24,21 +28,31 @@ int main()
     auto unwrapper = cam->createUnwrapper(unwrapRes);
     const auto cameraRes = cam->getOutputSize();
 
+    //create GPS
+    Gps gps("/dev/ttyACM0");
+
+
     // Create images
     cv::Mat originalImage(cameraRes, CV_8UC3);
     cv::Mat outputImage(unwrapRes, CV_8UC3);
 
-    // Create motor
-    cv::namedWindow("Unwrapped", cv::WINDOW_NORMAL);
-    cv::resizeWindow("Unwrapped", unwrapRes.width * outputScale,
-                     unwrapRes.height * outputScale);
+    constexpr float joystickDeadzone = 0.25f;
+    
+    // Create joystick interface
+    BoBRobotics::HID::Joystick joystick(joystickDeadzone);
+    
+    // Create motor interface
+    BoBRobotics::Robots::Norbot robot;
 
-    cv::namedWindow("Original", cv::WINDOW_NORMAL);
-    cv::resizeWindow("Original", cameraRes.width, cameraRes.height);
+    // output file for gps 
+    std::ofstream file("out.csv");
+
+    // for time
+    time_t rawtime;
+    struct tm * timeinfo;
+    
 
     {
-        Timer<> timer("Total time:");
-
         unsigned int frame = 0;
         for(frame = 0;; frame++) {
             // Read from camera
@@ -49,17 +63,34 @@ int main()
             // Unwrap
             unwrapper.unwrap(originalImage, outputImage);
 
-            // Show frame difference
-            cv::imshow("Original", originalImage);
-            cv::imshow("Unwrapped", outputImage);
+            // save camera
+            char filename[255];
+            sprintf(filename, "file_%u.jpg", frame);
+            cv::imwrite(filename, outputImage);
 
-            if(cv::waitKey(1) == 27) {
-                break;
+            // get gps data
+            degree_t lat,lon;
+            meter_t altitude;
+            bool didGetGps = gps.getPosition(lat,lon, altitude);
+
+            time (&rawtime);
+            timeinfo = localtime (&rawtime);
+            
+            if (didGetGps) {
+                // saving gps coords
+                file << lat.value() << "," << lon.value() << "," << robot.getLeft() << "," << robot.getRight() << "," << frame << "," << asctime(timeinfo) << std::endl;
             }
+            
+            // Read joystick
+            if (!joystick.isDown(BoBRobotics::HID::JButton::B)) {
+                joystick.update();
+      
+                // Use joystick to drive motor
+                robot.drive(joystick);
+            }
+            
         }
 
-        const double msPerFrame = timer.get() / (double)frame;
-        std::cout << "FPS:" << 1000.0 / msPerFrame << std::endl;
     }
 
     return EXIT_SUCCESS;
