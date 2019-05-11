8140bffdf87558ecf641162ad5387ef3f6c82261
Alex Dewar <a.dewar@sussex.ac.uk>
2019-05-11 10:48:14 +0100

    Revert "Tidy header"
    This reverts commit 2eab8daa9e0f40ae07f32bc7a584217d29dfb988.

--- include/hid/joystick_base.h
+++ include/hid/joystick_base.h
@@ -1,18 +1,18 @@
 #pragma once
 
-// BoB robotics includes
-#include "common/thread.h"
-
-// Standard C includes
+// C includes
 #include <cmath>
 #include <cstdint>
 
-// Standard C++ includes
+// C++ includes
 #include <array>
 #include <functional>
 #include <limits>
 #include <string>
 #include <vector>
+
+// BoB robotics includes
+#include "../common/threadable.h"
 
 namespace BoBRobotics {
 namespace HID {
