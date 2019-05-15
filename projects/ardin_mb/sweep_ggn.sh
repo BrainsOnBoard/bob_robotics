a1e1fb87047cbfeec94f0dd6e8b49a7987e3dc48
neworderofjamie <J.C.Knight@sussex.ac.uk>
2019-05-15 14:33:38 +0100

    Make output directory if it's missing for sweep

--- projects/ardin_mb/sweep_ggn.sh
+++ projects/ardin_mb/sweep_ggn.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-for kcToGGNWeight in $(seq 0.003 0.0002 0.004); do
-    for ggnToKCWeight in $(seq -5 0.5 -5); do
-        for ggnToKCVmid in $(seq -56 2 -56); do
+# Create output directory
+mkdir -p ggn_sweep
+
+# Sweep
+for kcToGGNWeight in $(seq 0.016 0.001 0.020); do
+    for ggnToKCWeight in $(seq -4 0.5 -4); do
+        for ggnToKCVmid in $(seq -54.2 0.1 -54.2); do
             for ggnToKCVSlope in $(seq 1 0.25 1); do
                 logFile=ggn_sweep/${kcToGGNWeight}_${ggnToKCWeight}_${ggnToKCVmid}_${ggnToKCVSlope}.csv
                 echo $logFile
