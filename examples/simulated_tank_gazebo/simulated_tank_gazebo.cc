87f4381db02cbbd38473553417b19f970cca3ac0
Shinjan Mitra <shinjanxp@gmail.com>
2019-05-28 14:46:45 +0530

    Mounted camera on bot and able to visualize camera feed using opencv

--- examples/simulated_tank_gazebo/simulated_tank_gazebo.cc
+++ examples/simulated_tank_gazebo/simulated_tank_gazebo.cc
@@ -12,9 +12,11 @@
 #include <chrono>
 #include <iostream>
 #include <thread>
+// Gazebo includes
 #include <gazebo/gazebo_config.h>
 #include <gazebo/transport/transport.hh>
 #include <gazebo/msgs/msgs.hh>
+#include <gazebo/common/common.hh>
 
 // Gazebo's API has changed between major releases. These changes are
 // accounted for with #if..#endif blocks in this file.
@@ -24,10 +26,39 @@
 #include <gazebo/gazebo_client.hh>
 #endif
 
+//OpenCV includes
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/features2d/features2d.hpp>
+
 using namespace BoBRobotics;
 using namespace std::literals;
 using namespace units::literals;
+void OnImageMsg(ConstImageStampedPtr &msg)
+{
+    // std::cout << msg->image().width() << std::endl;
+    // std::cout << msg->image().height() << std::endl;
+    // std::cout << msg->image().pixel_format() << std::endl;
+    // std::cout << std::endl;
 
+    int width;
+    int height;
+    char *data;
+
+    width = (int) msg->image().width();
+    height = (int) msg->image().height();
+    data = new char[msg->image().data().length() + 1];
+
+    memcpy(data, msg->image().data().c_str(), msg->image().data().length());
+    cv::Mat image(height, width, CV_8UC3, data);
+
+    cv::imshow("camera", image);
+    cv::waitKey(1);
+    delete data;  // DO NOT FORGET TO DELETE THIS, 
+                  // ELSE GAZEBO WILL TAKE ALL YOUR MEMORY
+}
 int
 bob_main(int, char **)
 {
@@ -38,11 +69,20 @@ bob_main(int, char **)
     #else
     gazebo::client::setup(0, 0);
     #endif
-    // Create our node for communication
+    // Create our node for publishing joystick values
     gazebo::transport::NodePtr node(new gazebo::transport::Node());
     node->Init();
 
-    // Publish to the  velodyne topic
+    //Setup image subscription node
+    gazebo::transport::NodePtr imageNode(new gazebo::transport::Node());
+    gazebo::transport::SubscriberPtr imageSub;
+    imageNode->Init();
+    // Subscribe to the topic, and register a callback
+    imageSub = imageNode->Subscribe("/gazebo/default/my_simple_cart/camera/link/camera/image", OnImageMsg);
+    std::cerr << "Subsribed to /gazebo/default/my_simple_cart/camera/link/camera/image \n";
+
+    
+    // Publish to the  simple_cart topic
     gazebo::transport::PublisherPtr pub =node->Advertise<gazebo::msgs::Vector3d>("~/my_simple_cart/vel_cmd");
 
     // Wait for a subscriber to connect to this publisher
