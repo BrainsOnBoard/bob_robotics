ded5f87e21abdce71adb4a3e1c4a185a3de01934
Alex Dewar <a.dewar@sussex.ac.uk>
2019-06-04 19:21:20 +0100

    Replace ev3_control and norbot_control with generic tank_control

--- tools/tank_control/tank_control.cc
+++ tools/tank_control/tank_control.cc
@@ -2,33 +2,42 @@
 #include "common/background_exception_catcher.h"
 #include "common/logging.h"
 #include "common/main.h"
+#include "hid/joystick.h"
+#include "net/client.h"
 #include "net/server.h"
-#include "robots/ev3/ev3.h"
-#include "robots/ev3/mindstorms_imu.h"
+#include "os/net.h"
+#include "robots/tank.h"
+#include "robots/tank_netsink.h"
 #include "video/netsink.h"
+#include "video/opencvinput.h"
 #include "video/panoramic.h"
+#include "video/randominput.h"
+
+#ifdef TANK_TYPE_EV3
+#include "robots/ev3/mindstorms_imu.h"
+#endif
+
+// Standard C includes
+#include <cstring>
 
 // Standard C++ includes
 #include <chrono>
+#include <memory>
 #include <thread>
 
-using namespace BoBRobotics;
 using namespace std::literals;
+using namespace BoBRobotics;
 
-int bob_main(int, char **)
+int
+bob_main(int, char **)
 {
     std::unique_ptr<Video::Input> camera;
+    std::unique_ptr<HID::Joystick> joystick;
     std::unique_ptr<Video::NetSink> netSink;
-    std::unique_ptr<MindstormsIMU> imu;
 
     // Listen for incoming connection on default port
     Net::Server server;
     auto connection = server.waitForConnection();
-
-    // Read motor commands from network
-    Robots::EV3 tank;
-    tank.setMaximumSpeedProportion(0.7f); // Sensible default
-    tank.readFromNetwork(connection);
 
     // Get panoramic camera
     try {
@@ -38,7 +47,26 @@ int bob_main(int, char **)
         LOGW << e.what();
     }
 
+    // Construct tank of desired type
+    Robots::TANK_TYPE tank;
+
+    // Read motor commands from network
+    tank.readFromNetwork(connection);
+
+    // Try to get joystick
+    try {
+        joystick = std::make_unique<HID::Joystick>();
+        tank.addJoystick(*joystick);
+    } catch (std::runtime_error &e) {
+        // Joystick not found
+        LOGW << e.what();
+    }
+
+#ifdef TANK_TYPE_EV3
+    tank.setMaximumSpeedProportion(0.7f); // Sensible default
+
     // If an IMU is present, stream over network
+    std::unique_ptr<MindstormsIMU> imu;
     try {
         imu = std::make_unique<MindstormsIMU>();
     } catch (...) {
@@ -48,14 +76,16 @@ int bob_main(int, char **)
         LOGI << "Found Mindstorms IMU";
         imu->streamOverNetwork(connection);
     }
+#endif
 
+    if (!joystick && !camera) {
+        // Run on main thread
+        connection.run();
+        return EXIT_SUCCESS;
+    }
     if (camera) {
         // Stream camera synchronously over network
         netSink = std::make_unique<Video::NetSink>(connection, camera->getOutputSize(), camera->getCameraName());
-    } else {
-        // Run server on main thread
-        connection.run();
-        return EXIT_SUCCESS;
     }
 
     // Run server in background,, catching any exceptions for rethrowing
@@ -68,10 +98,13 @@ int bob_main(int, char **)
         // Rethrow any exceptions caught on background thread
         catcher.check();
 
+        const bool joystickUpdate = joystick && joystick->update();
+        const bool cameraUpdate = camera && camera->readFrame(frame);
+
         // If there's a new frame, send it, else sleep
-        if (camera->readFrame(frame)) {
+        if (cameraUpdate) {
             netSink->sendFrame(frame);
-        } else {
+        } else if (!joystickUpdate) {
             std::this_thread::sleep_for(25ms);
         }
     }
