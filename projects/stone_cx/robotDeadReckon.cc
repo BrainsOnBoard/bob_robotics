793f0e5197db36f2999c152f5a14ff70ca96ec92
Alex Dewar <a.dewar@sussex.ac.uk>
2019-06-04 16:01:15 +0100

    Convert to newer BoB networking code
    It wasn't compiling previously.

--- projects/stone_cx/robotDeadReckon.cc
+++ projects/stone_cx/robotDeadReckon.cc
@@ -1,9 +1,3 @@
-// Standard C++ includes
-#include <atomic>
-#include <chrono>
-#include <fstream>
-#include <thread>
-
 // BoB robotics includes
 #include "common/lm9ds1_imu.h"
 #include "common/timer.h"
@@ -21,6 +15,15 @@
 #include "robotParameters.h"
 #include "visualizationCommon.h"
 
+// Standard C includes
+#include <cstring>
+
+// Standard C++ includes
+#include <atomic>
+#include <chrono>
+#include <fstream>
+#include <thread>
+
 using namespace BoBRobotics;
 using namespace BoBRobotics::StoneCX;
 using namespace BoBRobotics::HID;
@@ -34,21 +37,21 @@ void imuThreadFunc(std::atomic<bool> &shouldQuit, std::atomic<float> &heading, u
 {
     // Create IMU interface
     LM9DS1 imu;
-    
+
     // Initialise IMU magnetometer
     LM9DS1::MagnetoSettings magSettings;
     imu.initMagneto(magSettings);
-    
+
     // While quit signal isn't set
     for(numSamples = 0; !shouldQuit; numSamples++) {
         // Wait for magneto to become available
         while(!imu.isMagnetoAvailable()){
         }
-        
+
         // Read magneto
         float magnetoData[3];
         imu.readMagneto(magnetoData);
-            
+
         // Calculate heading angle from magneto data and set atomic value
         heading = atan2(magnetoData[0], magnetoData[2]);
     }
@@ -58,48 +61,52 @@ void imuThreadFunc(std::atomic<bool> &shouldQuit, std::atomic<float> &heading, u
 
 int main(int argc, char *argv[])
 {
+    std::cout << "Use --stream argument to stream over network" << std::endl;
+
     // Simulation rendering parameters
     const unsigned int activityImageWidth = 500;
     const unsigned int activityImageHeight = 1000;
     const float velocityScale = 1.0f / 10.0f;
 
-    const bool streamActivity = (argc > 1);
+    const bool streamActivity = (argc > 1) && strcmp(argv[1], "--stream");
 
     // Create server and sink for sending activity image over network
-    Net::Server server(Net::Socket::DefaultListenPort);
-    Video::NetSink netSink(server, cv::Size(activityImageWidth, activityImageHeight), "activity");
-    
-    // If command line arguments are specified, run server
+    Net::Server server;
+    auto connection = server.waitForConnection();
+    Video::NetSink netSink(connection, cv::Size(activityImageWidth, activityImageHeight), "activity");
+
+    // If command line arguments are specified, run connection
     if(streamActivity) {
         std::cout << "Streaming activity over network" << std::endl;
-        server.runInBackground();
+        connection.runInBackground();
     }
 
     // Create joystick interface
     Joystick joystick;
-    
+
     // Create motor interface
     Robots::TANK_TYPE motor;
-    
+
     // Initialise GeNN
     allocateMem();
     initialize();
     initializeSparse();
-    
+
     // Atomic flag for quitting child threads
     std::atomic<bool> shouldQuit{false};
-    
+
     // Create thread to read from IMU
     unsigned int numIMUSamples = 0;
     std::atomic<float> imuHeading{0.0f};
-    std::thread imuThread(&imuThreadFunc, 
+    std::thread imuThread(&imuThreadFunc,
                           std::ref(shouldQuit), std::ref(imuHeading), std::ref(numIMUSamples));
-    
+
     cv::Mat activityImage(activityImageHeight, activityImageWidth, CV_8UC3, cv::Scalar::all(0));
 
 #ifdef RECORD_SENSORS
     std::ofstream data("data.csv");
 #endif
+
     // Loop until second joystick button is pressed
     bool outbound = true;
     unsigned int numTicks = 0;
@@ -108,10 +115,10 @@ int main(int argc, char *argv[])
     for(;; numTicks++) {
         // Record time at start of tick
         const auto tickStartTime = std::chrono::high_resolution_clock::now();
-        
+
         // Read from joystick
         joystick.update();
-        
+
         // Stop if 2nd button is pressed
         if(joystick.isDown(JButton::B)) {
             break;
@@ -139,7 +146,7 @@ int main(int argc, char *argv[])
         // Pull outputs from device
         pullrCPU4FromDevice();
         pullrCPU1FromDevice();
-        
+
         // If we should be streaming activity
         if(streamActivity) {
             // Pull additional outputs from device
@@ -160,7 +167,7 @@ int main(int argc, char *argv[])
         if(outbound) {
             // Use joystick to drive motor
             motor.drive(joystick, RobotParameters::joystickDeadzone);
-            
+
             // If first button is pressed switch to returning home
             if(joystick.isDown(JButton::A)) {
                 std::cout << "Max CPU4 level r=" << *std::max_element(&rCPU4[0], &rCPU4[Parameters::numCPU4]) << ", i=" << *std::max_element(&iCPU4[0], &iCPU4[Parameters::numCPU4]) << std::endl;
@@ -173,16 +180,16 @@ int main(int argc, char *argv[])
         else {
             driveMotorFromCPU1(motor, (numTicks % 100) == 0);
         }
-        
+
         // Record time at end of tick
         const auto tickEndTime = std::chrono::high_resolution_clock::now();
-        
+
         // Calculate tick duration (in microseconds)
         const int64_t tickMicroseconds = std::chrono::duration_cast<std::chrono::microseconds>(tickEndTime - tickStartTime).count();
-        
+
         // Add to total
         totalMicroseconds += tickMicroseconds;
-        
+
         // If there is time left in tick, sleep for remainder
         if(tickMicroseconds < RobotParameters::targetTickMicroseconds) {
             std::this_thread::sleep_for(std::chrono::microseconds(RobotParameters::targetTickMicroseconds - tickMicroseconds));
@@ -192,18 +199,18 @@ int main(int argc, char *argv[])
             numOverflowTicks++;
         }
     }
-    
+
     // Set quit flag and wait for child threads to complete
     shouldQuit = true;
     imuThread.join();
-    
+
     // Show stats
     std::cout << numOverflowTicks << "/" << numTicks << " ticks overflowed, mean tick time: " << (double)totalMicroseconds / (double)numTicks << "uS, ";
     std::cout << "IMU samples: " << numIMUSamples << ", ";
-    
+
     // Stop motor
     motor.tank(0.0f, 0.0f);
-    
+
     // Exit
     return EXIT_SUCCESS;
 }
