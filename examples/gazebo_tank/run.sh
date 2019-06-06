0394adb8c85c8883908f6f5b1c0fb2ca0189d077
Alex Dewar <a.dewar@sussex.ac.uk>
2019-06-06 17:16:12 +0100

    Fix bugs in run.sh

--- examples/gazebo_tank/run.sh
+++ examples/gazebo_tank/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 trap 'kill %1' SIGINT
-GAZEBO_PLUGIN_PATH=GAZEBO_PLUGIN_PATH:${PWD} gazebo --verbose differential_drive.world & ./simulated_tank_gazebo $1
+GAZEBO_PLUGIN_PATH=$GAZEBO_PLUGIN_PATH:$(dirname "$0") gazebo --verbose differential_drive.world & ./gazebo_tank "$@"
