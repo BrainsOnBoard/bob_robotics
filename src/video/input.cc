a18fd6f097ded9c03abe9a271830cbb07ada5a95
Alex Dewar <a.dewar@sussex.ac.uk>
2019-06-17 15:57:08 +0100

    Make BOB_NOT_IMPLEMENTED macro and use in a few places

--- src/video/input.cc
+++ src/video/input.cc
@@ -1,9 +1,9 @@
 // BoB robotics includes
 #include "common/logging.h"
+#include "common/macros.h"
 #include "video/input.h"
 
 // Standard C++ includes
-#include <stdexcept>
 #include <chrono>
 #include <stdexcept>
 #include <thread>
@@ -56,17 +56,8 @@ Input::needsUnwrapping() const
     return getCameraName() != DefaultCameraName;
 }
 
-void
-Input::setOutputSize(const cv::Size &)
-{
-    throw std::runtime_error("This camera's resolution cannot be changed at runtime");
-}
-
-units::frequency::hertz_t
-Input::getFrameRate() const
-{
-    throw std::runtime_error("The frame rate for this camera is unknown");
-}
+BOB_NOT_IMPLEMENTED(void Input::setOutputSize(const cv::Size &))
+BOB_NOT_IMPLEMENTED(units::frequency::hertz_t Input::getFrameRate() const)
 
 void
 Input::readFrameSync(cv::Mat &outFrame)
