c057ba9a26cc3d133432e5aee1587d1f2964215a
Tenxlenx <liquidmind@me.com>
2019-06-18 15:59:37 +0100

    Restore old camera_360 example

--- examples/camera_360/camera_360_test.cc
+++ examples/camera_360/camera_360_test.cc
@@ -1,18 +1,11 @@
 // BoB robotics includes
-#include "../../common/logging.h"
-#include "../../common/timer.h"
-#include "../../imgproc/opencv_unwrap_360.h"
-#include "../../video/panoramic.h"
-#include "../../common/gps.h"
-#include "../../third_party/units.h"
-#include "../../hid/joystick.h"
-#include "../../robots/norbot.h"
-#include "../../common/lm9ds1_imu.h"
-#include <time.h> 
+#include "common/logging.h"
+#include "common/timer.h"
+#include "imgproc/opencv_unwrap_360.h"
+#include "video/panoramic.h"
 
 // Standard C++ includes
 #include <iostream>
-#include <thread>
 
 // Standard C includes
 #include <cmath>
@@ -20,34 +13,10 @@
 using namespace BoBRobotics;
 using namespace BoBRobotics::ImgProc;
 using namespace BoBRobotics::Video;
-using namespace units::angle;
-using namespace units::length;
 
-void gpsThreadFunc(const char *device, std::mutex &mutex, degree_t &latOut, degree_t &lonOut, meter_t &altitudeOut)
-{
-    //create GPS
-    Gps gps("/dev/ttyACM0");
-
-    std::cout << "GPS created" << std::endl;
-    while(true)
-    {
-        
-        // get gps data
-        degree_t lat,lon;
-        meter_t altitude;
-        bool didGetGps = gps.getPosition(lat,lon, altitude);
-
-        {
-            std::lock_guard<std::mutex> g(mutex);
-            latOut = lat;
-            lonOut = lon;
-            altitudeOut = altitude;
-        }
-    }
-}
 int main()
 {
-    const cv::Size unwrapRes(720, 150);
+    const cv::Size unwrapRes(90, 25);
     const unsigned int outputScale = 10;
 
     // Create panoramic camera and suitable unwrapper
@@ -55,34 +24,21 @@ int main()
     auto unwrapper = cam->createUnwrapper(unwrapRes);
     const auto cameraRes = cam->getOutputSize();
 
-    
-
-
     // Create images
     cv::Mat originalImage(cameraRes, CV_8UC3);
     cv::Mat outputImage(unwrapRes, CV_8UC3);
 
-    constexpr float joystickDeadzone = 0.25f;
-    
-    // Create joystick interface
-    BoBRobotics::HID::Joystick joystick(joystickDeadzone);
-    
-    // Create motor interface
-    BoBRobotics::Robots::Norbot robot;
+    // Create motor
+    cv::namedWindow("Unwrapped", cv::WINDOW_NORMAL);
+    cv::resizeWindow("Unwrapped", unwrapRes.width * outputScale,
+                     unwrapRes.height * outputScale);
 
-    // output file for gps 
-    std::ofstream file("out.csv");
-
-    // for time
-    time_t rawtime;
-    struct tm * timeinfo;
-    
-    degree_t lat,lon;
-    meter_t altitude;
-    std::mutex gpsMutex;
-    std::thread gpsThread(gpsThreadFunc, "/dev/ttyACM0", std::ref(gpsMutex), std::ref(lat), std::ref(lon), std::ref(altitude));
+    cv::namedWindow("Original", cv::WINDOW_NORMAL);
+    cv::resizeWindow("Original", cameraRes.width, cameraRes.height);
 
     {
+        Timer<> timer("Total time:");
+
         unsigned int frame = 0;
         for(frame = 0;; frame++) {
             // Read from camera
@@ -93,34 +49,17 @@ int main()
             // Unwrap
             unwrapper.unwrap(originalImage, outputImage);
 
-            // save camera
-            char filename[255];
-            sprintf(filename, "file_%u.jpg", frame);
-            cv::imwrite(filename, outputImage);
+            // Show frame difference
+            cv::imshow("Original", originalImage);
+            cv::imshow("Unwrapped", outputImage);
 
-            time (&rawtime);
-            timeinfo = localtime (&rawtime);
-            
-            {
-                std::lock_guard<std::mutex> g(gpsMutex);
-                // saving gps coords
-                file << lat.value() << "," << lon.value() << "," << robot.getLeft() << "," << robot.getRight() << "," << frame << "," << asctime(timeinfo) << std::endl;
-            }
-            
-            joystick.update();
-           
-                
-      
-            // Use joystick to drive motor
-            robot.drive(joystick);
-            
-
-            if(joystick.isDown(BoBRobotics::HID::JButton::B)) {
+            if(cv::waitKey(1) == 27) {
                 break;
             }
-            
         }
 
+        const double msPerFrame = timer.get() / (double)frame;
+        std::cout << "FPS:" << 1000.0 / msPerFrame << std::endl;
     }
 
     return EXIT_SUCCESS;
