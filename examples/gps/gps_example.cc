091b155a33313859594c936357ac1083798362ac
Tenxlenx <liquidmind@me.com>
2019-07-05 17:13:51 +0100

    fixed the gps example code, instead of exiting when found an empty string, it should keep trying to read

--- examples/gps/gps_example.cc
+++ examples/gps/gps_example.cc
@@ -11,40 +11,50 @@
 
 int main()
 {
-    //const char *path_linux = "/dev/ttyACM0";       // path for linux systems
-    const char *path_mac = "/dev/cu.usbmodem141401"; // the path for mac is often different!
+    const char *path_linux = "/dev/ttyACM0";       // path for linux systems
+   // const char *path_mac = "/dev/cu.usbmodem141401"; // the path for mac is often different!
 
     BoBRobotics::GPS::Gps gps;
-    gps.connect(path_mac);
+    gps.connect(path_linux);
 
     // if GPS location is invalid, keep trying to get a valid one
-    // if failed 10 times we exit
-    int numTrials = 10;
-    while(gps.getGPSData().gpsQuality == BoBRobotics::GPS::GPSQuality::INVALID && numTrials > 0) {
-        std::this_thread::sleep_for(std::chrono::milliseconds(500));
-        numTrials--;
+    // if failed x times we exit 
+    const int maxTrials = 20;
+    int numTrials = maxTrials;
+    while (numTrials > 0) {
+        try {
+            if (gps.getGPSData().gpsQuality != BoBRobotics::GPS::GPSQuality::INVALID) {
+                std::cout << " we have a valid measurement" << std::endl;
+                break;
+            }
+        } catch(...) {
+            std::cout << " measuring failed, trying again in 1 second " << "[" << maxTrials - numTrials << "/" << maxTrials << "]" << std::endl;
+            std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+            numTrials--;
+        }    
+    }  
+    if (numTrials == 0) {
+        std::cout << " There is no valid gps measurement, please try waiting for the survey in to finish and restart the program " << std::endl;
+        exit(1);
     }
 
-    if (gps.getGPSData().gpsQuality == BoBRobotics::GPS::GPSQuality::INVALID) {
-        throw BoBRobotics::GPS::GPSError("invalid GPS location, please wait until surveying finishes or place the antenna to a better spot");
-    } else {
-        int i = 0;
-        // print for 100 timestep
-        while (i < 100) {
-            try {
-                BoBRobotics::GPS::GPSData data = gps.getGPSData();
-                BoBRobotics::MapCoordinate::GPSCoordinate coord = data.coordinate;
-                BoBRobotics::GPS::TimeStamp time = data.time;
+    int i = 0;
+    // print for 100 timestep
+    while (i < 100) {
+        try {
+            BoBRobotics::GPS::GPSData data = gps.getGPSData();
+            BoBRobotics::MapCoordinate::GPSCoordinate coord = data.coordinate;
+            BoBRobotics::GPS::TimeStamp time = data.time;
 
-                std::cout << "latitude: " << coord.lat.value()<< " longitude: " <<  coord.lon.value() << "[ "
-                         <<  time.hour << ":" << time.minute << ":" <<  time.second << ":" << time.millisecond << "] " << std::endl;
+            std::cout << "latitude: " << coord.lat.value()<< " longitude: " <<  coord.lon.value() << "[ "
+                        <<  time.hour << ":" << time.minute << ":" <<  time.second << ":" << time.millisecond << "] " << std::endl;
 
-                i++;
-            } catch(BoBRobotics::GPS::GPSError &e) { LOG_WARNING << e.what(); }
-            // The gps receiver is set to 5HZ
-            std::this_thread::sleep_for(std::chrono::milliseconds(200));
-        }
+            i++;
+        } catch(BoBRobotics::GPS::GPSError &e) { LOG_WARNING << e.what(); }
+        // The gps receiver is set to 5HZ
+        std::this_thread::sleep_for(std::chrono::milliseconds(200));
     }
+
 
 
     return 0;
