8f72fec97c503de4eee7c1f9b2443ebd23879768
neworderofjamie <J.C.Knight@sussex.ac.uk>
2019-07-16 15:04:24 +0100

    Updated syntax

--- examples/gazebo_omniwheel/gazebo_omniwheel.cc
+++ examples/gazebo_omniwheel/gazebo_omniwheel.cc
@@ -1,6 +1,6 @@
 // BoB robotics includes
-#include "common/gazebo_node.h"
 #include "common/logging.h"
+#include "gazebo/node.h"
 #include "robots/gazebo/omni2d.h"
 
 #include <chrono>
@@ -12,11 +12,11 @@ using namespace std::literals;
 
 int main()
 {
-    auto node = getGazeboNode();
+    auto node = Gazebo::getNode();
     Robots::Gazebo::Omni2D robot(*node, 1_mps);
     LOGI << "Created robot";
     robot.drive(1.f, -1.f, 1.f);
     LOGI << "Driving robot";
     std::this_thread::sleep_for(120s);
-    shutdownGazeboNode();
+    Gazebo::shutDown();
 }
