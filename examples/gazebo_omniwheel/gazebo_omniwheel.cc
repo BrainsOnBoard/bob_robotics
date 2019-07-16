61eddc61766218f4b0fd4fe0c4668c5d9ef62297
neworderofjamie <neworderofjamie@gmail.com>
2019-07-16 17:13:18 +0100

    made omnirobot gazebo demo somewhat more fully-featured

--- examples/gazebo_omniwheel/gazebo_omniwheel.cc
+++ examples/gazebo_omniwheel/gazebo_omniwheel.cc
@@ -1,6 +1,7 @@
 // BoB robotics includes
 #include "common/logging.h"
 #include "gazebo/node.h"
+#include "hid/joystick.h"
 #include "robots/gazebo/omni2d.h"
 
 #include <chrono>
@@ -14,9 +15,22 @@ int main()
 {
     auto node = Gazebo::getNode();
     Robots::Gazebo::Omni2D robot(*node, 1_mps);
-    LOGI << "Created robot";
-    robot.drive(1.f, -1.f, 1.f);
-    LOGI << "Driving robot";
-    std::this_thread::sleep_for(120s);
+    HID::Joystick joystick(0.25f);
+    robot.addJoystick(joystick);
+
+    std::cout << "Drive the car using the two thumbsticks: each stick is for one motor" << std::endl;
+
+    do {
+        // Check for joystick events
+        if (!joystick.update()) {
+            // A small delay so we don't hog CPU
+            std::this_thread::sleep_for(5ms);
+        }
+    } while (!joystick.isPressed(HID::JButton::B));
+
+    // Make sure to shut everything down.
     Gazebo::shutDown();
+    std::cout <<"Shutting down...\n";
+
+    return EXIT_SUCCESS;
 }
