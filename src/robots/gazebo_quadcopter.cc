c60684a3b4f1f15fceb2c0468f298775854451a6
Shinjan Mitra <shinjanxp@gmail.com>
2019-07-19 10:45:43 +0530

    Parameter tuning to achieve stable flight

--- src/robots/gazebo_quadcopter.cc
+++ src/robots/gazebo_quadcopter.cc
@@ -6,41 +6,43 @@ namespace Robots {
 
 void GazeboQuadCopter::takeOff()
 {
-    //takeoff
+    m_Thrust = 0.6;
+    motorMixing();
 }
 
 void GazeboQuadCopter::land()
 {
-    //land
+    m_Thrust = 0.5;
+    motorMixing();
 }
 void GazeboQuadCopter::setPitch(float pitch)
 {
-    m_Pitch = pitch*0.1;
+    m_Pitch = pitch*0.01;
     motorMixing();
 }
 void GazeboQuadCopter::setRoll(float right)
 {
-    m_Roll = right*0.2;
+    m_Roll = right*0.01;
     motorMixing();
 }
 void GazeboQuadCopter::setVerticalSpeed(float up)
 {
-    m_Thrust = up;
+    m_Thrust = up*0.5 + 0.6;
     motorMixing();
 }
 void GazeboQuadCopter::setYawSpeed(float right)
 {
-    m_Yaw = right*0.1;
+    m_Yaw = right*0.01;
     motorMixing();
 }
 void GazeboQuadCopter::motorMixing()
 {
     float m_fr, m_fl, m_br, m_bl;
-    m_fr = m_Thrust + m_Yaw + m_Pitch + m_Roll;
-    m_fl = m_Thrust - m_Yaw + m_Pitch - m_Roll;
-    m_br = m_Thrust - m_Yaw - m_Pitch + m_Roll;
-    m_bl = m_Thrust + m_Yaw - m_Pitch - m_Roll;
-    // std::cout << m_fr<< m_fl<< m_br<< m_bl << std::endl;
+    m_fr = m_Thrust + m_Yaw - m_Pitch - m_Roll;
+    m_fl = m_Thrust - m_Yaw - m_Pitch + m_Roll;
+    m_br = m_Thrust - m_Yaw + m_Pitch - m_Roll;
+    m_bl = m_Thrust + m_Yaw + m_Pitch + m_Roll;
+    std::cout << m_fl << "\t\t"<< m_fr << std::endl << "\tX\n" <<  m_bl<< "\t\t" << m_br << std::endl;
     gazebo::msgs::Set(&msg, ignition::math::Quaterniond(m_fr, m_bl, m_fl, m_br));
     // Send the message
     pub->Publish(msg);
