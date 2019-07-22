414ed08e0fdaeb5e85449922dd877f0a0943c648
Alex Dewar <a.dewar@sussex.ac.uk>
2019-07-22 21:23:03 +0100

    Trim trailing whitespace

--- arduino/i2c_slave_code_mecanum/i2c_slave_code_mecanum.ino
+++ arduino/i2c_slave_code_mecanum/i2c_slave_code_mecanum.ino
@@ -1,5 +1,5 @@
-/* 
-This is a simple sketch based on the Adafruit DC motor test that controls the 
+/*
+This is a simple sketch based on the Adafruit DC motor test that controls the
 2Dbee's four motors. The motor direction is set using the bits in the first byte
 transmitted, then the remaining 4 bytes set the individual motor speeds.
 */
@@ -7,12 +7,12 @@ transmitted, then the remaining 4 bytes set the individual motor speeds.
 #include <Wire.h>
 #include <Adafruit_MotorShield.h>
 #include "utility/Adafruit_MS_PWMServoDriver.h"
-#include <Servo.h> 
+#include <Servo.h>
 
 // Create the motor shield object with the default I2C address
-Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
+Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 // Or, create it with a different I2C address (say for stacking)
-// Adafruit_MotorShield AFMS = Adafruit_MotorShield(0x61); 
+// Adafruit_MotorShield AFMS = Adafruit_MotorShield(0x61);
 
 // Assign motors to ports
 Adafruit_DCMotor *frontRightMotor = AFMS.getMotor(1);
@@ -35,7 +35,7 @@ void setup() {
 
   AFMS.begin();  // create with the default frequency 1.6KHz
   //AFMS.begin(1000);  // OR with a different frequency, say 1KHz
-  
+
   // turn on motor
   rearRightMotor->run(RELEASE);
   rearLeftMotor->run(RELEASE);
@@ -53,7 +53,7 @@ void setup() {
   frontLeftMotor->run(FORWARD);
 
   Serial.println("Setup complete");
-  
+
 }
 
 uint8_t j = 0;
@@ -92,7 +92,7 @@ void serialEvent(){
     }
 
     if (i > 7) {
- 
+
     byte m1 = in_data[0];
     byte m2 = in_data[1];
     byte m3 = in_data[2];
@@ -103,30 +103,30 @@ void serialEvent(){
       frontRightMotor->run(BACKWARD);
     }
     j=in_data[4];
-    frontRightMotor->setSpeed(j);  
+    frontRightMotor->setSpeed(j);
     if (m2 > 0) {
       frontLeftMotor->run(FORWARD);
     } else {
       frontLeftMotor->run(BACKWARD);
     }
     j=in_data[5];
-    frontLeftMotor->setSpeed(j);  
+    frontLeftMotor->setSpeed(j);
     if (m3 > 0) {
       rearRightMotor->run(BACKWARD);
     } else {
       rearRightMotor->run(FORWARD);
     }
     j=in_data[6];
-    rearRightMotor->setSpeed(j);  
+    rearRightMotor->setSpeed(j);
     if (m4 > 0) {
       rearLeftMotor->run(BACKWARD);
     } else {
       rearLeftMotor->run(FORWARD);
     }
     j=in_data[7];
-    rearLeftMotor->setSpeed(j);  
+    rearLeftMotor->setSpeed(j);
     //input=Serial.read();
-    //pan.write(input);  
+    //pan.write(input);
     }
     has_serial = 1;
    }
@@ -146,7 +146,7 @@ void loop() {
       frontRightMotor->run(FORWARD);
       frontLeftMotor->setSpeed(0);
       frontLeftMotor->run(FORWARD);
-  
+
       rearRightMotor->run(RELEASE);
       rearLeftMotor->run(RELEASE);
       frontRightMotor->run(RELEASE);
@@ -159,5 +159,5 @@ void loop() {
 
   //Serial.println("loop...");
   delay(3);
- 
+
 }
