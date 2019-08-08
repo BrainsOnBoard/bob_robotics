def7a8a534bdf0fbfeada8b905d5aca8705f78e0
neworderofjamie <neworderofjamie@gmail.com>
2019-08-08 11:50:43 +0100

    * fixed typo * added right trigger

--- src/hid/joystick_glfw_keyboard.cc
+++ src/hid/joystick_glfw_keyboard.cc
@@ -23,6 +23,9 @@ bool JoystickGLFWKeyboard::updateState()
     setAxisStateKeys(GLFW_KEY_UP, GLFW_KEY_DOWN, JAxis::RightStickVertical);
     setAxisStateKeys(GLFW_KEY_LEFT, GLFW_KEY_RIGHT, JAxis::RightStickHorizontal);
 
+    // Use page up and down for right trigger
+    setAxisStateKeys(GLFW_KEY_PAGE_UP, GLFW_KEY_PAGE_DOWN, JAxis::RightTrigger);
+
     setButtonStateKey(GLFW_KEY_1, JButton::A);
     setButtonStateKey(GLFW_KEY_2, JButton::B);
     setButtonStateKey(GLFW_KEY_3, JButton::X);
@@ -37,7 +40,7 @@ void JoystickGLFWKeyboard::setAxisStateKeys(int negativeKey, int positiveKey, JA
         setState(axis, -1.0f, false);
     }
     else if(glfwGetKey(m_Window, positiveKey) == GLFW_PRESS) {
-        setState(axis, -1.0f, false);
+        setState(axis, 1.0f, false);
     }
     else if((glfwGetKey(m_Window, negativeKey) == GLFW_RELEASE) || (glfwGetKey(m_Window, positiveKey) == GLFW_RELEASE)) {
         setState(axis, 0.0f, false);
