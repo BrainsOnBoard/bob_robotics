df6ee9f12237c036015e0f4e5c8f71c017587383
Alex Dewar <a.dewar@sussex.ac.uk>
2019-08-12 18:20:16 +0100

    Add option of using mask for bee-eye filter

--- include/imgproc/bee_eye.h
+++ include/imgproc/bee_eye.h
@@ -21,36 +21,14 @@ class Map
 public:
     //! Create x and y pixel maps
     Map(const float eyeData[][4], const int eyeDataLength,
-        const int eyeSize[2], const int imageSize[2])
-    {
-        cv::Size sz_out(eyeSize[0], eyeSize[1]);
-
-        m_MapX.create(sz_out, CV_32FC1);
-        m_MapY.create(sz_out, CV_32FC1);
-        for (int i = 0; i < eyeDataLength; i++) {
-            // left eye
-            m_MapX.at<float>(eyeData[i][3], 15 + eyeData[i][2]) = floor(eyeData[i][0]);
-            m_MapY.at<float>(eyeData[i][3], 15 + eyeData[i][2]) = floor(eyeData[i][1]);
-
-            // right eye
-            m_MapX.at<float>(eyeData[i][3],
-                             720 - 316 - eyeSize[0] - eyeData[i][2]) =
-                    imageSize[0] - floor(eyeData[i][0]);
-            m_MapY.at<float>(eyeData[i][3],
-                             720 - 316 - eyeSize[0] - eyeData[i][2]) =
-                    floor(eyeData[i][1]);
-        }
-    }
+        const int eyeSize[2], const int imageSize[2], bool useMask = true);
 
     //! Takes an input image and transforms it with the Giger bee-eye transform
-    inline void getEyeView(const cv::Mat &in, cv::Mat &out)
-    {
-        remap(in, out, m_MapX, m_MapY, cv::INTER_NEAREST);
-    }
+    void getEyeView(const cv::Mat &in, cv::Mat &out);
 
 private:
-    cv::Mat m_MapX, m_MapY;
-};
+    cv::Mat m_MapX, m_MapY, m_Mask;
+}; // Map
 } // BeeEye
 } // ImgProc
 } // BoBRobotics
