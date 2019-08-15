137076ecda39c1b95eabdf927c8e53e97286498e
Alex Dewar <a.dewar@sussex.ac.uk>
2019-08-15 14:36:06 +0100

    imgui: Automatically add deps + suppress warning

--- cmake/bob_robotics.cmake
+++ cmake/bob_robotics.cmake
@@ -639,6 +639,12 @@ function(BoB_third_party)
                 BoB_add_link_libraries(ev3dev)
             elseif(${module} STREQUAL imgui)
                 BoB_add_link_libraries(imgui)
+
+                # Extra libs needed
+                BoB_external_libraries(glew sfml-graphics)
+
+                # Suppress warning
+                add_compile_flags(-Wno-stringop-truncation)
             endif()
 
             # Checkout git submodules under this path
