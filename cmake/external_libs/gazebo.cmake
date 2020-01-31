f0ff24355fb6154175ed92911a474f060191d74d
Alex Dewar <a.dewar@sussex.ac.uk>
2020-01-31 17:55:06 +0000

    Remove out of date comment

--- cmake/external_libs/gazebo.cmake
+++ cmake/external_libs/gazebo.cmake
@@ -1,6 +1,3 @@
-# If Gazebo is added as a dependency multiple times (e.g. from
-# multiple CMakeLists.txt files) then I'm getting an error from the
-# target FreeImage::FreeImage being created multiple times - AD
 BoB_add_include_directories(${GAZEBO_INCLUDE_DIRS})
 BoB_add_link_libraries(${GAZEBO_LIBRARIES})
 link_directories(${GAZEBO_LIBRARY_DIRS})
