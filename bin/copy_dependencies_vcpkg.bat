75fbc1f5bc4148b4aafbb996bc745a8a7aa4f9b2
neworderofjamie <neworderofjamie@gmail.com>
2020-05-20 10:20:34 +0100

    Added @ and maybe fixed quote-handling issue

--- bin/copy_dependencies_vcpkg.bat
+++ bin/copy_dependencies_vcpkg.bat
@@ -1,8 +1,8 @@
-ECHO OFF
+@ECHO OFF
 SETLOCAL ENABLEDELAYEDEXPANSION
 
 REM Loop through dependencies of dll/executable, filtering out lines which contain dll names
-FOR /F %%d IN ('dumpbin /dependents %~1 ^| FINDSTR /I /R "[a-zA-Z_-]*\.dll"') DO (
+FOR /F %%d IN ('dumpbin /dependents "%~1" ^| FINDSTR /I /R "[a-zA-Z_-]*\.dll"') DO (
     REM If dll is supplied by VCPKG (others are likely to be in system32)
     SET "VCPKG_FILE=%~2\bin\%%d"
     IF EXIST !VCPKG_FILE! (
