6e1750432d95ee3199a2cd9a45d660a46a4f53c7
Alex Dewar <a.dewar@sussex.ac.uk>
2020-10-05 20:24:45 +0100

    Remove tests for old code

--- tests/test_headers/geometry.h
+++ tests/test_headers/geometry.h
@@ -6,8 +6,6 @@
 #include <array>
 #include <algorithm>
 
-using SL = StraightLine;
-
 template<int Rows1, int Cols1, int Rows2, int Cols2>
 void EXPECT_EIGEN_EQ(const Eigen::Matrix<double, Rows1, Cols1> &m1, const Eigen::Matrix<double, Rows2, Cols2> &m2)
 {
@@ -33,80 +31,4 @@ TEST(Geometry, ResizeObjectBy) {
     resizePolygonBy(object, 0.5_m);
 
     EXPECT_EIGEN_EQ(object, objectResized);
-}
-
-TEST(Geometry, StraightLineFromPoints) {
-    constexpr int numTests = 4;
-    Eigen::Matrix<double, numTests, 2> startPoints, endPoints;
-    startPoints << 1, -0.5,
-                  -1, -0.5,
-                  -1,  0.5,
-                   1,  0.5,
-    endPoints << -1, -0.5,
-                 -1,  0.5,
-                  1,  0.5,
-                  1, -0.5;
-
-    constexpr std::array<SL, numTests> lines = {
-        SL{ 0.0, -0.5 },
-        SL{ inf(), -1.0 },
-        SL{ 0.0, 0.5 },
-        SL{ inf(), 1.0 }
-    };
-
-    EXPECT_EQ(inf(), inf());
-    for (int i = 0; i < numTests; i++) {
-        const auto testLine = SL::fromPoints(startPoints.row(i), endPoints.row(i));
-        const auto &actualLine = lines[static_cast<size_t>(i)];
-        EXPECT_EQ(actualLine, testLine);
-    }
-}
-
-TEST(Geometry, Perpendicular) {
-    constexpr int numTests = 4;
-
-    Eigen::Matrix<double, numTests, 2> vertices;
-    vertices << -1, -0.5,
-                -1,  0.5,
-                 1,  0.5,
-                 1, -0.5;
-    constexpr std::array<SL, numTests> lines = {
-        SL{ 0.0, -0.5 },
-        SL{ inf(), -1.0 },
-        SL{ 0.0, 0.5 },
-        SL{ inf(), 1.0 }
-    };
-    const std::array<SL, numTests> actualLines = {
-        SL{ inf(), vertices(0, 0) },
-        SL{ 0.0, vertices(1, 1) },
-        SL{ inf(), vertices(2, 0) },
-        SL{ 0.0, vertices(3, 1) }
-    };
-
-    for (size_t i = 0; i < numTests; i++) {
-        const auto testLine = lines[i].perpendicular(vertices.row(static_cast<int>(i)));
-        EXPECT_EQ(actualLines[i], testLine);
-    }
-}
-
-TEST(Geometry, PointOnPerpendicular) {
-    constexpr int numTests = 4;
-
-    Eigen::Matrix<double, numTests, 2> vertices;
-    vertices << -1, -0.5,
-                -1,  0.5,
-                 1,  0.5,
-                 1, -0.5;
-    constexpr std::array<SL, numTests> lines = {
-        SL{ 0.0, -0.5 },
-        SL{ inf(), -1.0 },
-        SL{ 0.0, 0.5 },
-        SL{ inf(), 1.0 }
-    };
-
-    for (size_t i = 0; i < numTests; i++) {
-        const auto startPoint = vertices.row(static_cast<int>(i));
-        const auto testLine = lines[i].perpendicular(startPoint);
-        EXPECT_TRUE(testLine.isPointOnLine(startPoint));
-    }
 }
