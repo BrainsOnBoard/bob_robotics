3244f7eeecb5af6bbb8f51a61fbb9c7b820c2b70
Alex Dewar <a.dewar@sussex.ac.uk>
2020-10-06 19:26:48 +0100

    Disable OpenMP on Windows for now
    MSVC is complaining about my (ab)use of OpenMP, so just disable it for
    now.

--- cmake/external_libs/openmp.cmake
+++ cmake/external_libs/openmp.cmake
@@ -1,4 +1,6 @@
-if(OpenMP_CXX_FOUND)
+if(WIN32)
+    message(WARNING "OpenMP support is disabled on Windows; code will be single threaded")
+elseif(OpenMP_CXX_FOUND)
     BoB_add_link_libraries(OpenMP::OpenMP_CXX)
 else()
     # Let's not make it a fatal error if we can't find it
