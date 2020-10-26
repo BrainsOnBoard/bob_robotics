05e6c5b66ca7bd2116cbdbae17ab290c5c652452
Alex Dewar <a.dewar@sussex.ac.uk>
2020-10-26 16:31:37 +0000

    Use a more robust strategy for finding OpenCV

--- cmake/external_libs/opencv.cmake
+++ cmake/external_libs/opencv.cmake
@@ -1,5 +1,25 @@
-if(APPLE)
-    BoB_add_pkg_config_libraries(opencv4)
-else()
-    BoB_find_package(OpenCV REQUIRED)
+# Try looking for OpenCV in 3 ways:
+#   1) Look for CMake package
+#   2) Look for a pkg-config package called opencv4
+#   3) Look for a pkg-config package called opencv
+#
+# Hopefully this should work with all platforms/versions of OpenCV.
+if(NOT OpenCV_FOUND)
+    find_package(OpenCV QUIET)
+    if (OpenCV_FOUND)
+
+    else()
+        pkg_check_modules(OpenCV QUIET opencv4)
+        if (NOT OpenCV_FOUND)
+            pkg_check_modules(OpenCV QUIET opencv)
+            if (NOT OpenCV_FOUND)
+                message(FATAL_ERROR "Could not find OpenCV")
+            endif()
+        endif()
+    endif()
+
+    message("-- Found OpenCV v${OpenCV_VERSION}")
+    BoB_add_include_directories(${OpenCV_INCLUDE_DIRS})
+    BoB_add_link_libraries(${OpenCV_LIBS} ${OpenCV_LINK_LIBRARIES})
+    BoB_add_link_directories(${OpenCV_LIB_DIR})
 endif()
