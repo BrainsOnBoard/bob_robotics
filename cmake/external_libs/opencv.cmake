56163490f038f0941c38fc025e86242b4077d97d
Alex Dewar <a.dewar@sussex.ac.uk>
2020-10-27 11:50:05 +0000

    CMake: We need to account for broken versions of OpenCV's cmake file

--- cmake/external_libs/opencv.cmake
+++ cmake/external_libs/opencv.cmake
@@ -1,20 +1,19 @@
-# Try looking for OpenCV in 3 ways:
-#   1) Look for CMake package
-#   2) Look for a pkg-config package called opencv4
-#   3) Look for a pkg-config package called opencv
+# Using the OpenCV CMake package has broken the build in a couple of cases in
+# the past (on macOS and once when built from source on Ubuntu), because it
+# sometimes (with old versions of CMake, maybe?) unconditionally sets the C++
+# version to C++11, even though we explicitly specify that we need C++14.
 #
-# Hopefully this should work with all platforms/versions of OpenCV.
-if(NOT OpenCV_FOUND)
-    find_package(OpenCV QUIET)
-    if (OpenCV_FOUND)
-
-    else()
-        pkg_check_modules(OpenCV QUIET opencv4)
+# So we try looking for OpenCV in 3 ways:
+#   1) Look for a pkg-config package called opencv4
+#   2) Look for a pkg-config package called opencv
+#   3) Look for CMake package
+find_package(PkgConfig QUIET)
+if(PkgConfig_FOUND)
+    pkg_check_modules(OpenCV QUIET opencv4)
+    if (NOT OpenCV_FOUND)
+        pkg_check_modules(OpenCV QUIET opencv)
         if (NOT OpenCV_FOUND)
-            pkg_check_modules(OpenCV QUIET opencv)
-            if (NOT OpenCV_FOUND)
-                message(FATAL_ERROR "Could not find OpenCV")
-            endif()
+            message(FATAL_ERROR "Could not find OpenCV")
         endif()
     endif()
 
@@ -22,4 +21,6 @@ if(NOT OpenCV_FOUND)
     BoB_add_include_directories(${OpenCV_INCLUDE_DIRS})
     BoB_add_link_libraries(${OpenCV_LIBS} ${OpenCV_LINK_LIBRARIES})
     BoB_add_link_directories(${OpenCV_LIB_DIR})
+else()
+    BoB_find_package(OpenCV)
 endif()
