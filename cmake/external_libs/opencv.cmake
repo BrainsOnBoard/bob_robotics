ec118a16aa6ebb333af48f2e943a7342db0f0261
Alex Dewar <a.dewar@sussex.ac.uk>
2020-10-27 12:47:51 +0000

    CMake: Make using pkg-config to find OpenCV optional as this *also* breaks things :-(

--- cmake/external_libs/opencv.cmake
+++ cmake/external_libs/opencv.cmake
@@ -1,19 +1,29 @@
 # Using the OpenCV CMake package has broken the build in a couple of cases in
 # the past (on macOS and once when built from source on Ubuntu), because it
 # sometimes (with old versions of CMake, maybe?) unconditionally sets the C++
-# version to C++11, even though we explicitly specify that we need C++14.
+# version to C++11, even though we explicitly specify that we need C++14. On the
+# other hand, using pkg-config doesn't always find the contrib libs seemingly
+# (on JK's machine). Also, the CMake package isn't always present so we can't
+# rely on it. What a mess.
 #
-# So we try looking for OpenCV in 3 ways:
-#   1) Look for a pkg-config package called opencv4
-#   2) Look for a pkg-config package called opencv
-#   3) Look for CMake package
-find_package(PkgConfig QUIET)
-if(PkgConfig_FOUND)
-    pkg_check_modules(OpenCV QUIET opencv4)
+# By default we try looking for OpenCV in 3 ways:
+#   1) Look for CMake package
+#   2) Look for a pkg-config package called opencv4
+#   3) Look for a pkg-config package called opencv
+#
+# We also provide an extra option to explicitly use pkg-config.
+if(NOT OpenCV_FOUND)
+    if(NOT FIND_OPENCV_WITH_PKGCONFIG)
+        find_package(OpenCV QUIET)
+    endif()
     if (NOT OpenCV_FOUND)
-        pkg_check_modules(OpenCV QUIET opencv)
+        find_package(PkgConfig REQUIRED)
+        pkg_check_modules(OpenCV QUIET opencv4)
         if (NOT OpenCV_FOUND)
-            message(FATAL_ERROR "Could not find OpenCV")
+            pkg_check_modules(OpenCV QUIET opencv)
+            if (NOT OpenCV_FOUND)
+                message(FATAL_ERROR "Could not find OpenCV")
+            endif()
         endif()
     endif()
 
@@ -21,6 +31,4 @@ if(PkgConfig_FOUND)
     BoB_add_include_directories(${OpenCV_INCLUDE_DIRS})
     BoB_add_link_libraries(${OpenCV_LIBS} ${OpenCV_LINK_LIBRARIES})
     BoB_add_link_directories(${OpenCV_LIB_DIR})
-else()
-    BoB_find_package(OpenCV)
 endif()
