5d8032bda0e62fa2f9cb980511548ba585c41d40
Alex Dewar <a.dewar@sussex.ac.uk>
2020-11-02 16:02:15 +0000

    Add an image database example

---
+++ examples/image_database/write_example.cc
@@ -0,0 +1,59 @@
+// BoB robotics includes
+#include "common/path.h"
+#include "navigation/image_database.h"
+#include "video/randominput.h"
+
+// Third-party includes
+#include "third_party/units.h"
+
+using namespace BoBRobotics;
+using namespace units::length;
+using namespace units::literals;
+
+int bobMain(int, char **argv)
+{
+    // Make a new image database; make folder with random name in current folder
+    const auto programPath = filesystem::path{ argv[0] }.parent_path();
+    const auto imdbPath = Path::getNewPath(programPath);
+    Navigation::ImageDatabase imdb{ imdbPath };
+
+    // Use randomly generated images
+    Video::RandomInput<> cam{ { 180, 50 }, "random", /*seed=*/42 };
+
+    /*
+     * Start recording data, using a single extra field called Sensor value. You
+     * can have as many or as few extra fields as you want (or none...): just
+     * pass in all the names to this function.
+     *
+     * // No extra fields
+     * auto recorder = imdb.getRouteRecorder();
+     *
+     * // Two extra fields
+     * auto recorder = imdb.getRouteRecorder("Value 1", "Value 2");
+     *
+     * // ...etc.
+     */
+    auto recorder = imdb.getRouteRecorder("Sensor value");
+
+    // Save camera info (not essential, but useful so we know resolution etc.)
+    recorder.getMetadataWriter() << "camera" << cam;
+
+    Vector3<millimeter_t> pos;
+    pos.z() = 10_cm;
+    cv::Mat image;
+    double sensorValue = 0;
+
+    // Save some random images into database at given positions
+    for (int i = 0; i < 5; i++) {
+        cam.readFrameSync(image);
+
+        // Save image and info (including the reading from our extra dummy sensor)
+        recorder.record(pos, 0_deg, image, sensorValue);
+
+        pos.x() += 0.5_m;
+        pos.y() += 0.25_m;
+        sensorValue += 0.1;
+    }
+
+    return EXIT_SUCCESS;
+}
