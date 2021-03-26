d7c6645898f5efd983eff2cd3e82c959da12dd4b
Alex Dewar <a.dewar@sussex.ac.uk>
2021-03-26 15:30:58 +0000

    image_database_browser: Allow for skipping frames
    This is useful when loading huge databases.

--- tools/image_database_browser/image_database_browser.cc
+++ tools/image_database_browser/image_database_browser.cc
@@ -4,6 +4,7 @@
 #include "os/keycodes.h"
 
 // Third-party includes
+#include "third_party/CLI11.hpp"
 #include "third_party/path.h"
 
 // OpenCV
@@ -19,24 +20,32 @@ using namespace BoBRobotics;
 
 int bobMain(int argc, char **argv)
 {
-    constexpr size_t FrameSkip = 1;
+    size_t frameSkip = 1;
 
-    // Check we have one command-line argument
-    BOB_ASSERT(argc == 2);
+    // Handle command-line arguments
+    CLI::App app{ "Tool for examining the contents of an image database." };
+    app.add_option("-s,--skip-frames", frameSkip, "Number of frames to skip when reading");
+    app.allow_extras();
+    CLI11_PARSE(app, argc, argv);
+    BOB_ASSERT(frameSkip > 0);
+    if (app.remaining_size() != 1) {
+        std::cout << app.help();
+        return 1;
+    }
 
     // Load database metadata
-    const Navigation::ImageDatabase database(argv[1]);
+    const Navigation::ImageDatabase database{ app.remaining()[0] };
     BOB_ASSERT(!database.empty());
 
     // Load images
     std::cout << "Loading images..." << std::endl;
-    const auto images = database.loadImages({}, FrameSkip, false);
+    const auto images = database.loadImages({}, frameSkip, false);
 
     // Iterate through images with arrow keys
     cv::namedWindow(argv[0]);
     for (size_t i = 0; i < images.size();) {
         std::stringstream ssTitle, ssNumber;
-        const auto pos = database[i * FrameSkip].position;
+        const auto pos = database[i * frameSkip].position;
         ssTitle << database.getName() << " [" << i << "/" << images.size() << "]"
                 << " (" << pos[0] << ", " << pos[1] << ", " << pos[2] << ")";
         cv::imshow(argv[0], images[i]);
