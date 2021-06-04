6819b5a25debadc82bca8431d7fa5f3117ad25d2
Alex Dewar <a.dewar@sussex.ac.uk>
2021-06-04 08:45:26 +0100

    Fix whitespace; remove static attributes for functions

--- examples/dct_hash/dct_hash.cc
+++ examples/dct_hash/dct_hash.cc
@@ -1,29 +1,30 @@
 #include "imgproc/dct_hash.h"
 #include <opencv2/core.hpp>
-#include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
+#include <opencv2/imgcodecs.hpp>
 
-int bobMain(int, char **)
+int
+bobMain(int, char **)
 {
-	std::string image_path1 = "library1.jpg";
-	std::string image_path2 = "library2.jpg";
+    std::string image_path1 = "library1.jpg";
+    std::string image_path2 = "library2.jpg";
     cv::Mat img1 = imread(image_path1, cv::IMREAD_GRAYSCALE);
-	cv::Mat img2 = imread(image_path2, cv::IMREAD_GRAYSCALE);
+    cv::Mat img2 = imread(image_path2, cv::IMREAD_GRAYSCALE);
 
-	// convert to calculate dct
-	img1.convertTo(img1, CV_32F, 1.0/255);
-	img2.convertTo(img2, CV_32F, 1.0/255);
+    // convert to calculate dct
+    img1.convertTo(img1, CV_32F, 1.0 / 255);
+    img2.convertTo(img2, CV_32F, 1.0 / 255);
 
-	cv::imshow("Display window1", img1);
-	cv::imshow("Display window2", img2);
+    cv::imshow("Display window1", img1);
+    cv::imshow("Display window2", img2);
 
-	auto hash1 = DCTHash::computeHash(img1);
-	auto hash2 = DCTHash::computeHash(img2);
+    auto hash1 = DCTHash::computeHash(img1);
+    auto hash2 = DCTHash::computeHash(img2);
 
-	int distance = DCTHash::distance(hash1,hash2);
-	std::cout << "the distance between the 2 images is " << distance << std::endl;
+    int distance = DCTHash::distance(hash1, hash2);
+    std::cout << "the distance between the 2 images is " << distance << std::endl;
 
-	cv::waitKey(5000); // Wait for 5 sec in the window
+    cv::waitKey(5000); // Wait for 5 sec in the window
 
     return 0;
 }
--- include/imgproc/dct_hash.h
+++ include/imgproc/dct_hash.h
@@ -1,43 +1,44 @@
 #pragma once
 
 // Standard C++ includes
-#include <iostream>
-#include <string>
 #include <algorithm>
 #include <bitset>
+#include <iostream>
+#include <string>
 // Standard C includes
 #include <cmath>
 
 //opencv
 #include <opencv2/opencv.hpp>
 
-namespace DCTHash
+namespace DCTHash {
+//! computes the DCT hash
+inline std::bitset<64>
+computeHash(const cv::Mat &in)
 {
-    //! computes the DCT hash
-    inline static std::bitset<64> computeHash(const cv::Mat &in) {
+    cv::Mat dct_mat;
+    cv::dct(in, dct_mat);
+    cv::Mat rect(dct_mat, { 0, 0, 8, 8 });             // we only need the low 8x8 frequencies
+    std::sort(rect.begin<float>(), rect.end<float>()); // sorting to get median value
+    const float median = rect.at<float>(-1 + rect.size().height / 2, rect.size().width - 1);
+    rect = { dct_mat, { 0, 0, 8, 8 } };                // re-assigning so we get the correct order back
 
-        cv::Mat dct_mat;
-        cv::dct(in, dct_mat);
-        cv::Mat rect(dct_mat,{ 0, 0, 8, 8});                // we only need the low 8x8 frequencies
-        std::sort(rect.begin<float>(),rect.end<float>());   // sorting to get median value
-        const float median= rect.at<float>(-1+rect.size().height/2,rect.size().width-1); 
-        rect = {dct_mat,{ 0, 0, 8, 8}};                     // re-assigning so we get the correct order back
-	
-        std::bitset<64> binary;
-        for (size_t i = 0; i < 64; i++) {
-            if(reinterpret_cast<float *>(rect.data)[i] > median) {
-                binary.set(i, 1);
-            }
+    std::bitset<64> binary;
+    for (size_t i = 0; i < 64; i++) {
+        if (reinterpret_cast<float *>(rect.data)[i] > median) {
+            binary.set(i, 1);
         }
-		return binary;
     }
-
-    //! computes the hamming distance between 2 image hashes
-    inline static int distance(const std::bitset<64> &hash1, const std::bitset<64> &hash2) {
-        const std::bitset<64> delta = hash1 ^ hash2;
-        const int distance = delta.count();
-        return distance;
-    }
-
+    return binary;
 }
 
+//! computes the hamming distance between 2 image hashes
+inline int
+distance(const std::bitset<64> &hash1, const std::bitset<64> &hash2)
+{
+    const std::bitset<64> delta = hash1 ^ hash2;
+    const int distance = delta.count();
+    return distance;
+}
+
+}
