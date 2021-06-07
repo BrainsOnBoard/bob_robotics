b14bb49b43e1abcf65d73428f2c726f8bee5d569
Alex Dewar <a.dewar@sussex.ac.uk>
2021-06-07 11:31:11 +0100

    Use lower level of precision for HOG + CCoeff tests

--- tests/perfect_memory.cc
+++ tests/perfect_memory.cc
@@ -81,7 +81,7 @@ TEST(PerfectMemory, DISABLED_SampleImageCCoeffMaskWindow)
 
 template<class Store>
 void
-testHog(const std::string &filename, std::pair<size_t, size_t> window)
+testHog(const std::string &filename, std::pair<size_t, size_t> window, float precision)
 {
     /*
      * Using GTest's default EXPECT_FLOAT_EQ() method gives test failures when
@@ -91,7 +91,6 @@ testHog(const std::string &filename, std::pair<size_t, size_t> window)
      * for this test.
      *      -- AD
      */
-    constexpr float precision = 1e-7f;
 
     using namespace BoBRobotics;
 
@@ -112,20 +111,20 @@ testHog(const std::string &filename, std::pair<size_t, size_t> window)
 
 TEST(PerfectMemory, SampleImageHOG)
 {
-    testHog<PerfectMemoryStore::HOG<>>("pm_hog.bin", {});
+    testHog<PerfectMemoryStore::HOG<>>("pm_hog.bin", {}, 1e-7);
 }
 
 TEST(PerfectMemory, SampleImageHogWindow)
 {
-    testHog<PerfectMemoryStore::HOG<>>("window_pm_hog.bin", { 0, 10 });
+    testHog<PerfectMemoryStore::HOG<>>("window_pm_hog.bin", { 0, 10 }, 1e-7);
 }
 
 TEST(PerfectMemory, SampleImageHOGCorrCoefficient)
 {
-    testHog<PerfectMemoryStore::HOG<CorrCoefficient>>("pm_hog_ccoeff.bin", {});
+    testHog<PerfectMemoryStore::HOG<CorrCoefficient>>("pm_hog_ccoeff.bin", {}, 1e-5);
 }
 
 TEST(PerfectMemory, SampleImageHogCorrCoefficientWindow)
 {
-    testHog<PerfectMemoryStore::HOG<CorrCoefficient>>("window_pm_hog_ccoeff.bin", { 0, 10 });
+    testHog<PerfectMemoryStore::HOG<CorrCoefficient>>("window_pm_hog_ccoeff.bin", { 0, 10 }, 1e-5);
 }
