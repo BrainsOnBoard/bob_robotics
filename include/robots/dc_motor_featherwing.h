c5aad7f4757762b1b3703f754ef5cae4f291bea0
neworderofjamie <neworderofjamie@gmail.com>
2021-06-09 16:41:49 +0100

    basic implementation of DC motor control via featherwing

---
+++ include/robots/dc_motor_featherwing.h
@@ -0,0 +1,66 @@
+#pragma once
+#ifdef __linux__
+
+// BoB robotics includes
+#include "common/pca_9685.h"
+
+// Third-party includes
+#include "third_party/units.h"
+
+//----------------------------------------------------------------------------
+// BoBRobotics::Robots::DCMotorFeatherWing
+//----------------------------------------------------------------------------
+namespace BoBRobotics {
+namespace Robots {
+class DCMotorFeatherWing
+{
+public:
+    DCMotorFeatherWing(const char *path = I2C_DEVICE_DEFAULT, int slaveAddress = 0x60, 
+                       units::frequency::hertz_t pwmFrequency = units::frequency::hertz_t(1600));
+    
+    //------------------------------------------------------------------------
+    // Enumerations
+    //------------------------------------------------------------------------
+    enum Motor
+    {
+        MOTOR_1,
+        MOTOR_2,
+        MOTOR_3,
+        MOTOR_4,
+        MOTOR_MAX,
+    };
+    
+    //------------------------------------------------------------------------
+    // Public API
+    //------------------------------------------------------------------------
+    void enableMotor(Motor motor);
+    void setDCMotorThrottle(Motor motor, float throttle);
+    
+private:
+    //------------------------------------------------------------------------
+    // Enumerations
+    //------------------------------------------------------------------------
+    enum MotorChannel
+    {
+        MOTOR_CHANNEL_ENABLE,
+        MOTOR_CHANNEL_POSITIVE,
+        MOTOR_CHANNEL_NEGATIVE,
+        MOTOR_CHANNEL_MAX
+    };
+    
+    //------------------------------------------------------------------------
+    // Constants
+    //------------------------------------------------------------------------
+    //! Mapping of motors to PCA9685 PWM channels
+    static const PCA9685::Channel motorChannels[MOTOR_MAX][MOTOR_CHANNEL_MAX];
+    
+    //------------------------------------------------------------------------
+    // Members
+    //------------------------------------------------------------------------
+    //! PCA9685 chip all the motors are connected to
+    PCA9685 m_PCA9685;
+};
+}   // namespace Robots
+}   // namespace BoBRobotics
+
+#endif   // __linux__
---
+++ src/robots/dc_motor_featherwing.cc
@@ -0,0 +1,52 @@
+#ifdef __linux__
+// BoB robotics includes
+#include "robots/dc_motor_featherwing.h"
+
+//----------------------------------------------------------------------------
+// BoBRobotics::Robots::DCMotorFeatherWing
+//----------------------------------------------------------------------------
+namespace BoBRobotics {
+namespace Robots {
+const PCA9685::Channel DCMotorFeatherWing::motorChannels[MOTOR_MAX][MOTOR_CHANNEL_MAX] = {
+        {PCA9685::Channel::CHANNEL_8, PCA9685::Channel::CHANNEL_9, PCA9685::Channel::CHANNEL_10},
+        {PCA9685::Channel::CHANNEL_13, PCA9685::Channel::CHANNEL_11, PCA9685::Channel::CHANNEL_12},
+        {PCA9685::Channel::CHANNEL_2, PCA9685::Channel::CHANNEL_3, PCA9685::Channel::CHANNEL_4},
+        {PCA9685::Channel::CHANNEL_7, PCA9685::Channel::CHANNEL_5, PCA9685::Channel::CHANNEL_6}};
+//----------------------------------------------------------------------------
+DCMotorFeatherWing::DCMotorFeatherWing(const char *path, int slaveAddress, units::frequency::hertz_t pwmFrequency)
+:   m_PCA9685(path, slaveAddress)
+{
+    // Set PWM frequency
+    m_PCA9685.setFrequency(pwmFrequency);
+}
+//----------------------------------------------------------------------------
+void DCMotorFeatherWing::enableMotor(Motor motor)
+{
+    m_PCA9685.setDutyCycle(motorChannels[motor][MOTOR_CHANNEL_ENABLE], 1.0f);
+}
+//----------------------------------------------------------------------------
+void DCMotorFeatherWing::setDCMotorThrottle(Motor motor, float throttle)
+{
+    // Clamp throttle value
+    throttle = std::min(1.0f, std::max(-1.0f, throttle));
+    
+    // If throttle is zero, brake motor by turning on both channels
+    if(throttle == 0.0f) {
+        m_PCA9685.setDutyCycle(motorChannels[motor][MOTOR_CHANNEL_POSITIVE], 1.0f);
+        m_PCA9685.setDutyCycle(motorChannels[motor][MOTOR_CHANNEL_NEGATIVE], 1.0f);
+    }
+    // Otherwise, if throttle is negative, turn off positive channel and turn on negative channel
+    else if(throttle < 0.0f) {
+        m_PCA9685.setDutyCycle(motorChannels[motor][MOTOR_CHANNEL_POSITIVE], 0.0f);
+        m_PCA9685.setDutyCycle(motorChannels[motor][MOTOR_CHANNEL_NEGATIVE], -throttle);
+    }
+    // Otherwise, if throttle is positive, turn on negative channel and turn off positive
+    else {
+        m_PCA9685.setDutyCycle(motorChannels[motor][MOTOR_CHANNEL_POSITIVE], throttle);
+        m_PCA9685.setDutyCycle(motorChannels[motor][MOTOR_CHANNEL_NEGATIVE], 0.0f);
+    }
+}
+
+}   // namespace Robots
+}   // namespace BoBRobotics
+#endif   // __linux__
