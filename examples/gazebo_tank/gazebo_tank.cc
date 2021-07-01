057049a567127c241001d81f808ccc4d3ec604c0
Alex Dewar <a.dewar@sussex.ac.uk>
2021-07-01 11:02:12 +0100

    Allow running Gazebo examples without joystick

--- examples/gazebo_tank/gazebo_tank.cc
+++ examples/gazebo_tank/gazebo_tank.cc
@@ -1,10 +1,10 @@
 // BoB robotics includes
-#include "plog/Log.h"
 #include "hid/joystick.h"
-#include "robots/simulated_tank.h"
+#include "plog/Log.h"
 #include "robots/gazebo/camera.h"
 #include "robots/gazebo/node.h"
 #include "robots/gazebo/tank.h"
+#include "robots/simulated_tank.h"
 #include "video/display.h"
 
 // Third-party includes
@@ -23,7 +23,8 @@ using namespace BoBRobotics;
 using namespace BoBRobotics::Robots;
 using namespace std::literals;
 
-int bobMain(int argc, char **argv)
+int
+bobMain(int argc, char **argv)
 {
     /************************************Gazebo setup************/
 
@@ -33,14 +34,13 @@ int bobMain(int argc, char **argv)
     /************************************Gazebo setup end************/
     std::unique_ptr<Video::Display> display;
     std::unique_ptr<Gazebo::Camera> cam;
-    if(argc >= 3) { // Initialize gazebo camera if more than 2 arguments are provided (display switch and camera url)
+    if (argc >= 3) { // Initialize gazebo camera if more than 2 arguments are provided (display switch and camera url)
         std::cout << "Display switch enabled.\n";
-        if(strcmp(argv[1], "-p") == 0) {
+        if (strcmp(argv[1], "-p") == 0) {
             std::cout << "Using panoramic camera.\n";
             cam = std::make_unique<Gazebo::Camera>(node, argv[2], true);
-            display = std::make_unique<Video::Display>(*cam, cv::Size(640,320)); //unwrap resolution needs to be supplied
-        }
-        else if(strcmp(argv[1], "-s") == 0){
+            display = std::make_unique<Video::Display>(*cam, cv::Size(640, 320)); //unwrap resolution needs to be supplied
+        } else if (strcmp(argv[1], "-s") == 0) {
             std::cout << "Using simple camera.\n";
             cam = std::make_unique<Gazebo::Camera>(node, argv[2], false);
             display = std::make_unique<Video::Display>(*cam); //unwrap resolution needs to be supplied
@@ -48,23 +48,34 @@ int bobMain(int argc, char **argv)
     }
 
     Gazebo::Tank robot(1_mps, node); // Tank agent
-    HID::Joystick joystick(0.25f);
-    robot.controlWithThumbsticks(joystick);
+    std::unique_ptr<HID::Joystick> joystick;
+    try {
+        joystick = std::make_unique<HID::Joystick>(0.25f);
+        robot.controlWithThumbsticks(*joystick);
+        std::cout << "Drive the car using the two thumbsticks: each stick is for one motor" << std::endl;
+    } catch (std::runtime_error &) {
+        LOGW << "Could not find joystick";
+    }
 
-    std::cout << "Drive the car using the two thumbsticks: each stick is for one motor" << std::endl;
-
-    do {
-        // Check for joystick events
-        if (!joystick.update() && (!display || !display->update())) {
-            // A small delay so we don't hog CPU
-            std::this_thread::sleep_for(5ms);
-        }
-    } while (!joystick.isPressed(HID::JButton::B));
+    if (!display && !joystick) {
+        std::cout << "Press any key to exit\n";
+        std::cin.ignore();
+    } else {
+        do {
+            // Check for joystick events
+            bool joyUpdate = joystick && joystick->update();
+            bool dispUpdate = display && display->update();
+            if (!joyUpdate && !dispUpdate) {
+                // A small delay so we don't hog CPU
+                std::this_thread::sleep_for(5ms);
+            }
+        } while ((!joystick || !joystick->isPressed(HID::JButton::B)) &&
+                    (!display || display->isOpen()));
+    }
 
     // Make sure to shut everything down.
-    display->close();
     Gazebo::shutDown();
-    std::cout <<"Shutting down...\n";
+    std::cout << "Shutting down...\n";
 
     return EXIT_SUCCESS;
 }
--- examples/gazebo_uav/gazebo_uav.cc
+++ examples/gazebo_uav/gazebo_uav.cc
@@ -21,7 +21,8 @@ using namespace BoBRobotics;
 using namespace BoBRobotics::Robots;
 using namespace std::literals;
 
-int bobMain(int argc, char **argv)
+int
+bobMain(int argc, char **argv)
 {
     /************************************Gazebo setup************/
 
@@ -31,43 +32,54 @@ int bobMain(int argc, char **argv)
     /************************************Gazebo setup end************/
     std::unique_ptr<Video::Display> display;
     std::unique_ptr<Gazebo::Camera> cam;
-    switch(argc){
-        case 1:
-            std::cout << "Camera disabled.\n";
-            break;
-        case 3: // Initialize gazebo camera if more than 2 arguments are provided (display switch and camera url)
-            std::cout << "Display switch enabled.\n";
-            if(strcmp(argv[1], "-p") == 0) {
-                std::cout << "Using panoramic camera.\n";
-                cam = std::make_unique<Gazebo::Camera>(node, argv[2], true);
-                display = std::make_unique<Video::Display>(*cam, cv::Size(640,320)); //unwrap resolution needs to be supplied
-            }
-            else{
-                std::cerr << "Unsupported flag\n";
-            }
-            break;
-        default:
-            std::cout <<"Usage " << argv[0] << " [-s,-p] [camera_url]\n";
-
+    switch (argc) {
+    case 1:
+        std::cout << "Camera disabled.\n";
+        break;
+    case 3: // Initialize gazebo camera if more than 2 arguments are provided (display switch and camera url)
+        std::cout << "Display switch enabled.\n";
+        if (strcmp(argv[1], "-p") == 0) {
+            std::cout << "Using panoramic camera.\n";
+            cam = std::make_unique<Gazebo::Camera>(node, argv[2], true);
+            display = std::make_unique<Video::Display>(*cam, cv::Size(640, 320)); //unwrap resolution needs to be supplied
+        } else {
+            std::cerr << "Unsupported flag\n";
+        }
+        break;
+    default:
+        std::cout << "Usage " << argv[0] << " [-s,-p] [camera_url]\n";
     }
+
     Robots::Gazebo::UAV iris(node); // QuadCopter agent
-    HID::Joystick joystick(0.25f);
-    iris.addJoystick(joystick);
+    std::unique_ptr<HID::Joystick> joystick;
+    try {
+        joystick = std::make_unique<HID::Joystick>(0.25f);
+        iris.addJoystick(*joystick);
+    } catch (std::runtime_error &) {
+        LOGW << "Could not find joystick";
+    }
 
     std::cout << "Iris connected and ready for liftoff!" << std::endl;
 
-    do {
-        // Check for joystick events
-        if (!joystick.update() && (!display || !display->update())) {
-            // A small delay so we don't hog CPU
-            std::this_thread::sleep_for(5ms);
-        }
-    } while (!joystick.isPressed(HID::JButton::X));
+    if (!display && !joystick) {
+        std::cout << "Press any key to exit\n";
+        std::cin.ignore();
+    } else {
+        do {
+            // Check for joystick events
+            bool joyUpdate = joystick && joystick->update();
+            bool dispUpdate = display && display->update();
+            if (!joyUpdate && !dispUpdate) {
+                // A small delay so we don't hog CPU
+                std::this_thread::sleep_for(5ms);
+            }
+        } while ((!joystick || !joystick->isPressed(HID::JButton::X)) &&
+                 (!display || display->isOpen()));
+    }
 
     // Make sure to shut everything down.
-    display->close();
     Gazebo::shutDown();
-    std::cout <<"Shutting down...\n";
+    std::cout << "Shutting down...\n";
 
     return EXIT_SUCCESS;
 }
