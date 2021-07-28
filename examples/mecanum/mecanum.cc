8dc00bc91fe94e3d318af015490d8b22841d8072
neworderofjamie <neworderofjamie@gmail.com>
2021-07-28 14:48:57 +0100

    wrong wiring

--- examples/mecanum/mecanum.cc
+++ examples/mecanum/mecanum.cc
@@ -1,5 +1,5 @@
 // BoB robotics includes
-#include "robots/mecanum.h"
+#include "robots/mecanum_pca_9685.h"
 
 #include <iostream>
 
@@ -12,7 +12,7 @@ int bobMain(int, char **)
     HID::Joystick joystick(0.25f);
 
     // Create motor interface
-    Robots::Mecanum robot;
+    Robots::MecanumPCA9685 robot;
 
     std::cout << "Hold down joystick buttons A, B, X or Y to drive an individual motor and hold down LB to reverse." << std::endl;
     std::cout << "If wrong motor turns, or motors turn in wrong direction, swap wires going from motor shield." << std::endl;
