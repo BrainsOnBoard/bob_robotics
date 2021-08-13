c7ae0f0a4b5c29b56e6d5bd4c91ba46053a9ffaa
Alex Dewar <a.dewar@sussex.ac.uk>
2021-08-13 14:00:42 +0100

    SFML: Add specialised drive() method for Ackermann-type robots

--- examples/simulated_ackermann/simulated_ackermann.cc
+++ examples/simulated_ackermann/simulated_ackermann.cc
@@ -1,5 +1,6 @@
 // BoB robotics includes
 #include "robots/ackermann/simulated_ackermann.h"
+#include "viz/sfml/robot_control.h"
 #include "viz/sfml/world.h"
 
 // Third-party includes
@@ -21,57 +22,20 @@ bobMain(int, char **)
     constexpr auto MaxSpeed = 1.4_mps;
     constexpr auto MaxTurn = 30_deg;
 
-    Robots::Ackermann::SimulatedAckermann robot(MaxSpeed, 500_mm); // simulated Ackermann car
-    Viz::SFML::World display({ 10.2_m, 10.2_m });                  // For displaying the agent
+    Robots::Ackermann::SimulatedAckermann robot(MaxSpeed, 500_mm, 0_m, MaxTurn);
+    Viz::SFML::World display({ 10.2_m, 10.2_m });
 
     auto car = display.createCarAgent(160_mm);
 
-    auto velocity = 0_mps;
-    auto turn = 0_deg;
     while (display.isOpen()) {
         car.setPose(robot.getPose());
 
         auto eventHandler = [&](const sf::Event &event) {
-            switch (event.type) {
-            case sf::Event::KeyPressed:
-                switch (event.key.code) {
-                case sf::Keyboard::Up:
-                    velocity = MaxSpeed;
-                    break;
-                case sf::Keyboard::Down:
-                    velocity = -MaxSpeed;
-                    break;
-                case sf::Keyboard::Left:
-                    turn = MaxTurn;
-                    break;
-                case sf::Keyboard::Right:
-                    turn = -MaxTurn;
-                default:
-                    break;
-                }
-                break;
-            case sf::Event::KeyReleased:
-                switch (event.key.code) {
-                case sf::Keyboard::Up:
-                case sf::Keyboard::Down:
-                    velocity = 0_mps;
-                    break;
-                case sf::Keyboard::Left:
-                case sf::Keyboard::Right:
-                    turn = 0_deg;
-                default:
-                    break;
-                }
-            default:
-                break;
-            }
+            Viz::SFML::drive(robot, event);
         };
 
         // run GUI and handle events
         display.drawAndHandleEvents(eventHandler, car);
-
-        // drive robot
-        robot.move(velocity, turn);
     }
 
     return EXIT_SUCCESS;
--- include/viz/sfml/robot_control.h
+++ include/viz/sfml/robot_control.h
@@ -1,5 +1,8 @@
 #pragma once
 
+// BoB robotics includes
+#include "robots/type_traits.h"
+
 // SFML
 #include <SFML/Graphics.hpp>
 
@@ -12,7 +15,8 @@ namespace SFML {
  *
  * Returns true if event was handled or false otherwise.
  */
-template<class RobotType>
+template<class RobotType,
+         std::enable_if_t<!Robots::IsAckermann<RobotType>::value, int> = 0>
 bool drive(RobotType &robot, const sf::Event &event)
 {
     switch (event.type) {
@@ -49,6 +53,78 @@ bool drive(RobotType &robot, const sf::Event &event)
         return false;
     }
 }
+
+/**!
+ * \brief Drive an Ackermann-type robot with an SFML window event
+ *
+ * The types of maxSpeed and maxTurn are templatable so that one can pass in a
+ * speed in e.g. m/s. Defaults to using maximum speed and turn.
+ *
+ * Returns true if event was handled or false otherwise.
+ */
+template<class RobotType, class SpeedType = float, class TurnType = float,
+         std::enable_if_t<Robots::IsAckermann<RobotType>::value, int> = 0>
+bool
+drive(RobotType &robot, const sf::Event &event, SpeedType maxSpeed = 1.f,
+      TurnType maxTurn = 1.f)
+{
+    /*
+     * We need to keep track of these values between calls. This is a slightly
+     * gross way of doing things, but making a templatable RobotControl class
+     * to manage state instead seems grosser.
+     */
+    static SpeedType velocity{};
+    static TurnType turn{};
+
+    bool handled = true;
+    switch (event.type) {
+    case sf::Event::KeyPressed:
+        switch (event.key.code) {
+        case sf::Keyboard::Up:
+            velocity = maxSpeed;
+            break;
+        case sf::Keyboard::Down:
+            velocity = -maxSpeed;
+            break;
+        case sf::Keyboard::Left:
+            turn = maxTurn;
+            break;
+        case sf::Keyboard::Right:
+            turn = -maxTurn;
+            break;
+        default:
+            handled = false;
+            break;
+        }
+        break;
+    case sf::Event::KeyReleased:
+        switch (event.key.code) {
+        case sf::Keyboard::Up:
+        case sf::Keyboard::Down:
+            velocity = SpeedType{ 0 };
+            break;
+        case sf::Keyboard::Left:
+        case sf::Keyboard::Right:
+            turn = TurnType{ 0 };
+            break;
+        default:
+            handled = false;
+            break;
+        }
+        break;
+    default:
+        handled = false;
+        break;
+    }
+
+    // Drive the robot
+    if (handled) {
+        robot.move(velocity, turn);
+    }
+
+    return handled;
+}
+
 } // SFML
 } // Viz
 } // BoBRobotics
