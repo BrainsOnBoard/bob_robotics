7bb2cd312c65f9721d9f7df032c2eae25db80187
Alex Dewar <a.dewar@sussex.ac.uk>
2021-09-03 10:41:53 +0100

    examples/display: Add option to use RandomInput

--- examples/display/display.cc
+++ examples/display/display.cc
@@ -14,44 +14,53 @@
  */
 
 // BoB robotics includes
-#include "plog/Log.h"
 #include "video/display.h"
 #include "video/opencvinput.h"
+#include "video/randominput.h"
 #include "video/rpi_cam.h"
+
+// Third-party includes
+#include "plog/Log.h"
+
+// Standard C++ includes
+#include <memory>
 
 // Standard C includes
 #include <cstring>
 
 using namespace BoBRobotics::Video;
 
-int bobMain(int argc, char **argv)
+std::unique_ptr<Input>
+getCamera(int argc, char **argv)
 {
     if (argc == 1) {
         // if no args supplied, use default webcam
-        OpenCVInput cam;
-        Display display(cam);
-        display.run();
-    } else {
-        try {
-            // if the arg is an int, the user is specifying a camera...
-            int dev = std::stoi(argv[1]);
-            OpenCVInput cam(dev);
-            Display display(cam);
-            display.run();
-        } catch (std::invalid_argument &) {
-            // ...else it's a filename/URL/RPiCam
-            if (strcmp(argv[1], "r") == 0) {
-                // RPicam
-                RPiCamera cam(50091);
-                Display display(cam);
-                display.run();
-            } else {
-                OpenCVInput cam(argv[1]);
-                Display display(cam);
-                display.run();
-            }
-        }
+        return std::make_unique<OpenCVInput>();
     }
+
+    try {
+        // if the arg is an int, the user is specifying a camera...
+        return std::make_unique<OpenCVInput>(std::stoi(argv[1]));
+    } catch (std::invalid_argument &) {
+        // ...else it's a filename/URL/RPiCam
+        if (strcmp(argv[1], "rpi") == 0) {
+            // RPicam
+            return std::make_unique<RPiCamera>(50091);
+        }
+
+        if (strcmp(argv[1], "random") == 0) {
+            return std::make_unique<RandomInput<>>(cv::Size{ 360, 100 });
+        }
+
+        return std::make_unique<OpenCVInput>(argv[1]);
+    }
+}
+
+int bobMain(int argc, char **argv)
+{
+    auto cam = getCamera(argc, argv);
+    Display display{ *cam };
+    display.run();
 
     return EXIT_SUCCESS;
 }
