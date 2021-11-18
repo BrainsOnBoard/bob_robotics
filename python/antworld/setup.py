6facd30966cddb3f2d0aa4b303c95bc0006c1238
Alex Dewar <a.dewar@sussex.ac.uk>
2021-11-18 14:42:12 +0000

    Put Python antworld module into bobrobotics namespace

--- python/antworld/setup.py
+++ python/antworld/setup.py
@@ -6,6 +6,6 @@ setup(
     description="A python interface for the BoB robotics antworld module",
     author='Alex Dewar',
     license="GPLv2",
-    packages=['antworld'],
-    package_data = { 'antworld': ['*.dll'] }
+    packages=['bobrobotics.antworld'],
+    package_data = { 'bobrobotics/antworld': ['*.dll'] }
 )
