87f27945bfd999ffec8b4e710e5c843903043b3b
Alex Dewar <a.dewar@sussex.ac.uk>
2021-11-25 17:51:50 +0000

    Fix image database example code and add video recorder example

--- examples/image_database/write_example.cc
+++ examples/image_database/write_example.cc
@@ -3,19 +3,20 @@
 #include "video/randominput.h"
 
 // Third-party includes
+#include "plog/Log.h"
 #include "third_party/units.h"
 
 using namespace BoBRobotics;
 using namespace units::length;
 using namespace units::literals;
 
-int bobMain(int, char **)
+int bobMain(int argc, char **argv)
 {
     // Make a new image database with default path
     Navigation::ImageDatabase imdb;
 
     // Use randomly generated images
-    Video::RandomInput<> cam{ { 180, 50 }, "random", /*seed=*/42 };
+    Video::RandomInput<> cam{ { 640, 480 }, "random", /*seed=*/42 };
 
     /*
      * Start recording data, using a single extra field called Sensor value. You
@@ -23,14 +24,22 @@ int bobMain(int, char **)
      * pass in all the names to this function.
      *
      * // No extra fields
-     * auto recorder = imdb.createRouteRecorder();
+     * auto recorder = imdb.createRouteRecorder("jpg");
      *
      * // Two extra fields
-     * auto recorder = imdb.createRouteRecorder("Value 1", "Value 2");
+     * auto recorder = imdb.createRouteRecorder("jpg", { "Value 1", "Value 2" });
      *
      * // ...etc.
+     *
+     * You can also record databases as video files by using the syntax below.
      */
-    auto recorder = imdb.createRouteRecorder("Sensor value");
+    std::unique_ptr<Navigation::ImageDatabase::RouteRecorder> recorder;
+    if (argc == 2 && strcmp(argv[1], "video") == 0) {
+        LOGI << "Saving video file";
+        recorder = imdb.createVideoRouteRecorder(cam.getOutputSize(), 5_Hz, "avi", "XVID", { "Sensor value" });
+    } else {
+        recorder = imdb.createRouteRecorder("png", { "Sensor value" });
+    }
 
     // Save camera info (not essential, but useful so we know resolution etc.)
     recorder->getMetadataWriter() << "camera" << cam;
