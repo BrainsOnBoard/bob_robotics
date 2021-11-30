d287e9bebb52a90b6b512a1fe31eaf9cea23519b
Alex Dewar <a.dewar@sussex.ac.uk>
2021-11-30 14:37:05 +0000

    Jenkins: Make build folder explicitly
    It seems not to be being created on @neworderofjamie's machine.

--- bin/build_all_jenkins.sh
+++ bin/build_all_jenkins.sh
@@ -13,5 +13,6 @@ pip3 install numpy scikit-build
 
 # Configure and build source, continuing if there are errors
 rm -rf build
+mkdir build
 cmake -B build -DCMAKE_EXPORT_COMPILE_COMMANDS=1 "$@"
 make -C build -k -j $(nproc)
