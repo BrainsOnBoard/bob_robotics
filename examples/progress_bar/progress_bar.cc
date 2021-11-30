0a61781747cd5d3d83fc2e17e4d2e3169b0d812f
Alex Dewar <a.dewar@sussex.ac.uk>
2021-11-30 10:21:46 +0000

    Fix progress_bar example

--- examples/progress_bar/progress_bar.cc
+++ examples/progress_bar/progress_bar.cc
@@ -4,7 +4,8 @@
 int bobMain(int, char **)
 {
     constexpr size_t Count = 100;
-    BoBRobotics::ProgressBar progbar{ Count };
+    constexpr const char *Label = "Loop";
+    BoBRobotics::ProgressBar progbar{ Label, Count };
     for (size_t i = 0; i < Count; i++) {
         progbar.increment();
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
