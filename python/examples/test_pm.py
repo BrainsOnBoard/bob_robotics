c7bba0506507f4cdbcab535a069e2e8ce1dc7588
Alex Dewar <a.dewar@sussex.ac.uk>
2021-12-14 17:22:29 +0000

    test_pm: Plot RIDF

--- python/examples/test_pm.py
+++ python/examples/test_pm.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 from bob_robotics import navigation
 import cv2
+import matplotlib.pyplot as plt
+import numpy as np
 import sys
 
 dbPath = sys.argv[1]
@@ -12,4 +14,17 @@ algo = navigation.PerfectMemory((180, 45))
 algo.train_route(dbPath)
 print('Training complete')
 
-print("angle: %g°" % algo.get_heading(img))
+t0 = time()
+ang = algo.get_heading(img)
+print(time() - t0)
+
+# Alternatively you can use algo.get_heading(img) to just getting the heading,
+# which should be faster
+(heading, best_snap, best_min, diffs) = algo.get_ridf_data(img)
+
+print('Best snapshot: %d at %g° (diff: %g)' % (best_snap, heading, best_min))
+
+xs = np.linspace(0, 360, diffs.shape[1])
+ridf = diffs[best_snap, :]
+plt.plot(xs, ridf / 255.0)
+plt.show()
