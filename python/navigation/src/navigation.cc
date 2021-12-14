97681916221731a0be01f0ba1a1a0ddd5e594368
Alex Dewar <a.dewar@sussex.ac.uk>
2021-12-14 16:23:04 +0000

    bob_navigation: Add get_ridf_data() method for getting extra info from algos

--- python/navigation/src/navigation.cc
+++ python/navigation/src/navigation.cc
@@ -1,11 +1,14 @@
 // BoB robotics includes
 #include "common/main.h"
+#include "navigation/image_database.h"
 #include "navigation/infomax.h"
 #include "navigation/perfect_memory.h"
-#include "navigation/image_database.h"
 
 // Third-party includes
+#include "pybind11/eigen.h"
 #include "pybind11/pybind11.h"
+#include "pybind11/stl.h"
+#include "third_party/units.h"
 
 // Python includes
 #define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
@@ -20,66 +23,87 @@ using namespace units::angle;
 
 namespace pybind11 {
 namespace detail {
-    template <> struct type_caster<cv::Size> {
-    public:
-        PYBIND11_TYPE_CASTER(cv::Size, _("cvSize"));
+template<>
+struct type_caster<cv::Size>
+{
+    PYBIND11_TYPE_CASTER(cv::Size, _("cvSize"));
 
-        // For converting from a Python tuple to a cv::Mat
-        bool load(handle src, bool) {
-            return PyArg_ParseTuple(src.ptr(), "ii", &value.width, &value.height);
+    // For converting from a Python tuple to a cv::Mat
+    bool load(handle src, bool)
+    {
+        return PyArg_ParseTuple(src.ptr(), "ii", &value.width, &value.height);
+    }
+};
+
+// Coerce all angle-unit types into degrees
+template<class T>
+struct type_caster<T, std::enable_if_t<units::traits::is_angle_unit<T>::value>>
+{
+    PYBIND11_TYPE_CASTER(degree_t, _("degrees"));
+
+    static handle cast(degree_t src, return_value_policy /* policy */, handle /* parent */)
+    {
+        return PyFloat_FromDouble(src.value());
+    }
+};
+
+template<>
+struct type_caster<cv::Mat>
+{
+    PYBIND11_TYPE_CASTER(cv::Mat, _("cvMat"));
+
+    bool load(handle src, bool)
+    {
+        auto *obj = src.ptr();
+        if (!PyArray_Check(obj)) {
+            return false;
         }
-    };
 
-    template <> struct type_caster<cv::Mat> {
-    public:
-        PYBIND11_TYPE_CASTER(cv::Mat, _("cvMat"));
+        auto *arr = reinterpret_cast<PyArrayObject *>(obj);
 
-        bool load(handle src, bool) {
-            auto *obj = src.ptr();
-            if (!PyArray_Check(obj)) {
-                return false;
-            }
-
-            auto *arr = reinterpret_cast<PyArrayObject *>(obj);
-
-            /*
-             * It would be totally trivial to add other types, but we're only
-             * interested in uint8-type images for now.
-             */
-            int cvType;
-            switch (PyArray_TYPE(arr)) {
-            case NPY_UINT8:
-                cvType = CV_8U;
-                break;
-            default:
-                throw std::runtime_error{ "Only uint8-type arrays are allowed" };
-            }
-
-            /*
-             * Note that we are taking a reference to arr's data, rather than
-             * copying it. We only support 2D (i.e. greyscale) images.
-             */
-            BOB_ASSERT(PyArray_NDIM(arr) == 2);
-            const cv::Size size{ (int) PyArray_DIM(arr, 1), (int) PyArray_DIM(arr, 0) };
-            value = cv::Mat(size, cvType, PyArray_DATA(arr));
-
-            return true;
+        /*
+         * It would be totally trivial to add other types, but we're only
+         * interested in uint8-type images for now.
+         */
+        int cvType;
+        switch (PyArray_TYPE(arr)) {
+        case NPY_UINT8:
+            cvType = CV_8U;
+            break;
+        default:
+            throw std::runtime_error{ "Only uint8-type arrays are allowed" };
         }
-    };
+
+        /*
+         * Note that we are taking a reference to arr's data, rather than
+         * copying it. We only support 2D (i.e. greyscale) images.
+         */
+        BOB_ASSERT(PyArray_NDIM(arr) == 2);
+        const cv::Size size{ (int) PyArray_DIM(arr, 1), (int) PyArray_DIM(arr, 0) };
+        value = cv::Mat(size, cvType, PyArray_DATA(arr));
+
+        return true;
+    }
+};
 }
 } // namespace pybind11::detail
 
 template<class T>
-class PyAlgoWrapper {
+class PyAlgoWrapper
+{
 public:
     PyAlgoWrapper(const cv::Size &size)
       : m_Algo(size)
     {}
 
-    double getHeading(const cv::Mat &img) const
+    auto getHeading(const cv::Mat &img) const
     {
-        const degree_t angle = std::get<0>(m_Algo.getHeading(getResizedMat(img)));
-        return angle.value();
+        return std::get<0>(getRIDFData(img));
+    }
+
+    auto getRIDFData(const cv::Mat &img) const
+    {
+        return m_Algo.getHeading(getResizedMat(img));
     }
 
     auto test(const cv::Mat &img) const
@@ -120,18 +144,21 @@ private:
 };
 
 template<class Algo>
-void addAlgo(py::handle scope, const char *name)
+void
+addAlgo(py::handle scope, const char *name)
 {
     using T = PyAlgoWrapper<Algo>;
     py::class_<T>(scope, name)
             .def(py::init<const cv::Size &>())
             .def("get_heading", &T::getHeading)
+            .def("get_ridf_data", &T::getRIDFData)
             .def("test", &T::test)
             .def("train", &T::train)
             .def("train_route", &T::trainRoute);
 }
 
-PYBIND11_MODULE(_navigation, m) {
+PYBIND11_MODULE(_navigation, m)
+{
     // Initialise plog
     BoBRobotics::initLogging();
 
