1a21c420f535498dcc3b675f4455c47dc0168fde
Alex Dewar <a.dewar@sussex.ac.uk>
2022-01-17 16:24:13 +0000

    Python: Use git to get commit info for version string

--- python/navigation/setup.py
+++ python/navigation/setup.py
@@ -1,8 +1,13 @@
 from skbuild import setup  # This line replaces 'from setuptools import setup'
+import sys
+import os.path
+sys.path.append(os.path.dirname(__file__) + "/..")
+
+from build_common import get_git_version
 
 setup(
     name="bob_navigation",
-    version="0.0.1",
+    version=get_git_version(),
     description="A python interface for the BoB robotics navigation module",
     author='Alex Dewar',
     license="GPLv2",
