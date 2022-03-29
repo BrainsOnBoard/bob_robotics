39c3abfb112e67d840a54c307e73d349ab3327c1
Alex Dewar <a.dewar@sussex.ac.uk>
2022-03-29 15:36:02 +0100

    Fix build issues for test generator programs
    These were left behind in a previous refactor.
    
    Also include these programs in a full build so we can check for future breakages with CI.

--- tests/navigation/generate_data.h
+++ tests/navigation/generate_data.h
@@ -21,15 +21,14 @@ generateDataRaw(const std::string &filename, ImgProc::Mask mask, Window window,
                 Ts&&... args)
 {
     Algo algo{ TestImageSize, std::forward<Ts>(args)... };
-    algo.setMask(std::move(mask));
     for (const auto &image : TestImages) {
-        algo.train(image);
+        algo.train(image, mask);
     }
 
     if (window == Window{}) {
         window = algo.getFullWindow();
     }
-    const auto &differences = algo.getImageDifferences(window, TestImages[0]);
+    const auto &differences = algo.getImageDifferences(TestImages[0], mask, window);
     static_assert(std::is_same<const float &, const decltype(differences[0]) &>::value,
                   "Must return floats");
     writeMatrix(getTestsPath() / filename, differences);
