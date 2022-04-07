f732734b4558fa28274ced1a3fbe45fa8946d8c5
Alex Dewar <a.dewar@sussex.ac.uk>
2022-04-07 13:40:51 +0100

    Allow for passing subset of entries DataFrame to read_images

--- python/navigation/bob_robotics/navigation/__init__.py
+++ python/navigation/bob_robotics/navigation/__init__.py
@@ -106,6 +106,9 @@ class Database(DatabaseInternal):
 
     def read_images(self, entries=None, preprocess=None, to_float=True,
                     greyscale=True):
+        if hasattr(entries, "index"):
+            # ...then it's a subrange of the entries DataFrame
+            entries = entries.index
         images = super().read_images(entries, greyscale)
 
         if to_float:
