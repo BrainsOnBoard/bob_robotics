43e3c41b06b48775ca90b98616eb87f6dcf5c106
Alex Dewar <a.dewar@sussex.ac.uk>
2022-04-13 10:30:35 +0100

    Also missing dependency on scipy

--- python/navigation/build_and_test.sh
+++ python/navigation/build_and_test.sh
@@ -8,7 +8,7 @@ python -m venv virtualenv
 . virtualenv/bin/activate
 
 # Install necessary packages, including BoB navigation module
-pip install matplotlib numpy pandas pytest scikit-build wheel
+pip install matplotlib numpy pandas pytest scikit-build scipy wheel
 cd "$(dirname $0)"
 python setup.py build install
 
