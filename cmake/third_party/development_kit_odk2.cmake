10a553851de66200d0104958f02b4b2d9af6ae94
Alex Dewar <a.dewar@sussex.ac.uk>
2022-04-26 11:47:30 +0100

    Delete development_kit_odk2 submodule
    Now the user has to set the ODK2_PATH environment variable to point to an external copy of development_kit_odk2 instead.
    
    Fixes #322.

--- cmake/third_party/development_kit_odk2.cmake
+++ cmake/third_party/development_kit_odk2.cmake
@@ -1,3 +1,7 @@
-add_subdirectory("${BOB_ROBOTICS_PATH}/third_party/development_kit_odk2/devkit_driver"
+# The last known working commit for the ODK2 devkit is 4af04b8
+if (NOT DEFINED ENV{ODK2_PATH})
+    message(FATAL_ERROR "ODK2_PATH environment variable must be set to point to the development_kit_odk2 repo")
+endif()
+add_subdirectory("$ENV{ODK2_PATH}/devkit_driver"
                  "${CMAKE_CURRENT_BINARY_DIR}/BoB/third_party/${MODULE}")
 set(LIBRARIES devkit_driver)
