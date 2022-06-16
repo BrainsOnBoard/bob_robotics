b4d97a56601d09dfb65715fc49394b49b00c3109
Alex Dewar <a.dewar@sussex.ac.uk>
2022-06-16 16:16:35 +0100

    Print extra info in matlab script

--- examples/gantry/plot_velocities.m
+++ examples/gantry/plot_velocities.m
@@ -1,3 +1,4 @@
+%% Plot velocities saved by gantry_continuous.exe
 figure; hold on
 data = csvread('gantry_move.csv');
 for i = 2:4
@@ -5,3 +6,10 @@ for i = 2:4
 end
 xlabel('Time (ms)')
 ylabel('Velocity (m/s)')
+
+%% Sanity check: should be something sensible
+dt = diff(data(:,1)) / 1000;
+dist = sum(data(2:end, 2:end) .* dt);
+
+disp('Finishing position:')
+disp(dist)
